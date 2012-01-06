9a16928a26617ba1f0b1f24d727e1caefe9d7180
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-01-06 03:20:36 +0000

    reeb-17
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@8 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- website/user/app/common/CharacterUtil.java
+++ website/user/app/common/CharacterUtil.java
@@ -9,23 +9,20 @@ import java.util.Random;
  *
  */
 public class CharacterUtil {
-	
+
 	/**
 	 * 产生随机数
 	 * @param length
 	 * @return
 	 */
-	public static String getRandomString(int length)
-	{
-	String str="abcdefghigklmnopkrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ0123456789";
-	Random random=new Random();
-	StringBuffer sf=new StringBuffer();
-	for(int i=0;i<length;i++)
-	{
-	 int number=random.nextInt(62);//0~61
-	 sf.append(str.charAt(number));
+	public static String getRandomString(int length) {
+		String str="abcdefghigklmnopkrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ0123456789";
+		Random random=new Random();
+		StringBuffer sf=new StringBuffer();
+		for(int i=0;i<length;i++)	{
+			int number=random.nextInt(62);//0~61
+			sf.append(str.charAt(number));
+		}
+		return sf.toString();
 	}
-	return sf.toString();
-	}
-	
 }
