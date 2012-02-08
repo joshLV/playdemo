7e47c706fd4393bf239a758f1012c6e3c5c81c6e
tangliqun <tangliqun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-08 07:26:48 +0000

    REEB-46 实现trader home的layout module.
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@41 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- traders/home/test/ApplicationTest.java
+++ traders/home/test/ApplicationTest.java
@@ -12,6 +12,7 @@ public class ApplicationTest extends FunctionalTest {
         assertIsOk(response);
         assertContentType("text/html", response);
         assertCharset(play.Play.defaultWebEncoding, response);
+        this.assertContentMatch("Hello, world!" , response);
     }
     
 }
