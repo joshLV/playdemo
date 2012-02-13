9ca081b08349f33c45477b90e0a793dcd2ecb8c4
sujie <sujie@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-13 03:15:23 +0000

    REEB-47
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@69 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ website/www/app/controllers/Home.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import models.sales.Goods;
+import play.mvc.Controller;
+
+import java.util.List;
+
+/**
+ * 首页控制器.
+ * <p/>
+ * User: sujie
+ * Date: 2/13/12
+ * Time: 9:57 AM
+ */
+public class Home extends Controller {
+
+    public static void index() {
+        List<Goods> goodsList = Goods.findAll();
+
+        render(goodsList);
+    }
+
+}
