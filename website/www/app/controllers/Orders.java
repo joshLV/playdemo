33a0f02aadbaf7f8a3cb47918e4f834d846d5f81
sujie <sujie@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-14 03:49:21 +0000

    REEB-58
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@93 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ website/www/app/controllers/Orders.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * 用户订单确认控制器.
+ * <p/>
+ * User: sujie
+ * Date: 2/14/12
+ * Time: 11:31 AM
+ */
+public class Orders extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+}
