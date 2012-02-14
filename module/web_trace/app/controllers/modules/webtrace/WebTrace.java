e62b16ccd5a8785a207d49ff7f2b1fcce0dc128e
likang <likang@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-14 06:54:53 +0000

    REEB-56 webtrace
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@96 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- module/web_trace/app/controllers/modules/webtrace/WebTrace.java
+++ module/web_trace/app/controllers/modules/webtrace/WebTrace.java
@@ -11,7 +11,7 @@ public class WebTrace extends Controller {
         Http.Cookie cookieId = request.cookies.get("identity");
         if (cookieId == null){
             Logger.debug("[WebTrace]: set a new cookie identity");
-            response.setCookie("identity",session.getId());
+            response.setCookie("identity",session.getId(),"365d");
         }
     }
 
