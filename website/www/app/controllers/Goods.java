bc00ff43a9dd154d049701da31aa57ffbf54cfa2
likang <likang@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-14 03:33:59 +0000

    REEB-56 add webtrace to home page
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@91 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- website/www/app/controllers/Goods.java
+++ website/www/app/controllers/Goods.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import play.mvc.Controller;
+import play.mvc.With;
+import controllers.modules.webtrace.WebTrace;
 
 /**
  * 商品控制器.
@@ -9,6 +11,7 @@ import play.mvc.Controller;
  * Date: 2/13/12
  * Time: 5:32 PM
  */
+@With(WebTrace.class)
 public class Goods extends Controller {
 
     public static void show(long id) {
