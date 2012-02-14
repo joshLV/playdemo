5a5d8155314c57768b39631e1b7716ecfdc18dc8
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-14 09:23:46 +0000

    reeb-50
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@113 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- traders/sales/app/controllers/Goods.java
+++ traders/sales/app/controllers/Goods.java
@@ -4,6 +4,12 @@
  */
 package controllers;
 
+import java.io.File;
+import java.util.List;
+
+import CommonUtil.Common;
+
+import play.cache.Cache;
 import play.mvc.Controller;
 
 /**
@@ -14,23 +20,106 @@ import play.mvc.Controller;
  */
 public class Goods extends Controller {
 
-    /**
-     * 展示添加商品页面
-     */
-    public static void index() {
-        render();
-    }
+	/**
+	 * 展示商品一览页面
+	 */
+	public static void index() {
+		List list= models.sales.Goods.findAll();
+		renderTemplate("sales/Goods/index.html",list);
+	}
 
-    /**
-     * 添加商品
-     * POST /goods
-     */
-    public static void create(models.sales.Goods goods) {
-        if (validation.hasErrors()) {
-            error("Validation errors");
-        }
-        goods.create();
+	/**
+	 * 展示添加商品页面
+	 */
+	public static void add() {
+		renderTemplate("sales/Goods/add.html");
+	}
 
-        index();
-    }
+	/**
+	 *  添加商品
+	 * @param image_path
+	 * @param goods
+	 */
+	public static void create(File image_path,models.sales.Goods goods,String flag) {
+		if (validation.hasErrors()) {
+			error("Validation errors");
+		}
+		//默认商品下架状态，点击保存并上架状态为1
+		String status ="0";
+		if ("1".equals(flag)) {
+			status ="1";
+		}
+
+		if (image_path !=null && image_path.getName() !=null ) {
+			//取得文件存储路径
+			String storepath = play.Play.configuration.get("upload.imagepath").toString();
+			//上传文件
+			new Common().storeImage(image_path,storepath+"/1/1/1/");
+		}
+
+		//添加商品处理
+		models.sales.Goods.addGoods(image_path,goods,status);
+		index();
+	}
+
+	/**
+	 * 取得指定商品信息
+	 */
+	public static void edit(String id) {
+		models.sales.Goods goods= models.sales.Goods.findById(Long.parseLong(id));
+		renderTemplate("sales/Goods/edit.html",goods);
+
+	}
+	/**
+	 * 取得指定商品信息
+	 */
+	public static void detail(String id) {
+		models.sales.Goods goods= models.sales.Goods.findById(Long.parseLong(id));
+		renderTemplate("sales/Goods/detail.html",goods);
+	}
+
+	/**
+	 *  更新指定商品信息
+	 * 
+	 * @param id
+	 */
+	public static void update(String id,File image_path,models.sales.Goods goods) {
+		if (image_path !=null && image_path.getName() !=null ) {
+			//取得文件存储路径
+			String storepath = play.Play.configuration.get("upload.imagepath").toString();
+			//上传文件
+			new Common().storeImage(image_path,storepath+"/1/1/1/");
+		}
+		//更新处理
+		models.sales.Goods.updateGoods(id,image_path,goods);
+		index();
+	}
+
+	/**
+	 *  上下架指定商品
+	 * 
+	 * @param id
+	 */
+	public static void updateStatus(Long checkoption[],String status) {
+		//更新处理
+		for (Long id:checkoption) {
+			System.out.println("id="+id);
+			models.sales.Goods goods= models.sales.Goods.findById(id);
+			goods.status=status;
+			goods.save();
+		}
+
+		index();
+	}
+
+	/**
+	 * 删除指定商品
+	 * @param id
+	 */
+	public static void delete(Long checkoption[]) {
+		for (Long id:checkoption) {
+			models.sales.Goods.delete("id=?",id);
+		}
+		index();
+	}
 }
