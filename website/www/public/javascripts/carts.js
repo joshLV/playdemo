45d0c67b85432018f3140759bdd1ba873dcf055d
likang <likang@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-15 04:43:44 +0000

    REEB-49 auto refresh total amount of carts
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@124 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- website/www/public/javascripts/carts.js
+++ website/www/public/javascripts/carts.js
@@ -1,8 +1,6 @@
-function reorder(element,indent){
-    var attr_id = element.attr("id");
-    var idIndex = attr_id.indexOf("_");
-    var goods_id = attr_id.substr(idIndex+1);
-    var last_num = $("#last_num_"+goods_id);
+function reorder(goods_id,indent){
+    var element = $("#num_" + goods_id);
+    var last_num = $("#last_num_" + goods_id);
 
     var new_num = Number(last_num.val()) + indent;
     if(new_num < 0){
@@ -14,26 +12,46 @@ function reorder(element,indent){
                 element.val(new_num);
                 last_num.val(new_num);
             });
+    calItem(goods_id);
+    refreshAmount();
+}
+
+function calItem(goods_id){
+    var total_price = Number($("#price_" + goods_id).text()) * Number($("#num_" + goods_id).val());
+    $("#subtotal_" + goods_id).val(total_price);
+}
+
+function refreshAmount(){
+    var number = 0;
+    $("input.num_input").each(function(){number += Number($(this).val())});
+    $("#total_num").text(number);
+
+    var amount = 0;
+    $("input[id^=subtotal_]").each(function(){amount += Number($(this).val())});;
+    $("#carts_amount").text(amount);
 }
 
 $(window).load(
     function(){
         $("a.add_box").click(function() {
-            reorder($("#num_"+$(this).attr("name")),1);
+            reorder($(this).attr("name"),1);
             return false;
         });
         $("a.reduce_box").click(function() {
-            reorder($("#num_"+$(this).attr("name")),-1);
+            reorder($(this).attr("name"),-1);
             return false;
         });
         $("input.num_input").blur(function(){
-            var last_num = $("#last_"+$(this).attr("id"));
+            var el_id = $(this).attr("id");
+            var last_num = $("#last_" + el_id);
             var re= /^\d+(\.\d+)?$/;
             if(!re.test($(this).val())){
                 $(this).val(last_num.val());
                 return;
             }
-            reorder($(this), Number($(this).val()) - Number(last_num.val()));
+            var goods_id = el_id.substr(el_id.indexOf("_") + 1);
+
+            reorder(goods_id, Number($(this).val()) - Number(last_num.val()));
         });
         $("a.delete_gift").click(function(){  
             var goods_id = $(this).attr("name");
@@ -47,6 +65,7 @@ $(window).load(
             
             return false;
         });
+        refreshAmount();
     }
 );
 
