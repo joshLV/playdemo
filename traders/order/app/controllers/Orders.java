ab956e501e903abf3dc84ef46d5a2e6cde847555
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-17 01:17:34 +0000

    reeb-60
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@166 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- traders/order/app/controllers/Orders.java
+++ traders/order/app/controllers/Orders.java
@@ -1,11 +1,25 @@
 package controllers;
 
+import java.util.List;
+
+import models.order.OrderItems;
 import play.mvc.Controller;
 
 public class Orders extends Controller {
 
-    public static void index() {
-        render();
-    }
+	/**
+	 * 商户订单信息一览
+	 */
+	public static void index() {
+		
+		render();
+	}
 
+	/**
+	 * 券号列表
+	 */
+	public static void quan() {
+		
+		render();
+	}
 }
