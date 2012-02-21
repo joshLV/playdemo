9e061f6e0c2f7cc574cbee3dbfab9c659db3ceb8
tangliqun <tangliqun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-21 07:50:47 +0000

    REEB-57
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@205 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- website/cas/src/main/java/org/jasig/cas/userauthentication/UsernameLengthAuthnHandler.java
+++ website/cas/src/main/java/org/jasig/cas/userauthentication/UsernameLengthAuthnHandler.java
@@ -8,12 +8,16 @@ import org.jasig.cas.authentication.handler.AuthenticationException;
 
 import org.jasig.cas.authentication.handler.support.AbstractUsernamePasswordAuthenticationHandler;
 import org.jasig.cas.authentication.principal.UsernamePasswordCredentials;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class UsernameLengthAuthnHandler extends AbstractUsernamePasswordAuthenticationHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(UsernameLengthAuthnHandler.class);
+
     private JdbcTemplate jdbcTemplate;
-    
+
     public JdbcTemplate getJdbcTemplate() {
         return jdbcTemplate;
     }
@@ -25,27 +29,28 @@ public class UsernameLengthAuthnHandler extends AbstractUsernamePasswordAuthenti
     @Override
     protected boolean authenticateUsernamePasswordInternal(UsernamePasswordCredentials Credentials)
             throws AuthenticationException {
-        
+
         String username = Credentials.getUsername();
         String password = Credentials.getPassword();
-        
-        String sql = "select * from users where  email= ?";
-        Object[] params = new Object[]{username}; 
 
-        List userlist = getJdbcTemplate().queryForList(sql,params);
-       
-        if(userlist.size()  == 0){
+        System.out.println("================ H " + username + " ================");
+        if (log.isDebugEnabled()) {
+            log.debug("email=" + username);
+        }
+        String sql = "select * from users where  email= ?";
+        Object[] params = new Object[] { username };
+
+        List<Map<String, Object>> userlist = getJdbcTemplate().queryForList(sql, params);
+
+        if (userlist.size() == 0) {
             return false;
         }
-        Map<String,Object>   user  = (Map<String, Object>) userlist.get(0);
-        
-        
-        if(!DigestUtils.md5Hex(password+user.get("password_salt")).equals(user.get("crypted_password"))){
+        Map<String, Object> user = userlist.get(0);
+
+        if (!DigestUtils.md5Hex(password + user.get("password_salt")).equals(user.get("crypted_password"))) {
             return false;
         }
-        
-        
-        
+
         return true;
     }
 
