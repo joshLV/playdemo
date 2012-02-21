2a59ed73b3a6318deb4b4eb522ca067d259ea67c
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-21 10:03:19 +0000

    reeb-54
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@209 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- website/home/app/controllers/MyOrders.java
+++ website/home/app/controllers/MyOrders.java
@@ -1,10 +1,28 @@
 package controllers;
 
+import java.util.List;
+
+import models.consumer.User;
+import models.order.Orders;
 import play.mvc.Controller;
 
 public class MyOrders extends Controller {
 
+	/**
+	 * 我的订单
+	 */
 	public static void index() {
+		String username = session.get("username");
+		username = "yjy";
+		User user = User.find("byLoginName", username).first();
+		List<Orders> orderList = Orders.find("byUser", user).fetch();
+		render(orderList);
+	}
+
+	/**
+	 * 订单详情
+	 */
+	public static void details() {
 		render();
 	}
 
