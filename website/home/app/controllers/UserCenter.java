5986349637688ef8ee8323eec496e61d06898a7a
tangliqun <tangliqun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-22 08:17:48 +0000

    REEB-57: 更新website/home的用户信息
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@248 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- website/home/app/controllers/UserCenter.java
+++ website/home/app/controllers/UserCenter.java
@@ -13,37 +13,33 @@ import controllers.modules.webcas.WebCAS;
 @With({SecureCAS.class, WebCAS.class})
 public class UserCenter extends Controller {
 
-	/**
-	 * 我的订单
-	 */
-	public static void index() {
-		String username = session.get("username");
-		username = "yjy";
-		User user = User.find("byLoginName", username).first();
-		List<Orders> orderList = Orders.find("byUser", user).fetch();
-		render(orderList);
-	}
+    /**
+     * 我的订单
+     */
+    public static void index() {
+        User user = WebCAS.getUser();
+        List<Orders> orderList = Orders.find("byUser", user).fetch();
+        render(orderList);
+    }
 
-	/**
-	 * 订单详情
-	 */
-	public static void details(Long id) {
-		//订单信息
-		models.order.Orders	orders= models.order.Orders.findById(id);
-		 List<OrderItems> orderItems= orders.orderItems;
-		//收货信息
-		render(orders,orderItems);
-	}
+    /**
+     * 订单详情
+     */
+    public static void details(Long id) {
+        //订单信息
+        models.order.Orders     orders= models.order.Orders.findById(id);
+        List<OrderItems> orderItems= orders.orderItems;
+        //收货信息
+        render(orders,orderItems);
+    }
 
-	/**
-	 * 订单详情
-	 */
-	public static void tickets(String createdAt,String status) {
-		String username = session.get("username");
-		username = "yjy";
-		User user = User.find("byLoginName", username).first();
-		List<Orders> ticketList = Orders.userTicketsQuery(user.id,createdAt,status);
-		render(ticketList);
-	}
+    /**
+     * 订单详情
+     */
+    public static void tickets(String createdAt,String status) {
+        User user = WebCAS.getUser();
+        List<Orders> ticketList = Orders.userTicketsQuery(user.id,createdAt,status);
+        render(ticketList);
+    }
 
 }
