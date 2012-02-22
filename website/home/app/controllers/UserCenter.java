8b6b6637fbf1597a08dac71820fa16c7855d850f
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-22 03:26:21 +0000

    reeb-54
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@220 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ website/home/app/controllers/UserCenter.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import java.util.List;
+
+import models.consumer.User;
+import models.order.OrderItems;
+import models.order.Orders;
+import play.mvc.Controller;
+
+public class UserCenter extends Controller {
+
+	/**
+	 * 我的订单
+	 */
+	public static void index() {
+		String username = session.get("username");
+		username = "yjy";
+		User user = User.find("byLoginName", username).first();
+		List<Orders> orderList = Orders.find("byUser", user).fetch();
+		render(orderList);
+	}
+
+	/**
+	 * 订单详情
+	 */
+	public static void details(Long id) {
+		//订单信息
+		models.order.Orders	orders= models.order.Orders.findById(id);
+		 List<OrderItems> orderItems= orders.orderItems;
+		//收货信息
+		render(orders,orderItems);
+	}
+
+	/**
+	 * 订单详情
+	 */
+	public static void tickets() {
+		String username = session.get("username");
+		username = "yjy";
+		User user = User.find("byLoginName", username).first();
+		List<Orders> ticketList = Orders.userTicketsQuery(user.id);
+		render(ticketList);
+	}
+
+}
