6f74a2677b8c4bdcdcb0a3c1adc268a82a6064e5
sujie <sujie@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-24 09:53:36 +0000

    REEB-81 fix test/GoodsUnitTest.java bug
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@262 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- traders/sales/test/GoodsUnitTest.java
+++ traders/sales/test/GoodsUnitTest.java
@@ -1,35 +1,36 @@
+import models.sales.Goods;
+import models.sales.GoodsStatus;
+import org.junit.Test;
+import play.test.UnitTest;
+
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-import models.sales.Goods;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-import play.test.UnitTest;
-
 
 public class GoodsUnitTest extends UnitTest {
-	@Test
-	public void add(){
-		Goods goods = new Goods();
-		//默认商品下架状态
-		goods.status="1";
-		goods.companyId ="1";
-		goods.prompt="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-		goods.details="aaaaaaaaaasssssssssssssssssss";
-		goods.imagePath ="/1/1/1/"+"111.jpg";
-		goods.deleted="0";
-		SimpleDateFormat sdf  =   new  SimpleDateFormat( "yyyy-MM-dd HH:mm:ss" );
-		String datestr = sdf.format( new  Date());  
-		goods.createdAt =datestr;
-		goods.createdBy ="yanjy";
-		goods.save();		
-		
-		List<Goods> list = Goods.findAll(); 
+    @Test
+    public void add() {
+        Goods goods = new Goods();
+        //默认商品下架状态
+        goods.status = GoodsStatus.OFFSALE;
+        goods.companyId = "1";
+        goods.prompt = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        goods.details = "aaaaaaaaaasssssssssssssssssss";
+        goods.imagePath = "/1/1/1/" + "111.jpg";
+        goods.deleted = Goods.UNDELETED;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String datestr = sdf.format(new Date());
+        goods.createdAt = datestr;
+        goods.createdBy = "yanjy";
+        goods.originalPrice = new BigDecimal(100);
+        goods.salePrice = new BigDecimal(0.01);
+        goods.save();
 
-		Assert.assertNotNull(list);  
-		Assert.assertEquals(Goods.count(),list.size());  
-	}
+        List<Goods> list = Goods.findAll();
+
+        assertNotNull(list);
+        assertEquals(Goods.count(), list.size());
+    }
 }
