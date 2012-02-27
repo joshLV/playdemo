753eb05fbfc537baf03e73bde6092cff1ed5b27e
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-27 12:32:17 +0000

    reeb-70
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@288 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- traders/sales/app/controllers/Goods.java
+++ traders/sales/app/controllers/Goods.java
@@ -4,17 +4,23 @@
  */
 package controllers;
 
-import com.uhuila.common.constants.DeletedStatus;
-import com.uhuila.common.util.PathUtil;
-import models.sales.GoodsStatus;
-import models.sales.Shop;
-import play.mvc.Controller;
-import util.FileUploadUtil;
-
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+
+import models.sales.GoodsShop;
+import models.sales.GoodsStatus;
+import models.sales.Pager;
+import models.sales.Shop;
+import play.data.validation.Required;
+import play.data.validation.Valid;
+import play.mvc.Controller;
+import util.FileUploadUtil;
+
+import com.uhuila.common.util.PathUtil;
+
+
 
 /**
  * 通用说明：
@@ -24,153 +30,179 @@ import java.util.List;
  */
 public class Goods extends Controller {
 
-    /**
-     * 展示商品一览页面
-     */
-    public static void index(models.sales.Goods goods) {
+	/**
+	 * 展示商品一览页面
+	 */
+	public static void index(models.sales.Goods goods,String page) {
+		System.out.println("index");
+		Pager pager = new Pager();
+		if(page != null){
+			pager.currPage = Integer.parseInt(page);
+		}
+		List list = goods.query(goods,pager);
+		renderTemplate("sales/Goods/index.html", list,pager);
+	}
 
-        List list = goods.query(goods);
-        renderTemplate("sales/Goods/index.html", list);
-    }
+	/**
+	 * 展示添加商品页面
+	 */
+	public static void add() {
+		String companyId="1";
+		List<Shop> list = Shop.findShopByCompany(Long.parseLong(companyId));
+		renderTemplate("sales/Goods/add.html", list);
+	}
+	/**
+	 * 展示添加商品页面
+	 */
+	public static void preview(Long id) {
+		redirect("http://www.uhuiladev.com/goods/"+id+"?preview=true");
+	}
 
-    /**
-     * 展示添加商品页面
-     */
-    public static void add() {
-        List<Shop> list = Shop.findShopByCompany(Long.parseLong("1"));
-        renderTemplate("sales/Goods/add.html", list);
-    }
+	/**
+	 * 添加商品
+	 *
+	 * @param imagePath
+	 * @param goods
+	 */
+	public static void create(@Required File imagePath, @Valid models.sales.Goods goods,String radios,String status,
+			Long checkoption[]) {
+		if (validation.hasErrors()) {
+			params.flash();
+			validation.keep();
+			renderTemplate("sales/Goods/add.html");
+		}
+		String companyId="1";
+		//添加商品处理
+		goods.status = "OFFSALE".equals(status) ? GoodsStatus.OFFSALE:GoodsStatus.ONSALE;
+		goods.companyId = companyId;
+		goods.lockVersion = 0;
 
-    /**
-     * 添加商品
-     *
-     * @param imagePath
-     * @param goods
-     */
-    public static void create(File imagePath, models.sales.Goods goods,
-                              String radios, GoodsStatus status,
-                              Long checkOptions[]) {
-        if (validation.hasErrors()) {
-            error("Validation errors");
-        }
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String datestr = sdf.format(new Date());
+		goods.deleted = models.sales.Goods.UNDELETED;
+		goods.createdAt = datestr;
+		goods.createdBy = "yanjy";
+		goods.create();
+		uploadImagePath(imagePath, goods);
+		goods.save();
+		//全部门店的场合
+		GoodsShop goods_shop = new GoodsShop();
+		if ("1".equals(radios)) {
+			List<Shop> list = Shop.findShopByCompany(Long.parseLong(companyId));
+			for (Shop shop : list) {
+				goods_shop = new GoodsShop();
+				goods_shop.shopId = shop.id;
+				goods_shop.goodsId = goods.id;
+				goods_shop.save();
+			}
+		} else {
+			//部分门店
+			for (Long id : checkoption) {
+				goods_shop = new GoodsShop();
+				goods_shop.shopId = id;
+				goods_shop.goodsId = goods.id;
+				goods_shop.create();
+			}
+		}
+		//预览的情况
+		if ("2".equals(status)) {
+			redirect("http://www.uhuiladev.com/goods/"+goods.id+"?preview=true");
+		}
+		index(null,"1");
+	}
 
-        //添加商品处理
-        goods.status = status;
-        goods.companyId = "1";
-        goods.lockVersion = 0;
+	/**
+	 * 上传图片
+	 * 
+	 * @param uploadImageFile
+	 * @param goods
+	 */
+	private static void uploadImagePath(File uploadImageFile, models.sales.Goods goods) {
+		if (uploadImageFile != null && uploadImageFile.getName() != null) {
+			//取得文件存储路径
+			String storepath = play.Play.configuration.get("upload.imagepath").toString();
+			//上传文件
+			String path = PathUtil.getPathById(goods.id);
+			String uploadImageFileName = uploadImageFile.getName();
+			String extName = play.Play.configuration.get("imageExtName").toString();;
+			if (uploadImageFileName.indexOf(".") > 0) {
+				extName = uploadImageFileName.substring(uploadImageFileName.lastIndexOf(".") + 1, uploadImageFileName.length());
+			}
+			String baseFileName = "origin." + extName;
+			new FileUploadUtil().storeImage(uploadImageFile, storepath + path, baseFileName);
+			goods.imagePath = path + baseFileName;
+		}
+	}
 
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String datestr = sdf.format(new Date());
-        goods.deleted = DeletedStatus.UN_DELETED;
-        goods.createdAt = datestr;
-        goods.createdBy = "yanjy";
-        goods.create();
-        uploadImagePath(imagePath, goods);
-        goods.save();
-        //全部门店的场合
-        if ("1".equals(radios)) {
-            List<Shop> list = Shop.findShopByCompany(Long.parseLong("1"));
-            for (Shop shop : list) {
-                goods.shops.add(shop);
-            }
-        } else {
-            //部分门店
-            for (Long id : checkOptions) {
-                Shop shop = Shop.findById(id);
-                goods.shops.add(shop);
-            }
-        }
-        goods.save();
-        index(null);
-    }
+	/**
+	 * 取得指定商品信息
+	 */
+	public static void edit(Long id) {
+		models.sales.Goods goods = models.sales.Goods.findById(id);
+		List<Shop> list = Shop.findShopByCompany(Long.parseLong(goods.companyId));
+		renderTemplate("sales/Goods/edit.html", goods, list);
+	}
 
-    private static void uploadImagePath(File uploadImageFile, models.sales.Goods goods) {
-        if (uploadImageFile != null && uploadImageFile.getName() != null) {
-            //取得文件存储路径
-            String storepath = play.Play.configuration.get("upload.imagepath").toString();
-            //上传文件
-            String path = PathUtil.getPathById(goods.id);
-            System.out.println("path=" + path);
-            System.out.println("uploadImageFile=" + uploadImageFile);
-            String uploadImageFileName = uploadImageFile.getName();
-            String extName = ".jpg";
-            if (uploadImageFileName.indexOf(".") > 0) {
-                extName = uploadImageFileName.substring(uploadImageFileName.lastIndexOf(".") + 1, uploadImageFileName.length());
-            }
-            String baseFileName = "origin." + extName;
-            FileUploadUtil.storeImage(uploadImageFile, storepath + path, baseFileName);
-            goods.imagePath = path + baseFileName;
-        }
-    }
+	/**
+	 * 取得指定商品信息
+	 */
+	public static void detail(Long id) {
+		models.sales.Goods goods = models.sales.Goods.findById(id);
+		renderTemplate("sales/Goods/detail.html", goods);
+	}
 
-    /**
-     * 取得指定商品信息
-     */
-    public static void edit(String id) {
-        models.sales.Goods goods = models.sales.Goods.findById(Long.parseLong(id));
-        List<Shop> list = Shop.findShopByCompany(Long.parseLong("1"));
-        renderTemplate("sales/Goods/edit.html", goods, list);
+	/**
+	 * 更新指定商品信息
+	 *
+	 * @param id
+	 */
+	public static void update(Long id, File uploadImageFile, models.sales.Goods goods) {
+		models.sales.Goods updateGoods = models.sales.Goods.findById(id);
 
-    }
+		uploadImagePath(uploadImageFile, updateGoods);
 
-    /**
-     * 取得指定商品信息
-     */
-    public static void detail(String id) {
-        models.sales.Goods goods = models.sales.Goods.findById(Long.parseLong(id));
-        renderTemplate("sales/Goods/detail.html", goods);
-    }
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String datestr = sdf.format(new Date());
+		updateGoods.name = goods.name;
+		updateGoods.no = goods.no;
+		updateGoods.expiredBeginOn = goods.expiredBeginOn;
+		updateGoods.expiredEndOn = goods.expiredEndOn;
+		updateGoods.originalPrice = goods.originalPrice;
+		updateGoods.salePrice = goods.salePrice;
+		updateGoods.baseSale = goods.baseSale;
+		updateGoods.prompt = goods.prompt;
+		updateGoods.details = goods.details;
+		updateGoods.updateAt = datestr;
+		updateGoods.updateBy = "ytrr";
+		updateGoods.save();
 
-    /**
-     * 更新指定商品信息
-     *
-     * @param id
-     */
-    public static void update(String id, File uploadImageFile, models.sales.Goods goods) {
-        models.sales.Goods updateGoods = models.sales.Goods.findById(Long.parseLong(id));
+		index(null,"1");
+	}
 
-        uploadImagePath(uploadImageFile, updateGoods);
+	/**
+	 * 上下架指定商品
+	 */
+	public static void updateStatus(Long checkoption[], GoodsStatus status) {
+		//更新处理
+		for (Long id : checkoption) {
+			models.sales.Goods goods = models.sales.Goods.findById(id);
+			goods.status = status;
+			goods.save();
+		}
 
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String datestr = sdf.format(new Date());
-        updateGoods.name = goods.name;
-        updateGoods.no = goods.no;
-        updateGoods.expiredBeginOn = goods.expiredBeginOn;
-        updateGoods.expiredEndOn = goods.expiredEndOn;
-        updateGoods.originalPrice = goods.originalPrice;
-        updateGoods.salePrice = goods.salePrice;
-        updateGoods.baseSale = goods.baseSale;
-        updateGoods.prompt = goods.prompt;
-        updateGoods.details = goods.details;
-        updateGoods.updatedAt = datestr;
-        updateGoods.updatedBy = "ytrr";
-        updateGoods.save();
+		index(null,"1");
+	}
 
-        index(null);
-    }
-
-    /**
-     * 上下架指定商品
-     */
-    public static void updateStatus(Long checkoption[], GoodsStatus status) {
-        //更新处理
-        for (Long id : checkoption) {
-            models.sales.Goods goods = models.sales.Goods.findById(id);
-            goods.status = status;
-            goods.save();
-        }
-
-        index(null);
-    }
-
-    /**
-     * 删除指定商品
-     */
-    public static void delete(Long checkoption[]) {
-        for (Long id : checkoption) {
-            models.sales.Goods.delete("id=?", id);
-        }
-        index(null);
-    }
+	/**
+	 * 删除指定商品
+	 */
+	public static void delete(Long checkoption[]) {
+		for (Long id : checkoption) {
+			models.sales.Goods goods =	models.sales.Goods.findById(id);
+			goods.deleted=goods.DELETED;
+			goods.save();
+		}
+		index(null,"1");
+	}
 
 }
