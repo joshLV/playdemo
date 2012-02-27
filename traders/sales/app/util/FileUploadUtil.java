eee00a88a576acc4cffdc9e7f4189e524e40384f
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-27 12:33:15 +0000

    reeb-70
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@291 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- traders/sales/app/util/FileUploadUtil.java
+++ traders/sales/app/util/FileUploadUtil.java
@@ -7,33 +7,33 @@ import java.io.IOException;
 
 public class FileUploadUtil {
 
-    /**
-     * 上传图片
-     *
-     * @param Image
-     * @param desc
-     */
-    public static void storeImage(File file, String storePath, String targetFileName) {
-        //取得文件名
-        FileInputStream in;
-        try {
-            in = new FileInputStream(file);
-            // 将该数据流写入到指定文件中
-            FileOutputStream out;
+	/**
+	 * 上传图片
+	 *
+	 * @param Image
+	 * @param desc
+	 */
+	public static void storeImage(File file, String storePath, String targetFileName) {
+		//取得文件名
+		FileInputStream in;
+		try {
+			in = new FileInputStream(file);
+			// 将该数据流写入到指定文件中
+			FileOutputStream out;
 
-            if(!(new File(storePath).isDirectory())){
-                new File(storePath).mkdirs();
-            }
-            out = new FileOutputStream(storePath + targetFileName);
-            byte[] buffer = new byte[1024]; // To hold file contents
-            int bytes_read;
-            while ((bytes_read = in.read(buffer)) != -1) {
-                out.write(buffer, 0, bytes_read);
-            }
-            in.close();
-            out.close();
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-    }
+			if(!(new File(storePath).isDirectory())){
+				new File(storePath).mkdirs();
+			}
+			out = new FileOutputStream(storePath + targetFileName);
+			byte[] buffer = new byte[1024];
+			int bytes_read;
+			while ((bytes_read = in.read(buffer)) != -1) {
+				out.write(buffer, 0, bytes_read);
+			}
+			in.close();
+			out.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
 }
