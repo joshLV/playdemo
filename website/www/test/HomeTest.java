df0cba68c7fc28e904c93e682f234475edca448b
sujie <sujie@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-27 01:50:18 +0000

    REEB-81 rename file mvn module
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@270 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- website/www/test/HomeTest.java
+++ website/www/test/HomeTest.java
@@ -1,8 +1,18 @@
+import models.consumer.Address;
+import models.sales.Goods;
+import org.junit.Before;
 import org.junit.Test;
 import play.mvc.Http.Response;
+import play.test.Fixtures;
 import play.test.FunctionalTest;
 
 public class HomeTest extends FunctionalTest {
+
+    @Before
+    public void setup() {
+        Fixtures.delete(Goods.class);
+        Fixtures.loadModels("initial-data.yml");
+    }
 
     @Test
     public void testThatIndexPageWorks() {
