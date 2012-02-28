5c74242873be5f701b684ad889589e16e6a89f32
likang <likang@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-28 11:21:24 +0000

    REEB-97 增加发送短信服务
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@308 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- module/sms/app/models/sms/SMSService.java
+++ module/sms/app/models/sms/SMSService.java
@@ -1,5 +1,6 @@
 package models.sms;
 
+import play.Play;
 import play.modules.rabbitmq.producer.RabbitMQPublisher;
 
 import java.util.List;
@@ -8,10 +9,11 @@ import java.util.List;
  * User: likang
  */
 public class SMSService {
+    private static final String queueName = Play.mode.isProd() ? "send_sms" : "send_sms_dev";
     public static void send(String content, String phoneNumber){
-        RabbitMQPublisher.publish("send_sms", new SMSMessage(content, phoneNumber));
+        RabbitMQPublisher.publish(queueName, new SMSMessage(content, phoneNumber));
     }
     public static void send(String content, List<String> phoneNumbers){
-        RabbitMQPublisher.publish("send_sms", new SMSMessage(content, phoneNumbers));
+        RabbitMQPublisher.publish(queueName, new SMSMessage(content, phoneNumbers));
     }
 }
---
+++ website/www/test/functional/CartsTest.java
@@ -0,0 +1,12 @@
+package functional;
+
+import models.sms.SMSService;
+import org.junit.Test;
+import play.test.FunctionalTest;
+
+/**
+ * User: likang
+ */
+public class CartsTest extends FunctionalTest{
+
+}
