e012ad4a823274bca61db569d977aea618820bd7
likang <likang@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-29 09:36:35 +0000

    REEB-97 将sms队列消费者分离为单独的app
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@328 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ module/sms/app/models/sms/SMSUtil.java
@@ -0,0 +1,20 @@
+package models.sms;
+
+import play.Play;
+import play.modules.rabbitmq.producer.RabbitMQPublisher;
+
+import java.util.List;
+
+/**
+ * User: likang
+ */
+public class SMSUtil {
+    private static final String QUEUE_NAME = Play.mode.isProd() ? "send_sms" : "send_sms_dev";
+    private SMSUtil(){}
+    public static void send(String content, String phoneNumber){
+        RabbitMQPublisher.publish(QUEUE_NAME, new SMSMessage(content, phoneNumber));
+    }
+    public static void send(String content, List<String> phoneNumbers){
+        RabbitMQPublisher.publish(QUEUE_NAME, new SMSMessage(content, phoneNumbers));
+    }
+}
