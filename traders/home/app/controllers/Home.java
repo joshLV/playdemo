425d156fb1fb5f363538655eb258437a4455364c
tangliqun <tangliqun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-29 08:41:55 +0000

    REEB-101 实现登录
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@321 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- traders/home/app/controllers/Home.java
+++ traders/home/app/controllers/Home.java
@@ -1,17 +1,18 @@
 package controllers;
 
+import java.util.logging.Logger;
+
 import play.mvc.Controller;
 
 public class Home extends Controller {
 
     public static void index() {
-        
-        
-        
+
+        String title = new String("");
+
+        System.out.println("lenght=" + title.length());
+
         render();
     }
-    
-    
-    
 
 }
