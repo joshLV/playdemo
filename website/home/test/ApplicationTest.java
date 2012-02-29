b5c8adf61e92dc8666314f6cca03fe20fb5a6ffd
tangliqun <tangliqun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-02-29 10:35:46 +0000

    REEB-101
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@332 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- website/home/test/ApplicationTest.java
+++ website/home/test/ApplicationTest.java
@@ -1,17 +1,55 @@
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import models.*;
+
 import org.junit.*;
-import play.test.*;
+import org.junit.Before;
+
 import play.mvc.*;
 import play.mvc.Http.*;
-import models.*;
+import play.test.*;
 
 public class ApplicationTest extends FunctionalTest {
 
+    @Before
+    public void loadData() {
+        Fixtures.deleteAllModels();
+        Fixtures.loadModels("fixture/user.yml");
+    }
+
     @Test
     public void testThatIndexPageWorks() {
-        Response response = GET("/orders");
-        assertIsOk(response);
-        assertContentType("text/html", response);
-        assertCharset(play.Play.defaultWebEncoding, response);
+        System.out.println("hello");
+        //clearCookies();
+
+        loginForTest("selenium@uhuila.com");
+        Response response1 = GET("/orders");
+        assertStatus(302, response1);
+    }
+
+    public void loginForTest(String user) {
+        clearCookies();
+        Response response0 = GET("/login");
+
+        Request request = newRequest();
+        makeRequest(request, response0);
+
+        System.out.println("user2=" + user);
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("login", user);
+        params.put("password", user);
+
+        Response response = POST(request, "/@cas/authenticate", params, new HashMap<String, File>());
+        for(String name : response.cookies.keySet()) {
+            System.out.println("name=" + name + ", value=" + response.cookies.get(name).value);
+        }
+        //assertIsOk(response);
+
+        Request request0 = newRequest();
+        makeRequest(request0, response0);
     }
 
 }
