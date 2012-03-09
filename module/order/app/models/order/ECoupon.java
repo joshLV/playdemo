a7efaba25d5c339937b68be635f6c0064da71768
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-03-09 15:51:07 +0000

    reeb-82
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@430 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- module/order/app/models/order/ECoupon.java
+++ module/order/app/models/order/ECoupon.java
@@ -1,14 +1,33 @@
 package models.order;
 
-import models.sales.Goods;
-import play.db.jpa.Model;
-
-import javax.persistence.*;
-
 import java.math.BigDecimal;
-import java.text.DecimalFormat;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Query;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import models.accounts.Account;
+import models.accounts.TradeBill;
+import models.accounts.util.TradeUtil;
+import models.sales.Goods;
+
+import org.apache.commons.lang.StringUtils;
+
+import play.data.validation.Required;
+import play.db.jpa.Model;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,8 +39,6 @@ import java.util.*;
 @Entity
 @Table(name = "e_coupon")
 public class ECoupon extends Model {
-    private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddhhmmssSSS");
-    private static DecimalFormat decimalFormat = new DecimalFormat("00000");
 
     @ManyToOne(fetch= FetchType.LAZY)
     @JoinColumn(name="order_id",nullable=true)
@@ -30,11 +47,15 @@ public class ECoupon extends Model {
     @ManyToOne
     Goods goods;
 
+    @Required
     @Column(name="e_coupon_sn")
     public String eCouponSn;
 
     @Column(name="e_coupon_price")
     public BigDecimal eCouponPrice;
+    
+    @Column(name="income_price")
+    public BigDecimal incomePrice;
 
     @Column(name="refund_price")
     public BigDecimal refundPrice;
@@ -49,35 +70,19 @@ public class ECoupon extends Model {
     @Column(name="refund_at")
     public Date refundAt;
     
+    @Column(name="buy_number")
+    public int buyNumber;
+    
     @Enumerated(EnumType.STRING)
     public ECouponStatus status;
 
-    public ECoupon(Orders order, Goods goods, BigDecimal eCouponPrice){
-        this.order = order;
-        this.goods = goods;
-        this.eCouponPrice = eCouponPrice;
-        this.refundPrice = eCouponPrice;
-        this.createdAt = new Date();
-
-        this.consumedAt = null;
-        this.refundAt = null;
-        this.status = ECouponStatus.UNCONSUMED;
-        this.eCouponSn = generateSerialNumber();
-    }
-
-    private String generateSerialNumber() {
-        int random = new Random().nextInt() % 10000;
-        return dateFormat.format(new Date()) + decimalFormat.format(random);
-
-    }
-
-    /**
-     * 根据页面录入券号查询对应信息
-     * 
-     * @param eCouponSn 券号
-     * @return
-     */
-	public static Map<String,Object> query(String eCouponSn,Long companyId) {
+	/**
+	 * 根据页面录入券号查询对应信息
+	 * 
+	 * @param eCouponSn 券号
+	 * @return
+	 */
+	public static ECoupon query(String eCouponSn,Long companyId) {
 		EntityManager entityManager = play.db.jpa.JPA.em();
 		StringBuilder sql = new StringBuilder();
 		Map<String, Object> params = new HashMap<String, Object>();
@@ -86,22 +91,65 @@ public class ECoupon extends Model {
 			sql.append(" and e.goods.companyId = :companyId");
 			params.put("companyId", companyId);
 		}
+		if (StringUtils.isNotBlank(eCouponSn)) {
+			sql.append(" and e.eCouponSn = :eCouponSn");
+			params.put("eCouponSn", eCouponSn);
+		}
 		Query couponQery = entityManager.createQuery(sql.toString());
 		for(Map.Entry<String,Object> entry : params.entrySet()){
 			couponQery.setParameter(entry.getKey(), entry.getValue());
 		}
 		sql.append(" order by e.createdAt desc");
-		
-		Map<String,Object> queryMap = new HashMap();
+
 		List<ECoupon> queryList = couponQery.getResultList();
-		for (ECoupon e:queryList){
-			queryMap.put("name", e.goods.name);
-			queryMap.put("expireAt", e.goods.expireAt);
-			queryMap.put("status", e.status);
+		if (queryList.size() ==0) {
+			return null;
 		}
 		
+		return queryList.get(0);
+	}
+
+	/**
+	 * 根据页面录入券号查询对应信息
+	 * 
+	 * @param eCouponSn 券号
+	 * @return
+	 */
+	public static Map<String,Object> queryInfo(String eCouponSn,Long companyId) {
+		ECoupon eCoupon = query(eCouponSn,companyId);
+		Map<String,Object> queryMap = new HashMap();
+		if (eCoupon != null) {
+			java.text.DateFormat df = new java.text.SimpleDateFormat(
+					"yyyy-MM-dd HH:mm:ss");
+			queryMap.put("name", eCoupon.goods.name);
+			queryMap.put("expireAt", eCoupon.goods.expireAt != null? df.format(eCoupon.goods.expireAt):null);
+			queryMap.put("consumedAt",eCoupon.consumedAt != null ? df.format(eCoupon.consumedAt):null);
+			queryMap.put("eCouponSn", eCoupon.eCouponSn);
+			queryMap.put("refundAt", eCoupon.refundAt != null? df.format(eCoupon.refundAt):null);
+			queryMap.put("status", eCoupon.status);
+			queryMap.put("error",0);
+		}
+
 		return queryMap;
 	}
-    
+
+
+	/**
+	 * 修改券状态,并产生消费交易记录
+	 * 
+	 * @param eCouponSn
+	 * @param companyId
+	 */
+	public static void update(String eCouponSn, Long companyId) {
+		ECoupon eCoupon = query(eCouponSn,companyId);
+		eCoupon.status= ECouponStatus.CONSUMED;
+		eCoupon.save();
+		
+		//产生消费记录
+		Account account = Account.findById(companyId);
+		TradeBill tradeBill = TradeUtil.createConsumeTrade(eCouponSn, account, eCoupon.incomePrice,eCoupon.order.id);
+		TradeUtil.success(tradeBill);
+	}
+
     
 }
