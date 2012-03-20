3cc76f72b6368a36cc5f1ed054f8f948c30e4678
sujie <sujie@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-03-20 05:18:42 +0000

    REEB-122 add Consumers
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@546 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ website/www/app/controllers/Consumers.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * TODO.
+ * <p/>
+ * User: sujie
+ * Date: 3/20/12
+ * Time: 1:11 PM
+ */
+public class Consumers extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+}
