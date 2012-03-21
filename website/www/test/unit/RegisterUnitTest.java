2a85925b51bb119130812b44debc549638253514
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-03-21 01:52:14 +0000

    reeb-139
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@553 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- website/www/test/unit/RegisterUnitTest.java
+++ website/www/test/unit/RegisterUnitTest.java
@@ -3,38 +3,47 @@ package unit;
 import java.util.Date;
 import java.util.List;
 
+import models.consumer.Address;
 import models.consumer.User;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import play.libs.Images;
+import play.test.Fixtures;
 import play.test.UnitTest;
 
 public class RegisterUnitTest extends UnitTest {
+	@Before
+	public void setup() {
+		Fixtures.delete(User.class);
+	}
+
 	@Test
 	public void registersTest() { 
-		User user= new User();
-		user.mobile=	"13000000000";
-		user.loginName="admin@qq.com";
+		User user = new User();
+		user.mobile = "13000000001";
+		user.loginName = "11@qq.com";
 		Images.Captcha captcha = Images.captcha();
-		String passwordSalt=captcha.getText(6);
+		String passwordSalt = captcha.getText(6);
 		//密码加密
-		user.password=DigestUtils.md5Hex("1"+passwordSalt);
+		user.password = DigestUtils.md5Hex("1"+passwordSalt);
+		user.confirmPassword = "1";
 		//正常
-		user.status=1;
-		//随机吗
-		user.passwordSalt=passwordSalt;
-		user.loginIp="127.0.0.1";
+		user.status = 1;
+		//随机码
+		user.passwordSalt = passwordSalt;
+		user.captcha = "awwr";
+		user.loginIp = "127.0.0.1";
 		user.lastLoginAt = new Date();
 		user.createdAt = new Date();
 		user.save();
 
 		List<User> list = User.findAll(); 
-
 		Assert.assertNotNull(list);  
 		Assert.assertTrue(list.size() !=0);  
-		Assert.assertEquals("admin@qq.com", list.get(0).loginName);  
+		Assert.assertEquals("11@qq.com", list.get(0).loginName);  
 	}  
 }
