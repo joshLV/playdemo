7e8e18cced3df10ca64fbd11573fa9995c5b4afc
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-03-21 03:13:52 +0000

    reeb-139
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@561 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- resale/home/app/controllers/Register.java
+++ resale/home/app/controllers/Register.java
@@ -1,9 +1,18 @@
 package controllers;
 
+import java.util.Date;
+
+import models.resale.Resale;
+import models.resale.ResaleStatus;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang.StringUtils;
+
+import play.cache.Cache;
+import play.data.validation.Valid;
+import play.data.validation.Validation;
+import play.libs.Images;
 import play.mvc.Controller;
-import play.mvc.With;
-import controllers.modules.cas.SecureCAS;
-import controllers.modules.webcas.WebCAS;
 /**
  * 分销商注册
  * 
@@ -18,8 +27,36 @@ public class Register extends Controller {
 	public static void index(){
 		render();
 	}
-	
-	public static void create(){
-		render();
+
+	/**
+	 *  注册新用户
+	 * 
+	 * @param resaler 用户信息
+	 */
+	public static void create(@Valid Resale resaler) {
+		if(!resaler.password.equals(resaler.confirmPassword)){
+			Validation.addError("resaler.confirmPassword", "两次密码输入的不一样！！");
+		}
+		if (Validation.hasErrors()) {
+			render("Register/index.html", resaler);
+		}
+
+		Images.Captcha captcha = Images.captcha();
+		String passwordSalt=captcha.getText(6);
+		//密码加密
+		resaler.password=DigestUtils.md5Hex(resaler.password+passwordSalt);
+		//正常
+		resaler.status=ResaleStatus.NOMAL;
+		//随机吗
+		resaler.passwordSalt=passwordSalt;
+		//获得IP
+		resaler.loginIp=request.current().remoteAddress;
+		resaler.lastLoginAt = new Date();
+		resaler.createdAt = new Date();
+		resaler.save();
+
+		redirect("http://www.uhuiladev.com");
 	}
+
+
 }
