3682fdde878017ed7d7ce0337e7a048aaa1c6d56
sujie <sujie@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-03-23 14:22:04 +0000

    REEB-130 fix bug
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@638 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ traders/sales/public/javascripts/goods-form.js
@@ -0,0 +1,47 @@
+/**
+ * To Goods/add.html.
+ *
+ * User: sujie
+ * Date: 3/9/12
+ * Time: 7:00 PM
+ */
+$(window).load(
+    function () {
+        $("#goods_topCategoryId").change(function () {
+            $("#goods_categories_id").load("/category/sub/" + $("#goods_topCategoryId").val(), function (data) {
+                var categoryList = $.parseJSON(data);
+                $("#goods_categories_id").empty();
+                $.each(categoryList, function (i, category) {
+                    $("#goods_categories_id").append("<option value='" + category.id + "'>" + category.name + "</option>");
+                });
+            });
+        });
+
+        $("#onsales").click(function () {
+                if ($("#baseSale").val() > 0) {
+                    $("#status").val("ONSALE");
+                } else {
+                    $("#errorBaseSale").text("上架商品的库存不能为0！");
+                    return false;
+                }
+            }
+        )
+        ;
+        $("#goods_isAllShop_2").click(function () {
+            $("#shop").show();//显示门店列表
+        });
+        $("#goods_isAllShop_1").click(function () {
+            $("#shop").hide();//隐藏门店列表
+        });
+        $("#save").click(function () {
+            $("#status").val("OFFSALE");
+        });
+        $("#onsales").click(function () {
+            alert("onsales");
+            $("#status").val("ONSALE");
+        });
+        $("#preview").click(function () {
+            $("#status").val("UNCREATED");
+        });
+    }
+);
