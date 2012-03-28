d636d856a5c18e964b93a0cb45c23945e7aef341
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-03-28 09:20:30 +0000

    reeb-128 修改分销商注册
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@697 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ resale/home/app/controllers/ResalerRegister.java
@@ -0,0 +1,73 @@
+package controllers;
+
+import java.util.Date;
+
+import models.resale.Resaler;
+import models.resale.ResalerStatus;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import play.data.validation.Valid;
+import play.data.validation.Validation;
+import play.libs.Images;
+import play.mvc.Controller;
+import play.mvc.With;
+import controllers.resaletrace.ResaleCAS;
+/**
+ * 分销商注册
+ * 
+ * @author yanjy
+ *
+ */
+public class ResalerRegister extends Controller {
+
+	/**
+	 * 注册页面
+	 */
+	public static void index(){
+		render();
+	}
+
+	/**
+	 *  注册新用户
+	 * 
+	 * @param resaler 用户信息
+	 */
+	public static void create(@Valid Resaler resaler) {
+		if(!resaler.password.equals(resaler.confirmPassword)){
+			Validation.addError("resaler.confirmPassword", "两次密码输入的不一样！！");
+		}
+		if (Validation.hasErrors()) {
+			render("ResalerRegister/index.html", resaler);
+		}
+
+		Images.Captcha captcha = Images.captcha();
+		String passwordSalt=captcha.getText(6);
+		//密码加密
+		resaler.password=DigestUtils.md5Hex(resaler.password+passwordSalt);
+		//正常
+		resaler.status=ResalerStatus.PENDING;
+		//随机吗
+		resaler.passwordSalt=passwordSalt;
+		//获得IP
+		resaler.loginIp=request.current().remoteAddress;
+		resaler.lastLoginAt = new Date();
+		resaler.createdAt = new Date();
+		resaler.save();
+
+		redirect("http://www.114bsgo.com");
+	}
+
+	/**
+	 * 判断用户名和手机是否唯一
+	 * 
+	 * @param loginName
+	 *            用户名
+	 * @param mobile
+	 *            手机
+	 */
+	public static void checkLoginName(String loginName, String mobile) {
+		String returnFlag = Resaler.checkValue(loginName, mobile);
+		renderJSON(returnFlag);
+	}
+}
