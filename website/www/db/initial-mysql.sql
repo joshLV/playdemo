13dc06c09ed33d0c1813ab31ae94d0ce85934690
sujie <sujie@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-04-10 03:01:59 +0000

    REEB-238 fix initial-mysql.sql
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@860 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- website/www/db/initial-mysql.sql
+++ website/www/db/initial-mysql.sql
@@ -33,6 +33,8 @@ insert into brands (name,logo,display_order)
   values('豆捞坊', null, 200);
 
 -- Table operate_users Initial --
-insert into operate_users(id,created_at,deleted,encrypted_password,last_login_at,last_login_ip,lock_version,
+insert into operate_users(created_at,deleted,encrypted_password,lock_version,
 login_name,mobile,password_salt,user_name)
-values();
+values(now(), 'UNDELETED','1360dceeb96ae52402fb21f954a8268d',0,'admin',13910001000,'mSKHrC','小唐');
+insert into operate_users_roles(role_id,user_id)
+values(1,1);
