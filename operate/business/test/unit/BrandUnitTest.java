3a102269c1eff1fdbe0030c7e7fcf3e729cb3909
sujie <sujie@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-04-18 10:30:15 +0000

    REEB-287 add brand management to business
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@949 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- operate/business/test/unit/BrandUnitTest.java
+++ operate/business/test/unit/BrandUnitTest.java
@@ -49,7 +49,7 @@ public class BrandUnitTest extends UnitTest {
     @Test
     public void testGetBrandPage() {
         long supplierId = (Long) Fixtures.idCache.get("models.supplier.Supplier-Supplier1");
-        ModelPaginator brandPage = Brand.getBrandPage(1, 15, supplierId);
+        ModelPaginator brandPage = Brand.getBrandPage(1, 15, 1L);
         assertEquals(3, brandPage.size());
         Brand firstBrand = (Brand) brandPage.get(0);
         assertEquals("来一份", firstBrand.name);
