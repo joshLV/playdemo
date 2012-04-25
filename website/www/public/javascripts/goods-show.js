60d59140daf56762a157f491570ee03f9f75abbe
sujie <sujie@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-04-25 07:38:41 +0000

    REEB-309 show vip added price
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1044 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ website/www/public/javascripts/goods-show.js
@@ -0,0 +1,40 @@
+/**
+ * For goods/show.html.
+ * User: sujie
+ * Date: 2/20/12
+ * Time: 11:28 AM
+ */
+$(window).load(
+		
+    function () {
+        /**
+         *点击加入分销库按钮
+         */
+        $("#link_add_cart").click(function () {
+            $.post("/library", 
+                {'goodsIds':$("#goodsId").val()},
+                function (data) {
+                    if (data.isExist ==1) {
+                      $('#add_cart_result1').show();
+                	//5秒后自动消失
+                      setTimeout("$('#add_cart_result1').css('display','none')", 5000);
+                    } else {
+                        $('#add_cart_result').show();
+                        //5秒后自动消失
+                        setTimeout("$('#add_cart_result').css('display','none')", 5000);
+                    }
+                   
+                });
+        });
+
+        $("#link_buy_more").click(function () {
+            $('#add_cart_result').hide();
+        });
+
+        $("#link_buy_now").click(function () {
+            $('#buy_now_form').submit();
+        });
+
+    }
+);
+
