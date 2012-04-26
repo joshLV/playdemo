8e95601b5072ed89b0627fd7b8b178c800a8fa41
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-04-26 09:21:06 +0000

    reeb-358
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1074 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- utils/tools/src/main/java/com/uhuila/common/util/DateUtil.java
+++ utils/tools/src/main/java/com/uhuila/common/util/DateUtil.java
@@ -1,5 +1,6 @@
 package com.uhuila.common.util;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -11,17 +12,36 @@ import java.util.Date;
  * Time: 3:57 PM
  */
 public class DateUtil {
-
-    public static Date getEndOfDay(Date day) {
-        if (day == null){
-            return null;
-        }
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(day);
-        calendar.set(Calendar.HOUR_OF_DAY,23);
-        calendar.set(Calendar.MINUTE,59);
-        calendar.set(Calendar.SECOND,59);
-        calendar.set(Calendar.MILLISECOND,999);
-        return calendar.getTime();
-    }
+	public static SimpleDateFormat df = new SimpleDateFormat( "HH:mm:ss" );
+	public static Date getEndOfDay(Date day) {
+		if (day == null){
+			return null;
+		}
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(day);
+		calendar.set(Calendar.HOUR_OF_DAY,23);
+		calendar.set(Calendar.MINUTE,59);
+		calendar.set(Calendar.SECOND,59);
+		calendar.set(Calendar.MILLISECOND,999);
+		return calendar.getTime();
+	}
+	public static void main(String[] args){
+			System.out.println(getTimeRegion());
+		
+		
+	}
+	/**
+	 * 判断当日的时间是否在11点和14点之间
+	 * 
+	 * @return 在该范围内：true 
+	 */
+	public static boolean getTimeRegion() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(new Date());
+		String date= df.format(calendar.getTime());
+		if (date.compareTo("09:00:00")>0 && date.compareTo("18:00:00")<0) {
+			return true;
+		}
+		return false;
+	}
 }
