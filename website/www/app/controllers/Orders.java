634a83e48ad68f84dcd2a216cf8fab5af2569cc0
sujie <sujie@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-04-27 10:59:04 +0000

    REEB-250 fix bug
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1108 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- website/www/app/controllers/Orders.java
+++ website/www/app/controllers/Orders.java
@@ -1,26 +1,27 @@
 package controllers;
 
-import static play.Logger.warn;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import controllers.modules.website.cas.SecureCAS;
 import models.accounts.AccountType;
 import models.consumer.Address;
 import models.consumer.User;
 import models.order.Cart;
 import models.order.NotEnoughInventoryException;
 import models.order.Order;
+import models.sales.Goods;
+import models.sales.MaterialType;
 import play.Logger;
-import play.data.validation.Required;
-import play.data.validation.Valid;
 import play.data.validation.Validation;
-import play.modules.view_ext.annotation.Mobile;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.With;
-import controllers.modules.website.cas.SecureCAS;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static play.Logger.warn;
 
 /**
  * 用户订单确认控制器.
@@ -33,10 +34,11 @@ import controllers.modules.website.cas.SecureCAS;
 public class Orders extends Controller {
     /**
      * 预览订单.
+     *
      * @param items 选择的商品及数量，格式为 goods1-num1,goods2-num2-.....
      */
     public static void index(String items) {
-        if(items == null){
+        if (items == null) {
             error("no goods specified");
             return;
         }
@@ -47,7 +49,7 @@ public class Orders extends Controller {
     private static void showOrder(String items) {
         //解析提交的商品及数量
         List<Long> goodsIds = new ArrayList<>();
-        Map<Long,Integer> itemsMap = new HashMap<>();
+        Map<Long, Integer> itemsMap = new HashMap<>();
         parseItems(items, goodsIds, itemsMap);
 
         //计算电子商品列表和非电子商品列表
@@ -57,18 +59,18 @@ public class Orders extends Controller {
         BigDecimal rCartAmount = BigDecimal.ZERO;
 
         List<models.sales.Goods> goods = models.sales.Goods.findInIdList(goodsIds);
-        for (models.sales.Goods g : goods){
+        for (models.sales.Goods g : goods) {
             Integer number = itemsMap.get(g.getId());
-            if(g.materialType == models.sales.MaterialType.REAL){
+            if (g.materialType == models.sales.MaterialType.REAL) {
                 rCartList.add(new Cart(g, number));
                 rCartAmount = rCartAmount.add(g.salePrice.multiply(new BigDecimal(number.toString())));
-            }else if(g.materialType == models.sales.MaterialType.ELECTRONIC){
+            } else if (g.materialType == models.sales.MaterialType.ELECTRONIC) {
                 eCartList.add(new Cart(g, number));
                 eCartAmount = eCartAmount.add(g.salePrice.multiply(new BigDecimal(number.toString())));
             }
         }
 
-        if(rCartList.size() == 0 && eCartList.size() == 0){
+        if (rCartList.size() == 0 && eCartList.size() == 0) {
             error("no goods specified");
             return;
         }
@@ -77,8 +79,8 @@ public class Orders extends Controller {
 
         //如果有实物商品，加上运费
         if (rCartList.size() > 0) {
-           rCartAmount = rCartAmount.add(new BigDecimal("5"));
-       }
+            rCartAmount = rCartAmount.add(new BigDecimal("5"));
+        }
         BigDecimal totalAmount = eCartAmount.add(rCartAmount);
         BigDecimal goodsAmount = rCartList.size() == 0 ? eCartAmount : totalAmount.subtract(new BigDecimal("5"));
 
@@ -95,61 +97,78 @@ public class Orders extends Controller {
     /**
      * 提交订单.
      */
-    public static void create(String items, @Valid @Required @Mobile String mobile){
-        if (Validation.hasErrors()){
-            for (String key : validation.errorsMap().keySet()) {
-                warn("validation.errorsMap().get(key):" + validation.errorsMap().get(key));
-            }
-
-            showOrder(items);
-            render("Orders/index.html");
-        }
-        Http.Cookie cookie= request.cookies.get("identity");
+    public static void create(String items, String mobile) {
+        //如果订单中有电子券，则必须填写手机号
+        Http.Cookie cookie = request.cookies.get("identity");
         String cookieValue = cookie == null ? null : cookie.value;
         User user = SecureCAS.getUser();
 
         //解析提交的商品及数量
         List<Long> goodsIds = new ArrayList<>();
-        Map<Long,Integer> itemsMap = new HashMap<>();
+        Map<Long, Integer> itemsMap = new HashMap<>();
         parseItems(items, goodsIds, itemsMap);
+        List<models.sales.Goods> goods = models.sales.Goods.findInIdList(goodsIds);
+        boolean containsElectronic = false;
+        for (Goods good : goods) {
+            if (MaterialType.ELECTRONIC.equals(good.materialType)) {
+                containsElectronic = true;
+                break;
+            }
+        }
+
+        System.out.println("containsElectronic:" + containsElectronic);
+
+
+        if (containsElectronic) {
+            Validation.required("mobile", mobile);
+            Validation.match("mobile", mobile, "^1[3|4|5|8][0-9]\\d{4,8}$");
+        }
+
+        if (Validation.hasErrors()) {
+            for (String key : validation.errorsMap().keySet()) {
+                warn("validation.errorsMap().get(" + key + "):" + validation.errorsMap().get(key));
+            }
+
+            showOrder(items);
+            render("Orders/index.html");
+        }
 
         //创建订单
         Order order = new Order(user.getId(), AccountType.CONSUMER);
         Address defaultAddress = Address.findDefault(user);
         order.setAddress(defaultAddress);
 
-        List<models.sales.Goods> goods = models.sales.Goods.findInIdList(goodsIds);
         //添加订单条目
-        try{
-            for(models.sales.Goods goodsItem : goods){
+        try {
+            for (models.sales.Goods goodsItem : goods) {
                 order.addOrderItem(goodsItem, itemsMap.get(goodsItem.getId()), mobile,
                         goodsItem.salePrice, //最终成交价
                         goodsItem.getResalerPriceOfUhuila()//优惠啦作为分销商的成本价
                 );
             }
-        }catch (NotEnoughInventoryException e){
+        } catch (NotEnoughInventoryException e) {
             //todo 缺少库存
             Logger.error(e, "inventory not enough");
             error("inventory not enough");
         }
-        
+
         //确认订单
         order.receiverMobile = mobile;
         order.receiverPhone = mobile;
         order.createAndUpdateInventory();
         //删除购物车中相应物品
         Cart.delete(user, cookieValue, goodsIds);
-        
+
         redirect("/payment_info/" + order.getId());
     }
-    
-    private static void parseItems(String items, List<Long> goodsIds, Map<Long, Integer> itemsMap){
+
+    private static void parseItems(String items, List<Long> goodsIds, Map<Long, Integer> itemsMap) {
         String[] itemSplits = items.split(",");
-        for(String split : itemSplits){
+        for (String split : itemSplits) {
             String[] goodsItem = split.split("-");
-            if(goodsItem.length == 2){
+            if (goodsItem.length == 2) {
                 Integer number = Integer.parseInt(goodsItem[1]);
-                if(number > 0){
+                if (number > 0) {
                     Long goodsId = Long.parseLong(goodsItem[0]);
                     goodsIds.add(goodsId);
                     itemsMap.put(goodsId, number);
