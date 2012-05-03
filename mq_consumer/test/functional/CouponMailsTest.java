dc896b15f84d12ec688e0ca4d410fcf056b53f3b
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-05-03 02:00:47 +0000

    reeb-376
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1125 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- mq_consumer/test/functional/CouponMailsTest.java
+++ mq_consumer/test/functional/CouponMailsTest.java
@@ -12,7 +12,8 @@ public class CouponMailsTest extends FunctionalTest {
 
     @Test
     public void testDirectCouponMailSend() {
-        String email = "test@uhuila.com";
+    	Long time = System.currentTimeMillis();
+    	String email = "test"+time+"@uhuila.com";
         String fullName = "张三";
         String coupon1 = "0280ec55a4fb4dde3aef27ee287d61c9";
         String coupon2 = "2720737f2d56c6f50528fc3307d5fa91";
@@ -36,7 +37,8 @@ public class CouponMailsTest extends FunctionalTest {
     
     @Test
     public void testCouponMailSendByMQ() throws Exception {
-        String email = "test3@uhuila.com";
+    	Long time = System.currentTimeMillis();
+        String email = "test"+time+"@uhuila.com";
         String fullName = "李四";
         String coupon1 = "9780ec55a4fb4ddeabef27ee287d61c9";
         String coupon2 = "0420737f2d56c1150528fc3307d5fa91";
