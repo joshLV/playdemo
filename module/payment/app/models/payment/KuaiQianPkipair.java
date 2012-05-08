65f92d0d9f2d77d3642ca730559710fdcae04f9b
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-05-08 08:48:46 +0000

    reeb-378
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1189 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ module/payment/app/models/payment/KuaiQianPkipair.java
@@ -0,0 +1,68 @@
+package models.payment;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+
+public class KuaiQianPkipair {
+	
+	
+	public  String signMsg( String signMsg) {
+
+		String base64 = "";
+		try {
+			// 密钥仓库
+			KeyStore ks = KeyStore.getInstance("PKCS12");
+
+			// 读取密钥仓库（相对路径）
+			FileInputStream ksfis = new FileInputStream("/opt/99bill/tester-rsa.pfx");
+			BufferedInputStream ksbufin = new BufferedInputStream(ksfis);
+
+			char[] keyPwd = "yu@uhuila.seewi".toCharArray();
+			ks.load(ksbufin, keyPwd);
+			// 从密钥仓库得到私钥
+			PrivateKey priK = (PrivateKey) ks.getKey("test-alias", keyPwd);
+			Signature signature = Signature.getInstance("SHA1withRSA");
+			signature.initSign(priK);
+			signature.update(signMsg.getBytes("utf-8"));
+			sun.misc.BASE64Encoder encoder = new sun.misc.BASE64Encoder();
+			base64 = encoder.encode(signature.sign());
+			
+		} catch(FileNotFoundException e){
+			System.out.println("文件找不到");
+		}catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return base64;
+	}
+	public  boolean enCodeByCer( String val, String msg) {
+		boolean flag = false;
+		try {
+			InputStream inStream = new FileInputStream("/opt/99bill/99bill.cert.rsa.20140728.cer");
+			CertificateFactory cf = CertificateFactory.getInstance("X.509");
+			X509Certificate cert = (X509Certificate) cf.generateCertificate(inStream);
+			//获得公钥
+			PublicKey pk = cert.getPublicKey();
+			//签名
+			Signature signature = Signature.getInstance("SHA1withRSA");
+			signature.initVerify(pk);
+			signature.update(val.getBytes());
+			//解码
+			sun.misc.BASE64Decoder decoder = new sun.misc.BASE64Decoder();
+			flag = signature.verify(decoder.decodeBuffer(msg));
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("no");
+		} 
+		return flag;
+	}
+}
+
