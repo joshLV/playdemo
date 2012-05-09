167ea2351e44f62118ebc42bae6edb7a7028cc33
tangliqun <tangliqun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-05-09 13:19:34 +0000

    REEB-404
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1224 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ traders/sms/test/BasicUtilsTest.java
@@ -0,0 +1,12 @@
+
+
+import org.junit.Test;
+import play.test.UnitTest;
+
+public class BasicUtilsTest extends UnitTest {
+
+    @Test
+    public void testGetDomainName(){
+        assertTrue(true);
+    }
+}
