483818e2a5aa005562daa68b9de2ec6accb4ebd9
sujie <sujie@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-05-10 08:36:22 +0000

    REEB-430 update website-home pages of www
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1256 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- cdn/assets/public/javascripts/u/base.js
+++ cdn/assets/public/javascripts/u/base.js
@@ -12,8 +12,11 @@ $(function () {
     // 购物车
     $('.carts').mouseover(function () {
         //从服务端加载购物车
-        $("#cart-list").load("/carts/tops", function (data) {
-        });
+        if ($("#reload").val() == "true") {
+            $("#cart-list").load("/carts/tops", function (data) {
+                $("#reload").val("false");
+            });
+        }
         $(this).addClass('carts-on');
         $('.carts-bd').show();
     });
@@ -22,17 +25,23 @@ $(function () {
         $('.carts-bd').hide();
     });
     //购物车记录删除
-    $(".cart-delete").live('click',function (ev) {
+    $(".cart-delete").live('click', function (ev) {
         ev.preventDefault();
+        var buyCount = parseInt($(this).attr("count"));
         var goods_id = $(this).attr("goods_id");
         $.ajax({
             type:'DELETE',
             url:'/carts/' + goods_id,
             success:function (data) {
-                $("#row_" + goods_id).remove();
                 var oldSize = parseInt($("#carts_size").html());
-                var buyCount = parseInt($(this).attr("count"));
-                $("#carts_size").html(oldSize - buyCount);
+                var currentCount = oldSize - buyCount;
+                $("#carts_size").html(currentCount);
+                if (currentCount == 0) {
+                    $("#row_" + goods_id).html('您的购物车中没有任何商品。<br/>');
+                    $("#order_confirm").hide();
+                } else {
+                    $("#row_" + goods_id).remove();
+                }
             }});
     });
 
