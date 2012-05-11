39a1403cc70fcd2554150b00cd87c9bcfb1eb409
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-05-11 07:31:31 +0000

    reeb-417
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1287 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- module/consumer/app/models/consumer/UserInfo.java
+++ module/consumer/app/models/consumer/UserInfo.java
@@ -16,112 +16,115 @@ import java.util.List;
 @Entity
 @Table(name = "users_info")
 public class UserInfo extends Model {
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id")
-    public User user;
+	public User user;
 
-    @Column(name = "full_name")
-    public String fullName;
+	@Column(name = "full_name")
+	public String fullName;
 
-    @Mobile
-    public String mobile;
+	@Mobile
+	public String mobile;
 
-    /**
-     * 性别
-     */
-    @Column(name = "user_sex")
-    public int userSex;
+	/**
+	 * 性别
+	 */
+	@Column(name = "user_sex")
+	public int userSex;
 
-    /*出生年月*/
-    public String birthday;
-    /*电话*/
-    public String phone;
+	/*出生年月*/
+	public String birthday;
+	/*电话*/
+	public String phone;
 
-    /*婚姻状况*/
-    public int marryState;
-    /*职位*/
-    public String position;
-    /*qq*/
-    public String userqq;
-    /*薪水*/
-    public int salary;
-    /*职位*/
-    public String interest;
-    /*行业*/
-    public String industry;
-    /*行业*/
-    public String otherInfo;
-    @Column(name = "created_at")
-    public Date createdAt;
+	/*婚姻状况*/
+	@Column(name = "marry_state")
+	public int marryState;
+	/*职位*/
+	public String position;
+	/*qq*/
+	public String userqq;
+	/*薪水*/
+	@Column(name = "salary")
+	public int salary;
+	/*职位*/
+	public String interest;
+	/*行业*/
+	public String industry;
+	/*行业*/
+	public String otherInfo;
+	@Column(name = "created_at")
+	public Date createdAt;
 
-    @Column(name = "bindMobile_at")
-    public Date bindMobileAt;
-    
-    @Column(name = "total_points")
-    public int totalPoints;
-    
+	@Column(name = "bindMobile_at",nullable=true)
+	public Date bindMobileAt;
 
-    public UserInfo(User user) {
-        this.user = user;
-        this.createdAt = new Date();
-    }
+	@Column(name = "total_points",nullable=true)
+	public int totalPoints;
 
-    public UserInfo() {
-    }
 
-    /**
-     * 更新用户信息
-     *
-     * @param userInfo 用户信息
-     */
-    public void update(UserInfo userInfo, String interest) {
-        fullName = userInfo.fullName;
-        salary = userInfo.salary;
-        position = userInfo.position;
-        phone = userInfo.phone;
-        userqq = userInfo.userqq;
-        userSex = userInfo.userSex;
-        industry = userInfo.industry;
-        birthday = userInfo.birthday;
-        this.interest = interest;
-        marryState = userInfo.marryState;
-        otherInfo = userInfo.otherInfo;
-        createdAt = new Date();
-        this.save();
+	public UserInfo(User user) {
+		this.user = user;
+		this.marryState = 0;
+		this.userSex = 0;
+		this.createdAt = new Date();
+	}
 
-    }
+	public UserInfo() {}
 
-    /**
-     * 更新手机
-     *
-     * @param user     用户
-     * @param mobile 手机
-     */
-    public static void updateById(User user, String mobile) {
-        List<UserInfo> userInfos = UserInfo.find("byUser", user).fetch();
-        UserInfo userInfo = null;
-        if (userInfos.size() > 0) {
-            userInfo = userInfos.get(0);
-            userInfo.mobile = mobile;
-            userInfo.bindMobileAt = new Date();
-            userInfo.save();
-        }
-    }
+	/**
+	 * 更新用户信息
+	 *
+	 * @param userInfo 用户信息
+	 */
+	public void update(UserInfo userInfo, String interest) {
+		fullName = userInfo.fullName;
+		salary = userInfo.salary;
+		position = userInfo.position;
+		phone = userInfo.phone;
+		userqq = userInfo.userqq;
+		userSex = userInfo.userSex;
+		industry = userInfo.industry;
+		birthday = userInfo.birthday;
+		this.interest = interest;
+		marryState = userInfo.marryState;
+		otherInfo = userInfo.otherInfo;
+		createdAt = new Date();
+		this.save();
 
-    /**
-     * 查询用户基本信息
-     *
-     * @param user 用户ID信息
-     * @return 用户基本信息
-     */
-    public static UserInfo findByUser(User user) {
-        List<UserInfo> userInfos = UserInfo.find("byUser", user).fetch();
-        UserInfo userInfo = null;
-        if (userInfos.size() > 0) {
-            userInfo = userInfos.get(0);
-        }
+	}
 
-        return userInfo;
-    }
+	/**
+	 * 更新手机
+	 *
+	 * @param user     用户
+	 * @param mobile 手机
+	 */
+	public static void updateById(User user, String mobile) {
+		List<UserInfo> userInfos = UserInfo.find("byUser", user).fetch();
+		UserInfo userInfo = null;
+		if (userInfos.size() > 0) {
+			userInfo = userInfos.get(0);
+			userInfo.mobile = mobile;
+			userInfo.bindMobileAt = new Date();
+			userInfo.save();
+		}
+	}
+
+	/**
+	 * 查询用户基本信息
+	 *
+	 * @param user 用户ID信息
+	 * @return 用户基本信息
+	 */
+	public static UserInfo findByUser(User user) {
+		List<UserInfo> userInfos = UserInfo.find("byUser", user).fetch();
+		UserInfo userInfo = null;
+		if (userInfos.size() > 0) {
+			userInfo = userInfos.get(0);
+		}
+
+		return userInfo;
+	}
 
 }
