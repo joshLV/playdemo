246395272b95a5b6e5062a546b4a7300ec3f8d91
likang <likang@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-05-18 02:20:51 +0000

    reeb-288
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1375 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- resale/home/app/models/taobao/TaobaoComet.java
+++ resale/home/app/models/taobao/TaobaoComet.java
@@ -20,7 +20,11 @@ import play.jobs.OnApplicationStart;
 public class TaobaoComet extends Job{
     private static final String APPKEY = Play.configuration.getProperty("taobao.top.appkey", "12621657");
     private static final String APPSECRET = Play.configuration.getProperty("taobao.top.appsecret", "b0d06603b45a281f783b6ccd72ad8745");
+    private static final String COMET_ON = Play.configuration.getProperty("taobao.comet.on", "false");
     public void doJob(){
+        if(Play.runingInTestMode() || COMET_ON.equals("false")){
+            return;
+        }
         //不传入具体的userId，这样所有授权此应用的账号消息都会发到此连接
         Configuration conf = new Configuration(APPKEY,APPSECRET,null);
         TopCometStream stream = new TopCometStreamFactory(conf).getInstance();
