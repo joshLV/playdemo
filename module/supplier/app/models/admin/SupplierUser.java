96bc44a3b00921027a9093cd0caee6730619a8d2
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-05-22 06:54:45 +0000

    reeb-405
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1425 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- module/supplier/app/models/admin/SupplierUser.java
+++ module/supplier/app/models/admin/SupplierUser.java
@@ -1,305 +1,297 @@
 package models.admin;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import javax.persistence.OrderBy;
-import javax.persistence.Table;
-import javax.persistence.Transient;
-
+import com.uhuila.common.constants.DeletedStatus;
 import models.sales.Shop;
 import models.supplier.Supplier;
-
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang.StringUtils;
-
 import play.Logger;
 import play.data.validation.Required;
-import play.data.validation.Unique;
 import play.db.jpa.Model;
 import play.libs.Images;
 import play.modules.paginate.JPAExtPaginator;
 import play.modules.view_ext.annotation.Mobile;
 import play.mvc.Http.Request;
 
-import com.uhuila.common.constants.DeletedStatus;
+import javax.persistence.*;
+import java.util.*;
 
 @Entity
 @Table(name = "supplier_users")
 public class SupplierUser extends Model {
-	@Column(name = "login_name")
-	@Required
-	public String loginName;
+    @Column(name = "login_name")
+    @Required
+    public String loginName;
 
-	@Required
-	@Mobile
-	public String mobile;
-	
-	/**
-	 * 工号.
-	 * 用于短信验证方式，消费者向指定号码回复工号，即可完成验证。
-	 * TODO: 员工工号需要保证在同一个Supplier中唯一
-	 */
-	@Column(name="job_number")
-	public String jobNumber;
+    @Required
+    @Mobile
+    public String mobile;
 
-	@Column(name = "encrypted_password")
-	public String encryptedPassword;
+    /**
+     * 工号.
+     * 用于短信验证方式，消费者向指定号码回复工号，即可完成验证。
+     * TODO: 员工工号需要保证在同一个Supplier中唯一
+     */
+    @Column(name = "job_number")
+    public String jobNumber;
 
-	@Column(name = "password_salt")
-	public String passwordSalt;
+    @Column(name = "encrypted_password")
+    public String encryptedPassword;
 
-	@Column(name = "last_login_ip")
-	public String lastLoginIP;
+    @Column(name = "password_salt")
+    public String passwordSalt;
 
-	@Column(name = "last_login_at")
-	public Date lastLoginAt;
+    @Column(name = "last_login_ip")
+    public String lastLoginIP;
 
-	@Column(name = "lock_version")
-	public int lockVersion;
+    @Column(name = "last_login_at")
+    public Date lastLoginAt;
 
-	@Column(name = "created_at")
-	public Date createdAt;
+    @Column(name = "lock_version")
+    public int lockVersion;
 
-	@Column(name = "updated_at")
-	public Date updatedAt;
+    @Column(name = "created_at")
+    public Date createdAt;
 
-	@Column(name = "user_name")
-	public String userName;
+    @Column(name = "updated_at")
+    public Date updatedAt;
 
-	@ManyToOne
-	@JoinColumn(name="supplier_id")
-	public Supplier supplier;
+    @Column(name = "user_name")
+    public String userName;
 
-	@Transient
-	public String confirmPassword;
-	@Transient
-	public String oldPassword;
+    @ManyToOne
+    @JoinColumn(name = "supplier_id")
+    public Supplier supplier;
 
-	@OneToOne
-	@JoinColumn(name="shop_id",nullable=true)
-	public Shop shop;
+    @Transient
+    public String confirmPassword;
+    @Transient
+    public String oldPassword;
 
-	/**
-	 * 逻辑删除,0:未删除，1:已删除
-	 */
-	@Enumerated(EnumType.ORDINAL)
-	public DeletedStatus deleted;
+    @OneToOne
+    @JoinColumn(name = "shop_id", nullable = true)
+    public Shop shop;
 
-	@ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
-	@JoinTable(name = "supplier_users_roles",
-	inverseJoinColumns = @JoinColumn(name = "role_id"),
-	joinColumns = @JoinColumn(name = "user_id"))
-	@OrderBy("id")
-	public List<SupplierRole> roles;
+    /**
+     * 逻辑删除,0:未删除，1:已删除
+     */
+    @Enumerated(EnumType.ORDINAL)
+    public DeletedStatus deleted;
 
-	@ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
-	@JoinTable(name = "supplier_permissions_users",
-	inverseJoinColumns = @JoinColumn(name = "permission_id"),
-	joinColumns = @JoinColumn(name = "user_id"))
-	public Set<SupplierPermission> permissions;
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
+    @JoinTable(name = "supplier_users_roles",
+            inverseJoinColumns = @JoinColumn(name = "role_id"),
+            joinColumns = @JoinColumn(name = "user_id"))
+    @OrderBy("id")
+    public List<SupplierRole> roles;
 
-	/**
-	 * 查询操作员信息
-	 * 
-	 * @param loginName 用户名
-	 * @param pageNumber 页数
-	 * @param pageSize 记录数
-	 * @return 操作员信息
-	 */
-	public static JPAExtPaginator<SupplierUser> getSupplierUserList(String loginName, Long supplierId,
-			int pageNumber, int pageSize) {
-		StringBuffer sql = new StringBuffer();
-		Map params = new HashMap();
-		sql.append("supplier.id = :supplierId");
-		params.put("supplierId", supplierId);
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
+    @JoinTable(name = "supplier_permissions_users",
+            inverseJoinColumns = @JoinColumn(name = "permission_id"),
+            joinColumns = @JoinColumn(name = "user_id"))
+    public Set<SupplierPermission> permissions;
 
-		//		sql.append(" and loginName <> 'admin' ");
+    /**
+     * 查询操作员信息
+     *
+     * @param loginName  用户名
+     * @param pageNumber 页数
+     * @param pageSize   记录数
+     * @return 操作员信息
+     */
+    public static JPAExtPaginator<SupplierUser> getSupplierUserList(String loginName, Long supplierId,
+                                                                    int pageNumber, int pageSize) {
+        StringBuffer sql = new StringBuffer();
+        Map params = new HashMap();
+        sql.append("supplier.id = :supplierId");
+        params.put("supplierId", supplierId);
 
-		sql.append(" and deleted = :deleted ");
-		params.put("deleted", DeletedStatus.UN_DELETED);
+        //		sql.append(" and loginName <> 'admin' ");
 
-		if (StringUtils.isNotBlank(loginName)) {
-			sql.append(" and loginName like :loginName");
-			params.put("loginName", loginName + "%");
-		}
+        sql.append(" and deleted = :deleted ");
+        params.put("deleted", DeletedStatus.UN_DELETED);
 
-		JPAExtPaginator<SupplierUser> usersPage = new JPAExtPaginator<>("SupplierUser s", "s",
-				SupplierUser.class, sql.toString(), params).orderBy("createdAt desc");
-		usersPage.setPageNumber(pageNumber);
-		usersPage.setPageSize(pageSize);
-		usersPage.setBoundaryControlsEnabled(false);
-		return usersPage;
-	}
+        if (StringUtils.isNotBlank(loginName)) {
+            sql.append(" and loginName like :loginName");
+            params.put("loginName", loginName + "%");
+        }
 
-	/**
-	 * 更新操作员信息
-	 *
-	 * @param id    ID
-	 * @param supplierUser 用户信息
-	 */
-	public static void update(long id, SupplierUser supplierUser) {
+        JPAExtPaginator<SupplierUser> usersPage = new JPAExtPaginator<>("SupplierUser s", "s",
+                SupplierUser.class, sql.toString(), params).orderBy("createdAt desc");
+        usersPage.setPageNumber(pageNumber);
+        usersPage.setPageSize(pageSize);
+        usersPage.setBoundaryControlsEnabled(false);
+        return usersPage;
+    }
 
-		SupplierUser updatedUser = SupplierUser.findById(id);
-		updatedUser.roles = supplierUser.roles;
-		updatedUser.loginName = supplierUser.loginName;
-		updatedUser.userName = supplierUser.userName;
-		updatedUser.mobile = supplierUser.mobile;
-		updatedUser.shop=supplierUser == null ? null:supplierUser.shop;
-		updatedUser.lastLoginAt = new Date();
-		updatedUser.updatedAt = new Date();
-		updatedUser.jobNumber = supplierUser.jobNumber;
-		//获得IP
-		updatedUser.lastLoginIP = Request.current().remoteAddress;
+    /**
+     * 更新操作员信息
+     *
+     * @param id           ID
+     * @param supplierUser 用户信息
+     */
+    public static void update(long id, SupplierUser supplierUser) {
 
-		updatedUser.save();
-	}
+        SupplierUser updatedUser = SupplierUser.findById(id);
+        updatedUser.roles = supplierUser.roles;
+        updatedUser.loginName = supplierUser.loginName;
+        updatedUser.userName = supplierUser.userName;
+        updatedUser.mobile = supplierUser.mobile;
+        updatedUser.shop = supplierUser == null ? null : supplierUser.shop;
+        updatedUser.lastLoginAt = new Date();
+        updatedUser.updatedAt = new Date();
+        updatedUser.jobNumber = supplierUser.jobNumber;
+        //获得IP
+        updatedUser.lastLoginIP = Request.current().remoteAddress;
 
-	/**
-	 * 判断用户名和手机是否唯一
-	 *
-	 * @param loginName 用户名
-	 * @param mobile    手机
-	 * @param supplierId
-	 */
-	public static String checkValue(Long id, String loginName, String mobile, Long supplierId) {
+        updatedUser.save();
+    }
 
-		StringBuilder sq = new StringBuilder("loginName = ? and supplier=? ");
-		List params = new ArrayList();
-		params.add(loginName);
-		params.add(new Supplier(supplierId));
-		if (id != null) {
-			sq.append("and id <> ?");
-			params.add(id);
-		}
-		List<SupplierUser> supplierUserList = SupplierUser.find(sq.toString(), params.toArray()).fetch();
-		String returnFlag = "0";
-		//用户名存在的情况
-		if (supplierUserList.size() > 0) returnFlag = "1";
-		else {
-			sq = new StringBuilder("mobile = ? ");
-			params = new ArrayList();
-			params.add(mobile);
-			if (id != null) {
-				sq.append("and id <> ?");
-				params.add(id);
-			}
-			//手机存在的情况
-			List<SupplierUser> mList = SupplierUser.find(sq.toString(), params.toArray()).fetch();
-			if (mList.size() > 0) returnFlag = "2";
-		}
+    /**
+     * 判断用户名和手机是否唯一
+     *
+     * @param loginName  用户名
+     * @param mobile     手机
+     * @param supplierId
+     */
+    public static String checkValue(Long id, String loginName, String mobile, Long supplierId) {
+        StringBuilder sq = new StringBuilder("loginName = ? and supplier=? ");
+        List params = new ArrayList();
+        params.add(loginName);
+        params.add(new Supplier(supplierId));
+        if (id != null) {
+            sq.append("and id <> ?");
+            params.add(id);
+        }
+        List<SupplierUser> supplierUserList = SupplierUser.find(sq.toString(), params.toArray()).fetch();
+        String returnFlag = "0";
+        //用户名存在的情况
+        if (supplierUserList.size() > 0) returnFlag = "1";
+        else {
+            sq = new StringBuilder("mobile = ? and supplier=? ");
+            params = new ArrayList();
+            params.add(mobile);
+            params.add(new Supplier(supplierId));
+            if (id != null) {
+                sq.append("and id <> ?");
+                params.add(id);
+            }
+            //手机存在的情况
+            List<SupplierUser> mList = SupplierUser.find(sq.toString(), params.toArray()).fetch();
+            if (mList.size() > 0) returnFlag = "2";
+        }
+        return returnFlag;
+    }
 
-		return returnFlag;
-	}
-	
-	/**
-	 * 创建用户信息
-	 *
-	 * @param supplierId
-	 * @return
-	 */
-	public boolean create(Long supplierId) {
-		Images.Captcha captcha = Images.captcha();
-		Supplier supplier = Supplier.findById(supplierId);
-		String password_salt = captcha.getText(6);
-		// 密码加密
-		encryptedPassword = DigestUtils.md5Hex(encryptedPassword
-				+ password_salt);
-		// 随机码
-		passwordSalt = password_salt;
-		createdAt = new Date();
-		lockVersion = 0;
-		this.supplier = supplier;
-		deleted = DeletedStatus.UN_DELETED;
-		// 获得IP
-		lastLoginIP = Request.current().remoteAddress;
-		return super.create();
-	}
+    /**
+     * 创建用户信息
+     *
+     * @param supplierId
+     * @return
+     */
+    public boolean create(Long supplierId) {
+        Images.Captcha captcha = Images.captcha();
+        Supplier supplier = Supplier.findById(supplierId);
+        String password_salt = captcha.getText(6);
+        // 密码加密
+        encryptedPassword = DigestUtils.md5Hex(encryptedPassword
+                + password_salt);
+        // 随机码
+        passwordSalt = password_salt;
+        createdAt = new Date();
+        lockVersion = 0;
+        this.supplier = supplier;
+        deleted = DeletedStatus.UN_DELETED;
+        // 获得IP
+        lastLoginIP = Request.current().remoteAddress;
+        return super.create();
+    }
 
-	// FIXME: findAdmin这个名字，是指只找Admin用户？这个应该是findUser
-	public static SupplierUser findAdmin(Long supplierId, String admin) {
-		Supplier supplier = Supplier.findById(supplierId);
-		return find("bySupplierAndLoginName", supplier, admin).first();
-	}
+    // FIXME: findAdmin这个名字，是指只找Admin用户？这个应该是findUser
+    public static SupplierUser findAdmin(Long supplierId, String admin) {
+        Supplier supplier = Supplier.findById(supplierId);
+        return find("bySupplierAndLoginName", supplier, admin).first();
+    }
 
-	public static SupplierUser findUserByDomainName(String domainName, String loginName) {
-		Supplier supplier = Supplier.find("byDomainName", domainName).first();
-		if (supplier == null) {
-			return null;
-		}
+    public static SupplierUser findUserByDomainName(String domainName, String loginName) {
+        Supplier supplier = Supplier.find("byDomainName", domainName).first();
+        if (supplier == null) {
+            return null;
+        }
 
-		List<SupplierUser> all = SupplierUser.findAll();
-		for (SupplierUser user : all) {
-			Logger.debug("----- user.id:" + user.id + ", supplierId:" + user.supplier.id + ", loginName:" + user.loginName);
-		}
+        List<SupplierUser> all = SupplierUser.findAll();
+        for (SupplierUser user : all) {
+            Logger.debug("----- user.id:" + user.id + ", supplierId:" + user.supplier.id + ", loginName:" + user.loginName);
+        }
 
-		return SupplierUser.find("bySupplierAndLoginName", supplier, loginName).first();
-	}
+        return SupplierUser.find("bySupplierAndLoginName", supplier, loginName).first();
+    }
 
-	/**
-	 * 修改密码
-	 * 
-	 * @param newUser 新用户
-	 * @param user 原用户
-	 */
-	public static void updatePassword(SupplierUser newUser, SupplierUser user) {
-		// 随机码
-		Images.Captcha captcha = Images.captcha();
-		String newPasswordSalt = captcha.getText(6);
-		newUser.passwordSalt = newPasswordSalt;
-		// 新密码
-		String newPassword = user.encryptedPassword;
-		newUser.encryptedPassword = DigestUtils.md5Hex(newPassword + newPasswordSalt);
-		newUser.save();
+    /**
+     * 修改密码
+     *
+     * @param newUser 新用户
+     * @param user    原用户
+     */
+    public static void updatePassword(SupplierUser newUser, SupplierUser user) {
+        // 随机码
+        Images.Captcha captcha = Images.captcha();
+        String newPasswordSalt = captcha.getText(6);
+        newUser.passwordSalt = newPasswordSalt;
+        // 新密码
+        String newPassword = user.encryptedPassword;
+        newUser.encryptedPassword = DigestUtils.md5Hex(newPassword + newPasswordSalt);
+        newUser.save();
 
-	}
+    }
 
-	/**
-	 * 验证手机是否存在
-	 * @param mobile
-	 * @return
-	 */
-	public static boolean checkMobile(String mobile) {
-		SupplierUser supplierUser = SupplierUser.find("mobile = ?", mobile).first();
-		if (supplierUser != null) {
-			return true;
-		}
-		return false;
-	}
+    /**
+     * 验证手机是否存在
+     *
+     * @param mobile
+     * @return
+     */
+    public static boolean checkMobile(String mobile) {
+        SupplierUser supplierUser = SupplierUser.find("mobile = ?", mobile).first();
+        if (supplierUser != null) {
+            return true;
+        }
+        return false;
+    }
 
-	/**
-	 * 找回密码，更新新密码
-	 * @param supplierUserId
-	 * @param mobile
-	 * @param password
-	 */
-	public static void updateFindPwd(Long supplierUserId, String mobile,
-			String password) {
-		SupplierUser supplierUser = SupplierUser.find("id = ? and mobile = ?",supplierUserId,mobile).first();
-		if (supplierUser != null) {
-			Images.Captcha captcha = Images.captcha();
-			String password_salt = captcha.getText(6);
-			// 密码加密
-			supplierUser.encryptedPassword = DigestUtils.md5Hex(password
-					+ password_salt);
-			// 随机码
-			supplierUser.passwordSalt = password_salt;
-			supplierUser.save();
-		}
-		
-	}}
+    /**
+     * 找回密码，更新新密码
+     *
+     * @param supplierUserId
+     * @param mobile
+     * @param password
+     */
+    public static void updateFindPwd(Long supplierUserId, String mobile,
+                                     String password) {
+        SupplierUser supplierUser = SupplierUser.find("id = ? and mobile = ?", supplierUserId, mobile).first();
+        if (supplierUser != null) {
+            Images.Captcha captcha = Images.captcha();
+            String password_salt = captcha.getText(6);
+            // 密码加密
+            supplierUser.encryptedPassword = DigestUtils.md5Hex(password
+                    + password_salt);
+            // 随机码
+            supplierUser.passwordSalt = password_salt;
+            supplierUser.save();
+        }
+
+    }
+
+    /**
+     * 查询操作员信息
+     * @param mobile 手机
+     * @param supplier 商户ID
+     * @return 操作员
+     */
+    public static SupplierUser findByMobileAndSupplier(String mobile, Supplier supplier) {
+        SupplierUser supplierUser = SupplierUser.find("delete = ? and mobile = ? and supplier = ?",
+                DeletedStatus.UN_DELETED,mobile,supplier).first();
+        return supplierUser;
+    }
+
+}
