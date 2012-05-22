b89d1fedababff1f010a525105ba5bb666566adc
sujie <sujie@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-05-22 11:32:18 +0000

    REEB-476 fix bug
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1440 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ website/www/app/controllers/Application.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * .
+ * <p/>
+ * User: sujie
+ * Date: 5/22/12
+ * Time: 6:56 PM
+ */
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+    public static void about() {
+        render();
+    }
+
+    public static void help() {
+        render();
+    }
+
+    public static void contact() {
+        render();
+    }
+}
