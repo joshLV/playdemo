36d5d018582eae2e0319ec30163f4024abba970b
zhangjunyi <zhangjunyi@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-05-22 06:04:40 +0000

    REEB-456
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1417 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- resale/home/app/controllers/Application.java
+++ resale/home/app/controllers/Application.java
@@ -6,7 +6,10 @@ import play.mvc.*;
 import java.util.*;
 
 import models.*;
+import play.mvc.With;
+import controllers.modules.resale.cas.SecureCAS;
 
+@With(SecureCAS.class)
 public class Application extends Controller {
 
     public static void index() {
