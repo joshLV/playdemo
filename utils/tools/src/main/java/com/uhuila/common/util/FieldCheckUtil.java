f5e4ab7808cc7e81a1d6ca3e694e7c6a7c45ddf4
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-05-22 03:00:38 +0000

    reeb-405
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1414 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ utils/tools/src/main/java/com/uhuila/common/util/FieldCheckUtil.java
@@ -0,0 +1,28 @@
+package com.uhuila.common.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * <p/>
+ * User: yanjy
+ * Date: 12-5-22
+ * Time: 上午10:56
+ */
+public class FieldCheckUtil {
+    /**
+     * 判断是否为数字
+     * @param str 字符串
+     * @return true or false
+     */
+    public boolean isNumeric(String str) {
+        Pattern pattern = Pattern.compile("[0-9]*");
+        Matcher isNum = pattern.matcher(str);
+        if (!isNum.matches()) {
+            return false;
+        }
+        return true;
+    }
+}
+
