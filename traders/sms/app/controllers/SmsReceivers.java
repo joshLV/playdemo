686eb22e123489becede934b906c22b3bb2959a2
tangliqun <tangliqun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-05-22 02:36:01 +0000

    REEB-455
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1412 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- traders/sms/app/controllers/SmsReceivers.java
+++ traders/sms/app/controllers/SmsReceivers.java
@@ -10,7 +10,8 @@ import play.mvc.Controller;
 public class SmsReceivers extends Controller {
 
     /**
-     * mobiles=1391234567&msg=23223432432&username=wang&pwd=5a1a023fd486e2f0edbc595854c0d808&dt=1319873904&code=1028
+     * 消费者验证：mobiles=1391234567&msg=9527&username=wang&pwd=5a1a023fd486e2f0edbc595854c0d808&dt=1319873904&code=1028
+     * 店员验证：  mobiles=15900002342&msg=#xxxx#&username=wang&pwd=5a1a023fd486e2f0edbc595854c0d808&dt=1319873904&code=1028
      */
     public static void getSms() {
        String mobile = params.get("mobiles");
@@ -20,17 +21,19 @@ public class SmsReceivers extends Controller {
        String dt = params.get("dt");
        String code = params.get("code");
 
-       System.out.println("mobile=" + mobile + ",msg=" + msg + ",username=" 
+       System.out.println("mobile=" + mobile + ",msg=" + msg + ",username="
                + username + ",pwd=" + pwd + ",dt=" + dt + ",code=" + code);
-       
+
+
+       // 消费者验证逻辑
        ECoupon ecoupon = ECoupon.findByMobileAndCode(mobile, code);
        if (ecoupon == null) {
            renderText("Not Found the ecoupon");
        }
-       
+
        Supplier supplier = Supplier.findById(ecoupon.goods.supplierId);
        SupplierUser supplierUser = SupplierUser.find("from SupplierUser where supplier.id=? and jobNumber=?", ecoupon.goods.supplierId, msg).first();
-       
+
        if (supplierUser == null) {
            // 发给消费者
            SMSUtil.send("没有找到对应的店员工号，请确认您的输入只包括工号，或确认您是在<" + supplier.fullName + ">的门店消费，谢谢！", mobile, code);
@@ -41,7 +44,7 @@ public class SmsReceivers extends Controller {
            SMSUtil.send("指定店员" + supplierUser.userName + "无门店设置，请询问店员，谢谢！", mobile, code);
            renderText("error, coupon=" + ecoupon.getMaskedEcouponSn() + ",指定店员" + supplierUser.userName + "无门店设置");
        }
-       
+
        if (ecoupon.status == ECouponStatus.UNCONSUMED) {
            ecoupon.consumed(supplierUser.shop.id);
            // 发给店员
@@ -52,7 +55,7 @@ public class SmsReceivers extends Controller {
            // 发给消费者
            SMSUtil.send("你的券号:" + ecoupon.getMaskedEcouponSn() + "(面值" + ecoupon.faceValue + "元)不能消费，状态码：" + ecoupon.status.toString(), mobile, code);
        }
-       
+
        renderText("ok, coupon=" + ecoupon.getMaskedEcouponSn());
     }
 }
