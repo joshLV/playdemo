7a2729d96eede11fb5b5675fdb365c9bc2e5fdee
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-05-23 02:58:23 +0000

    reeb-493
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1445 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- module/consumer/app/models/consumer/User.java
+++ module/consumer/app/models/consumer/User.java
@@ -193,6 +193,9 @@ public class User extends Model {
      */
     public void updateMobile(String mobile) {
         this.mobile = mobile;
+        this.userInfo.mobile = mobile;
+        this.userInfo.bindMobileAt = new Date();
+        this.userInfo.save();
         this.save();
     }
 
