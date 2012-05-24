64ad0723469d31247ea75b68ff5fe2ec73bb2a3c
likang <likang@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-05-24 15:06:29 +0000

    reeb-502
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1580 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- resale/home/public/javascripts/resaler_fav.js
+++ resale/home/public/javascripts/resaler_fav.js
@@ -45,6 +45,8 @@ $(window).load(
                 url:'/library/' + goods_id,
                 success:function(data){
                     $("#row_" + goods_id).remove();
+                    $("#row_hd_" + goods_id).remove();
+                    $("#row_sep_" + goods_id).remove();
                 }});
             
             return false;
@@ -116,6 +118,8 @@ $(window).load(
                 success:function(data){
                     for(var i = 0; i< checked.length; i++){
                         $("#row_" + checked[i]).remove();
+                        $("#row_hd_" + goods_id).remove();
+                        $("#row_sep_" + goods_id).remove();
                     }
                     set_all_select_all_checkbox(false);
                 }});
