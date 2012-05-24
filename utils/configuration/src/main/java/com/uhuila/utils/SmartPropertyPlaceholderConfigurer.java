7eed9fbf697ebf892d138e92356cd13a97ead345
tangliqun <tangliqun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-05-24 08:40:37 +0000

    REEB-455 CAS配置分离
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1542 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- utils/configuration/src/main/java/com/uhuila/utils/SmartPropertyPlaceholderConfigurer.java
+++ utils/configuration/src/main/java/com/uhuila/utils/SmartPropertyPlaceholderConfigurer.java
@@ -2,13 +2,18 @@ package com.uhuila.utils;
 
 import java.io.File;
 import java.util.Properties;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.FileSystemResource;
 
 public class SmartPropertyPlaceholderConfigurer extends
-        PropertyPlaceholderConfigurer {
+        PropertyPlaceholderConfigurer 
+        implements BeanNameAware, BeanFactoryAware {
 
+    private Properties _properties;
+    
     /**
      * 传入一个文件名，然后检查是否在指定的reconfig.dir中有这个文件，如果有就使用之。
      * 如果没有，检查classpath中有没有，有则使用之。
@@ -18,6 +23,7 @@ public class SmartPropertyPlaceholderConfigurer extends
         String strReconfigDir = System.getProperty("reconfig.dir",  "/etc/reeb/cas"); 
         
         File reconfigDir = new File(strReconfigDir);
+        System.out.println("strConfigDir=" + strReconfigDir);
         if (reconfigDir.exists() && reconfigDir.isDirectory()) {
             File reconfigFile = new File(reconfigDir, fileName);
             if (reconfigFile.exists()) {
@@ -32,7 +38,28 @@ public class SmartPropertyPlaceholderConfigurer extends
     }
     
     public Properties getMergedProperties() throws Exception {
-        return this.mergeProperties();
+        if (_properties == null) {
+            _properties = this.mergeProperties();
+        }
+        return _properties;
     }
+    
+
+    @Override
+    protected String resolvePlaceholder(String placeholder, Properties props) {
+        String result;
+        try {
+            result = getMergedProperties().getProperty(placeholder);
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = null;
+        }
+        if (result == null) {
+            return super.resolvePlaceholder(placeholder, props);
+        } else {
+            return result;
+        }
+    }
+    
     
 }
