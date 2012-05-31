e43c6cac4216d12c0eb5ef1a8ff80256edc233da
zhangjunyi <zhangjunyi@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-05-31 02:48:45 +0000

    REEB-550
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1729 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ cdn/assets/public/javascripts/u/carts.js
@@ -0,0 +1,212 @@
+/**
+ *
+ * @author likang
+ */
+
+/**
+ * 发送订单数量变更请求，若数量变为非正整数，则拒绝操作
+ *
+ * @param goods_id  商品ID
+ * @param increment 增量
+ */
+function reorder(goods_id,increment){
+    var element = $("#num_" + goods_id);
+    var last_num_ele = $("#last_num_" + goods_id);
+    var stock = Number($("#stock_" + goods_id).val());
+
+    var last_num = Number(last_num_ele.val())
+    var new_num = last_num + increment;
+    if(new_num <= 0){
+        element.val(last_num);
+        return;
+    }
+    if(new_num > 999){
+        new_num = 999;
+        increment = 999 - last_num;
+    }
+    if(new_num > stock){
+        new_num = stock;
+        increment = stock - last_num;
+    }
+    if(increment == 0){
+        element.val(last_num);
+        return;
+    }
+    $.post('/carts',
+            {goodsId:goods_id,increment:increment},
+            function(data){
+                element.val(new_num);
+                last_num_ele.val(new_num);
+                calItem(goods_id);
+                refreshAmount();
+            });
+}
+//计算单行的总价
+function calItem(goods_id){
+    var total_price = (new BigNumber($("#price_" + goods_id).text())).multiply($("#num_" + goods_id).val()).toString();
+    $("#subtotal_" + goods_id).text(total_price.toString());
+}
+//计算订单总价
+function refreshAmount(){
+    var number = 0;
+    var amount = new BigNumber("0");
+
+    $("input[id^=check_goods_]").each(function(){
+        if(!this.checked){
+            return true;
+        }
+        var el_id = $(this).attr("id");
+        var goods_id = el_id.substr(el_id.lastIndexOf("_") + 1);
+        number += Number($("#num_" + goods_id).val());
+        amount = amount.add($("#subtotal_" + goods_id).text());
+
+    });
+
+    //var number = 0;
+    //$("input.num_input").each(function(){number += Number($(this).val())});
+    $("#total_num").text(number);
+
+    //var amount = new BigNumber("0");
+    //$("span[id^=subtotal_]").each(function(){amount = amount.add($(this).text())});;
+    $("#carts_amount").text(amount.toString());
+}
+
+$(window).load(
+    function(){
+        //点击+按钮
+        $("a.add_box").click(function() {
+            reorder($(this).attr("name"),1);
+            return false;
+        });
+        //点击-按钮
+        $("a.reduce_box").click(function() {
+            reorder($(this).attr("name"),-1);
+            return false;
+        });
+        //直接在文本框里输入
+        $("input.num_input").blur(function(){
+            var el_id = $(this).attr("id");
+            var last_num = $("#last_" + el_id);
+            var re= /^\d+(\.\d+)?$/;
+            if(!re.test($(this).val())){
+                $(this).val(last_num.val());
+                return;
+            }
+            var goods_id = el_id.substr(el_id.lastIndexOf("_") + 1);
+            reorder(goods_id, Number($(this).val()) - Number(last_num.val()));
+        });
+        //点击删除
+        $("a.del").click(function(){
+            var goods_id = $(this).attr("goods_id");
+            $.ajax({
+                type:'DELETE',
+                url:'/carts/' + goods_id,
+                success:function(data){
+                    $("#row_" + goods_id).remove()
+                    refreshAmount();
+                }});
+            
+            return false;
+        });
+
+        var set_all_select_all_checkbox =function(checked){
+            $("input[name=select_all_checkbox]").each(function(){
+                this.checked = checked
+            });
+        };
+        var set_all_goods_checkbox = function(checked){
+            $("input[id^=check_goods_]").each(function(){
+                this.checked = checked
+            }); 
+        };
+        //点击全选
+        $("input[name=select_all_checkbox]").each(function(){
+               $(this).click( 
+                  function(){ 
+                     if(this.checked){ 
+                        set_all_select_all_checkbox(true);
+                        set_all_goods_checkbox(true);
+                     }else{ 
+                        set_all_select_all_checkbox(false);
+                        set_all_goods_checkbox(false);
+                     } 
+                    refreshAmount();
+                  } 
+                )});
+        //点击单个复选框
+        var all_checked= function(){
+            var all_check = true;
+            $("input[id^=check_goods_]").each(function(){
+                if(this.checked){
+                    return true;
+                }else{
+                    all_check = false;
+                    return false;
+                }
+            }); 
+
+            return all_check;
+        };
+        $("input[id^=check_goods_]").each(function(){ 
+            $(this).click( function(){
+                if(all_checked()){
+                    set_all_select_all_checkbox(true);
+                }else{
+                    set_all_select_all_checkbox(false);
+                }
+                refreshAmount();
+            })
+        }); 
+
+
+        //点击批量删除
+        $("#batch_delete").click(function(){
+            var checked=[]
+            $("input[id^=check_goods_]").each(function(){
+                if(this.checked){
+                    var id_temp = $(this).attr("id");
+                    checked.push(id_temp.substr(id_temp.lastIndexOf("_")+1));
+                }
+            });
+
+            if(checked.length == 0) return;
+
+            $.ajax({
+                type:'DELETE',
+                url:'/carts/' +checked.join(","),
+                success:function(data){
+                    for(var i = 0; i< checked.length; i++){
+                        $("#row_" + checked[i]).remove();
+                    }
+                    set_all_select_all_checkbox(false);
+                    refreshAmount();
+                }});
+            
+            });
+        //点击确认付款
+        $("#confirm_to_order").click(function(){
+        	var items = "";
+        	$("input[id^=check_goods_]").each(function(){
+                if(!this.checked){
+                    return true;
+                }
+                var el_id = $(this).attr("id");
+                var goods_id = el_id.substr(el_id.lastIndexOf("_") + 1);
+                items += goods_id +"-" + $("#num_" + goods_id).val() + ",";
+            });
+            if(items.length == 0){
+                return false;
+            }
+        	
+        	var t = $(this);
+        	t.attr("href", t.attr("href") + items);
+        	return true;
+        });
+        
+        
+        set_all_select_all_checkbox(true);
+        set_all_goods_checkbox(true);
+        refreshAmount();
+    }
+);
+
---
+++ cdn/assets/public/javascripts/u/orders.js
@@ -0,0 +1,213 @@
+$(function ($) {
+    $('#J_modifyAddr').click(function (ev) {
+        ev.preventDefault();
+        var _this = $(this);
+
+        if (_this.text() == '[修改]') {
+            $("#err-addressInfo").hide();
+            _this.text('[关闭]');
+            $('#J_addrList').load('/Addresses/index', function() {
+                $('#J_addrAll').show(100);
+            });
+            $('#J_addrEditBox').html("");
+        } else {
+            _this.text('[修改]');
+            $('#J_addrAll').hide();
+            $('#J_useAddr').hide();
+            $('#J_confirm').text('送到这个地址').removeAttr('data-action');
+        }
+    });
+
+    // 切换地址
+    $('#J_addrAll ul input').live('click', function () {
+        $('#J_addrEditBox').html("");
+
+        if ($(this).attr('id') == 'addrId_new') {
+            $('#J_addrEditBox').load('/orders/addresses/new', function (data) {
+                $('#J_confirm').text('保存并送到这个地址').attr('data-action', 'add-addr');
+            });
+        } else {
+            var thisid = $(this).attr('id'),
+                thisaddr = $('label[for='+ thisid +']').html();
+            $('#J_addrCurrent').html(thisaddr);
+            $('#J_confirm').text('送到这个地址');
+        }
+    });
+
+    // 删除地址
+    $('.addr-del').live('click', function (ev) {
+        ev.preventDefault();
+
+        var addrId = $(this).attr('data-addrid');
+        var isDefault = $("#addrId_" + addrId).attr('checked') == 'checked';
+
+        $(this).append('<div class="addr-del-confirm">您要删除该地址吗？<br><b>确定删除</b> <b>取消</b><img src="http://img.uhcdn.com/images/u/o_jian.png" /></div>');
+
+        $('.addr-del-confirm').live('click', function (ev) {
+            var txt = $(ev.target).text();
+            if (txt == '确定删除') {
+                $.ajax({
+                    url:'/orders/addresses/' + addrId,
+                    type:'DELETE',
+                    success:function () {
+                        $('#addr-li-' + addrId).remove();
+                        //如果被删除的地址是默认地址的话，则默认收货地址信息和地址列表都要重新加载
+                        if (isDefault) {
+                            $('#J_addrCurrent').load('/orders/addresses/default');
+                            $('#J_addrList').load('/orders/addresses');
+                        }
+                    }
+                });
+            } else if (txt == '取消') {
+                $(this).remove();
+            }
+        });
+    });
+
+    // 编辑地址
+    $('.addr-edit').live('click', function (ev) {
+        ev.preventDefault();
+        var addrId = $(this).attr('data-addrid');
+
+        $('#J_addrEditBox').load('/orders/addresses/' + addrId + '/edit', function (data) {
+            $("#addrId_" + addrId).attr('checked', 'checked');
+
+            $('#J_confirm').text('保存并送到这个地址').attr({'data-action':'edit-addr', 'data-addrid':addrId});
+        });
+    });
+
+    // 添加、修改地址
+    $('#J_confirm').click(function (ev) {
+        ev.preventDefault();
+
+        var addrId = $(this).attr('data-addrid');
+
+        if ($(this).text() == '保存并送到这个地址') { //新增地址时
+            if ($(this).attr('data-action') == 'add-addr') {
+
+                var addrName = $('#J_addrName'),
+                    addrPost = $('#J_addrPost'),
+                    addrStreet = $('#J_addrStreet'),
+                    addrMobile = $('#J_addrMobile'),
+                    addrAreaCode = $('#J_addrAreaCode'),
+                    addrPhoneNum = $('#J_addrPhoneNum'),
+                    addrPhoneExt = $('#J_addrPhoneExt');
+
+                if (addrStreet.val() == '') {
+                    $("#err-address").html('街道地址不能空！');
+                    return;
+                }
+                if (!/^[1-9]\d{5}$/.test($.trim(addrPost.val()))) {
+                    $("#err-postcode").html('邮政编码格式不对！');
+                    return;
+                }
+                if (addrName.val() == '') {
+                    $("#err-name").html('收货人不能空！');
+                    return;
+                }
+                if (!/^[1-9]\d{10}$/.test($.trim(addrMobile.val()))) {
+                    if (!/^\d{3,4}$/.test($.trim(addrAreaCode.val())) || !/^\d{7,8}$/.test($.trim(addrPhoneNum.val()))) {
+                        $("#err-phone").html("联系电话填写不对!");
+                        return;
+                    }
+                }
+                $.post('/orders/addresses/new', {
+                    'address.name':$.trim($('#J_addrName').val()),
+                    'address.postcode':$.trim(addrPost.val()),
+                    'address.province':$('#J_addrProv').val(),
+                    'address.city':$('#J_addrCity').val(),
+                    'address.district':$('#J_addrDist').val(),
+                    'address.address':$('#J_addrStreet').val(),
+                    'address.mobile':$.trim($('#J_addrMobile').val()),
+                    'address.areaCode':$.trim($('#J_addrAreaCode').val()),
+                    'address.phoneNumber':$.trim($('#J_addrPhoneNum').val()),
+                    'address.phoneExtNumber':$.trim($('#J_addrPhoneExt').val()),
+                    'address.isDefault':true
+                }, function (data) {
+                    $('#J_addrCurrent').load('/orders/addresses/default');
+                    $('#J_modifyAddr').text('[修改]');
+                    $('#J_addrAll').hide();
+
+                    $('#J_addrAll ul').prepend(data);
+                });
+
+            } else if ($(this).attr('data-action') == 'edit-addr') { //修改地址时
+
+                var addrName = $('#J_addrName'),
+                    addrPost = $('#J_addrPost'),
+                    addrStreet = $('#J_addrStreet'),
+                    addrMobile = $('#J_addrMobile'),
+                    addrAreaCode = $('#J_addrAreaCode'),
+                    addrPhoneNum = $('#J_addrPhoneNum'),
+                    addrPhoneExt = $('#J_addrPhoneExt');
+
+                if (addrStreet.val() == '') {
+                    $("#err-address").html('街道地址不能空！');
+                    return;
+                }
+                if (!/^[1-9]\d{5}$/.test($.trim(addrPost.val()))) {
+                    $("#err-postcode").html('邮政编码格式不对！');
+                    return;
+                }
+                if (addrName.val() == '') {
+                    $("#err-name").html('收货人不能为空！');
+                    return;
+                }
+                if (!/^[1-9]\d{10}$/.test($.trim(addrMobile.val()))) {
+                    if (!/^\d{3,4}$/.test($.trim(addrAreaCode.val())) || !/^\d{7,8}$/.test($.trim(addrPhoneNum.val()))) {
+                        $("#err-phone").html("联系电话填写不对!");
+                        $("#err-phone").html("联系电话填写不对!");
+                        return;
+                    }
+                }
+                $.ajax({
+                    url:'/orders/addresses/' + addrId,
+                    type:'PUT',
+                    data:{
+                        'address.id':addrId,
+                        'address.name':$.trim($('#J_addrName').val()),
+                        'address.postcode':$.trim(addrPost.val()),
+                        'address.province':$('#J_addrProv').val(),
+                        'address.city':$('#J_addrCity').val(),
+                        'address.district':$('#J_addrDist').val(),
+                        'address.address':$('#J_addrStreet').val(),
+                        'address.mobile':$.trim($('#J_addrMobile').val()),
+                        'address.areaCode':$.trim($('#J_addrAreaCode').val()),
+                        'address.phoneNumber':$.trim($('#J_addrPhoneNum').val()),
+                        'address.phoneExtNumber':$.trim($('#J_addrPhoneExt').val()),
+                        'address.isDefault':true
+                    },
+                    success:function (data) {
+                        $('#J_addrCurrent').html(data);
+                        $('#J_modifyAddr').text('[修改]');
+                        $('#J_addrAll').hide();
+                        $('#J_addrAll label[for="addrId_' + addrId + '"]').html(data);
+                        $('#J_addrAll input:checked').removeAttr('checked');
+                        $('#addrId_' + addrId).attr('checked', 'checked');
+                        $('#J_addrEditBox table').remove();
+                        $('#J_confirm').text('送到这个地址').removeAttr('data-action');
+                    }
+                });
+            }
+        }
+        else if ($(this).text() == '送到这个地址') {
+            $('#addressId').val(addrId);
+            $('#J_modifyAddr').text('[修改]');
+            $('#J_addrAll').hide();
+            var checkedId = $('#J_addrAll input:checked').attr('id'),
+                checkHTML = $('#J_addrAll label[for="' + checkedId + '"]').html();
+            $.ajax({
+                url:'/orders/addresses/' + $('#J_addrAll input:checked').attr("value") + "/default",
+                type:'PUT',
+                data:{},
+                success:function (data) {
+                }
+            });
+            $('#J_addrCurrent').html(checkHTML);
+        }
+    });
+
+    $("#link_confirm_pay").click(function () {
+        $("#order_create_form").submit();
+    });
+});
