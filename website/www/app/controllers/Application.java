eecb0e4f766086b636bda34ae1c3cb2a70115306
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-06-12 06:43:39 +0000

    reeb-607
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1802 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- website/www/app/controllers/Application.java
+++ website/www/app/controllers/Application.java
@@ -1,6 +1,10 @@
 package controllers;
 
+import controllers.modules.website.cas.SecureCAS;
+import controllers.modules.website.cas.annotations.SkipCAS;
+import models.cms.Topic;
 import play.mvc.Controller;
+import play.mvc.With;
 
 /**
  * .
@@ -9,6 +13,8 @@ import play.mvc.Controller;
  * Date: 5/22/12
  * Time: 6:56 PM
  */
+@With({SecureCAS.class, WebsiteInjector.class})
+@SkipCAS
 public class Application extends Controller {
 
     public static void index() {
@@ -29,5 +35,9 @@ public class Application extends Controller {
 
     public static void service() {
         render();
+    }
+    public static void topic(Long id) {
+        Topic topic = Topic.findById(id);
+        render(topic);
     }
 }
