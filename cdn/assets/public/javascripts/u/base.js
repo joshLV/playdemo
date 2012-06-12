116524836888ab051e00f3c7c78c2c5428c2cbcf
zhangjunyi <zhangjunyi@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-06-12 01:45:16 +0000

    REEB-598
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1792 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- cdn/assets/public/javascripts/u/base.js
+++ cdn/assets/public/javascripts/u/base.js
@@ -1,62 +1,56 @@
 $(function () {
-    //  我的一百券
-    $('.myUhuila').mouseover(function () {
-        $(this).addClass('myUhuila-on');
-        $('.myUhuila-bd').show();
-    });
-    $('.myUhuila').mouseout(function () {
-        $(this).removeClass('myUhuila-on');
-        $('.myUhuila-bd').hide();
+    $('#slides').slides({
+        play:5000,
+        pause:2500,
+        slideSpeed:600,
+        hoverPause:true
     });
 
     // 购物车
-    $('.carts').mouseover(function () {
-        //从服务端加载购物车
-        if ($("#reload").val() == "true") {
+    var isLoaded = false;
+    $('#cart').mouseover(function () {
+        if (isLoaded == false) {
             $("#cart-list").load("/carts/tops", function (data) {
-                $("#reload").val("false");
+                isLoaded = true;
+                $('.cart-bd').show();
             });
+        } else {
+            $('.cart-bd').show();
         }
-//        $("#carts_size").html($("#goods_count").val());
-        $(this).addClass('carts-on');
-        $('.carts-bd').show();
     });
-    $('.carts').mouseout(function () {
-        $(this).removeClass('carts-on');
-        $('.carts-bd').hide();
+    $('#cart').mouseout(function () {
+        $('.cart-bd').hide();
     });
+
     //购物车记录删除
     $(".cart-delete").live('click', function (ev) {
         ev.preventDefault();
-        var buyCount = parseInt($(this).attr("count"));
-        var goods_id = $(this).attr("goods_id");
-        var sale_price = $(this).attr("sale_price");
-        var all_number = parseInt($("#all_number").html());
-        var all_price = $("#all_price").html();
+        var buyCount = parseInt($(this).attr("count")),
+            goods_id = $(this).attr("goods_id"),
+            sale_price = $(this).attr("sale_price"),
+            all_number = parseInt($("#all_number").html()),
+            all_price = $("#all_price").html();
+
         $.ajax({
             type:'DELETE',
             url:'/carts/' + goods_id,
             success:function (data) {
-                var oldSize = parseInt($("#carts_size").html());
+                var oldSize = parseInt($("#cart-count").html());
                 var currentCount = oldSize - buyCount;
-                $("#carts_size").html(currentCount);
+                $("#cart-count").html(currentCount);
 
                 if (currentCount == 0) {
                     $("#row_" + goods_id).html('您的购物车中没有任何商品。<br/>');
                     $("#order_confirm").hide();
                     $("#detail").hide();
                 } else {
-                    $("#row_" + goods_id).remove();
+                    $("#row_" + goods_id).animate({height: '0'}, 100, function(){
+                        $(this).remove();
+                    });
                     $("#all_number").html(all_number - buyCount);
                     $("#all_price").html((all_price - (sale_price * buyCount)).toFixed(2));
                 }
-            }});
-    });
-
-    $('#slides').slides({
-        play:5000,
-        pause:2500,
-        slideSpeed:600,
-        hoverPause:true
+            }
+        });
     });
 });
