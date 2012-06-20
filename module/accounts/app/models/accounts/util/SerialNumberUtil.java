eaa92c93d1b08c79b831d6de534a5a31a6b82503
likang <likang@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-06-20 03:09:18 +0000

    reeb-620 用线程安全的方式使用SimpleDateFormat和DecimalFormat
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@1879 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- module/accounts/app/models/accounts/util/SerialNumberUtil.java
+++ module/accounts/app/models/accounts/util/SerialNumberUtil.java
@@ -9,8 +9,8 @@ import java.util.Random;
  * User: likang
  */
 public class SerialNumberUtil {
-    private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddhhmmssSSS");
-    private static DecimalFormat decimalFormat = new DecimalFormat("00000");
+    private static final String DATE_FORMAT = "yyyyMMddhhmmssSSS";
+    private static final String DECIMAL_FORMAT = "00000";
     
     public static String generateSerialNumber(){
         return generateSerialNumber(new Date());
@@ -18,6 +18,10 @@ public class SerialNumberUtil {
     
     public static String generateSerialNumber(Date date){
         int random = new Random().nextInt(100000);
+
+        //SimpleDateFormat和DecimalFormat不是线程安全的
+        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+        DecimalFormat decimalFormat = new DecimalFormat(DECIMAL_FORMAT);
         return dateFormat.format(date) + decimalFormat.format(random);
     }
 }
