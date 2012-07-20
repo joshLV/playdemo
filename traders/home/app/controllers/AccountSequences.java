6f6dcb73911ed9d1e05842a41ac2105cc78a5e1f
likang <likang@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-07-20 10:42:07 +0000

    reeb-741 修复供应商看财务报表时看到的order remark 可能显示别家商品的问题
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@2175 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- traders/home/app/controllers/AccountSequences.java
+++ traders/home/app/controllers/AccountSequences.java
@@ -3,7 +3,10 @@ package controllers;
 import models.accounts.Account;
 import models.accounts.AccountSequence;
 import models.accounts.AccountSequenceCondition;
+import models.accounts.TradeType;
 import models.accounts.util.AccountUtil;
+import models.order.Order;
+import models.order.OrderItems;
 import navigation.annotations.ActiveNavigation;
 import navigation.annotations.Right;
 
@@ -12,6 +15,9 @@ import org.apache.commons.lang.StringUtils;
 import play.modules.paginate.JPAExtPaginator;
 import play.mvc.Controller;
 import play.mvc.With;
+
+import javax.persistence.Query;
+import java.util.List;
 
 
 /**
@@ -40,6 +46,20 @@ public class AccountSequences extends Controller {
         condition.account = account;
         JPAExtPaginator<AccountSequence> accountSequences = AccountSequence.findByCondition(condition,
                 pageNumber, PAGE_SIZE);
+        //account_sequence记录的备注是订单的首个商品名，为避免出现显示别家商户的商品名，在此重新查一遍
+        for (AccountSequence sequence : accountSequences) {
+            if (sequence.tradeType != TradeType.PURCHASE_COSTING) {
+                continue;
+            }
+            Query query =  OrderItems.em().createQuery("select o from OrderItems o where o.order = :order and o.goods.supplierId = :supplier");
+            query.setParameter("order", Order.findById(sequence.orderId));
+            query.setParameter("supplier", accountId);
+            List<OrderItems> orderItems = query.getResultList();
+            if (orderItems.size() > 0 ) {
+                String postfix = orderItems.size() > 1 ? "等" + orderItems.size() + "个商品" : "";
+                sequence.remark = orderItems.get(0).goods.name + postfix;
+            }
+        }
         renderArgs.put("condition", condition);
 
         render(account, accountSequences);
