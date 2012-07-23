2f9498ef2dc6917a608d82f2695718f470175d90
hejun <hejun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-07-23 04:52:21 +0000

    REEB-756
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@2188 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- operate/business/test/unit/SupplierUnitTest.java
+++ operate/business/test/unit/SupplierUnitTest.java
@@ -1,10 +1,13 @@
 package unit;
 
 import java.util.List;
+
+import com.uhuila.common.util.PathUtil;
 import models.supplier.Supplier;
 import models.supplier.SupplierStatus;
 import org.junit.Before;
 import org.junit.Test;
+import play.Play;
 import play.test.Fixtures;
 import play.test.UnitTest;
 import com.uhuila.common.constants.DeletedStatus;
@@ -16,6 +19,13 @@ import com.uhuila.common.constants.DeletedStatus;
  * Date: 3/20/12
  * Time: 4:52 PM
  */
+
+/**
+ * Juno
+ * 7/23/12
+ * 添加测试
+ */
+
 public class SupplierUnitTest extends UnitTest {
     @Before
     @SuppressWarnings("unchecked")
@@ -29,6 +39,15 @@ public class SupplierUnitTest extends UnitTest {
         List<Supplier> suppliers = Supplier.findUnDeleted();
         assertEquals(2, suppliers.size());
         assertEquals("localhost", suppliers.get(0).domainName);
+    }
+
+    @Test
+    public void testGetSmallLogo(){
+        Long id = (Long) Fixtures.idCache.get("models.supplier.Supplier-Supplier1");
+        Supplier supplier = Supplier.findById(id);
+        assertEquals(PathUtil.getImageUrl(Play.configuration.getProperty
+                ("image.server", "img0.uhcdn.com"), "logo.jpg", "172x132"),
+                supplier.getOriginalLogo());
     }
 
     @Test
@@ -48,6 +67,18 @@ public class SupplierUnitTest extends UnitTest {
     }
 
     @Test
+    public void testUpdateWithNull(){
+        Long id = (Long) Fixtures.idCache.get("models.supplier.Supplier-Supplier1");
+        Supplier supplier = Supplier.findById(id);
+        // generate an empty id
+        Long emptyId = 123456789L;
+        Supplier.update(emptyId,supplier);
+        Supplier updatedSupplier = Supplier.findById(emptyId);
+        // Compare that the emptyId was not updated
+        assertNotSame(updatedSupplier,supplier);
+    }
+
+    @Test
     public void testUpdate() {
         Long id = (Long) Fixtures.idCache.get("models.supplier.Supplier-Supplier1");
         Supplier supplier = new Supplier(id);
@@ -59,10 +90,30 @@ public class SupplierUnitTest extends UnitTest {
     }
 
     @Test
+    public void testDeleteNull(){
+        List<Supplier> suppliers = Supplier.findUnDeleted();
+        Long emptyId = 123456789L;
+        Supplier.delete(emptyId);
+        // nothing is deleted
+        assertEquals(2, suppliers.size());
+    }
+
+    @Test
     public void testDelete() {
         Long id = (Long) Fixtures.idCache.get("models.supplier.Supplier-Supplier1");
         Supplier.delete(id);
         Supplier supplier = Supplier.findById(id);
         assertEquals(DeletedStatus.DELETED, supplier.deleted);
+    }
+
+    @Test
+    public void testFindListByFullName(){
+        List<Supplier> suppliers = Supplier.findListByFullName("本地");
+        assertEquals(1, suppliers.size());
+    }
+
+    @Test
+    public void testExistDomainName(){
+        assertTrue(Supplier.existDomainName("localhost"));
     }
 }
