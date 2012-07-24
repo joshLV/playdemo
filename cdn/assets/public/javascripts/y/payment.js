6825d2f4aadd2d22f772f986f0c2022e6518c5e7
zhangjunyi <zhangjunyi@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-07-24 08:15:28 +0000

    REEB-700
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@2219 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- cdn/assets/public/javascripts/y/payment.js
+++ cdn/assets/public/javascripts/y/payment.js
@@ -1,8 +1,5 @@
 $(function(){
     var do_not_need_epay = $("#do_not_need_epay").val();
-    $("#confirm").click(function(){
-        $("#confirm_form").submit();
-    });
 
     $("#use_balance").click(function(){
         if($(this).attr("checked") == "checked"){
@@ -15,6 +12,17 @@ $(function(){
                 $("#other-pay").css("display","block");
                 $("#confirm-pay").css("display","none");
             }
+        }
+    });
+        
+    $('#confirm').click(function(ev){
+        ev.preventDefault();
+        var ipt = $('input[name=paymentSourceCode]:checked');
+
+        if (ipt.length == 0) {
+            $('.onlinepay-bd').append('<span id="onlinepay-error" style="padding:2px;color:#f00">请选择支付方式</span>');;
+        } else {
+            $("#confirm_form").submit();
         }
     });
 });
