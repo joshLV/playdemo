e31f438e4a633683897ca7814780d87734834b37
tangliqun <tangliqun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-07-26 07:22:51 +0000

    REEB-597
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@2288 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- operate/report/app/controllers/Application.java
+++ operate/report/app/controllers/Application.java
@@ -1,12 +1,16 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
 import java.util.*;
 
 import models.*;
 
+import operate.rbac.annotations.ActiveNavigation;
+
+import play.*;
+import play.mvc.*;
+
+@With(OperateRbac.class)
+@ActiveNavigation("reports_app")
 public class Application extends Controller {
 
     public static void index() {
