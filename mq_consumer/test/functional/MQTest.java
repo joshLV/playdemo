9ef93fb94dc5a92a7243520e3ca1bdcb458b2e19
yanjingyun <yanjingyun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-07-31 07:01:52 +0000

    reeb-763
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@2364 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- mq_consumer/test/functional/MQTest.java
+++ mq_consumer/test/functional/MQTest.java
@@ -30,7 +30,7 @@ public class MQTest extends FunctionalTest{
             RabbitMQPublisher.publish(MQTestConsumer.QUEUE, "test message" + i);
         }
 
-        Thread.sleep(5000);
+        Thread.sleep(10000);
         List<MQTestJournal> journals = MQTestJournal.findAll();
         assertTrue(journals.size() > 0);
         Logger.info("first consumed time:" + format.format(journals.get(0).createdAt));
