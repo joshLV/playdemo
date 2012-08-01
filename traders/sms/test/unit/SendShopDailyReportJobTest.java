26e542da2382eecff954243a9def9bc836f9322e
likang <likang@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-08-01 08:56:27 +0000

    reeb-762
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@2376 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ traders/sms/test/unit/SendShopDailyReportJobTest.java
@@ -0,0 +1,20 @@
+package unit;
+
+import org.junit.Before;
+import org.junit.Test;
+import play.test.UnitTest;
+
+/**
+ * @author likang
+ *         Date: 12-8-1
+ */
+public class SendShopDailyReportJobTest extends UnitTest{
+    @Before
+    public void setup(){
+
+    }
+    @Test
+    public void jobTest(){
+
+    }
+}
