abf662b457d9d89aa71e4e30f98f0030dd1ff50f
wangjia <wangjia@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-08-01 02:37:49 +0000

    REEB-822
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@2368 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ website/www/test/functional/FindPasswordTest.java
@@ -0,0 +1,127 @@
+/**
+ * 
+ */
+package functional;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import models.cms.VoteQuestion;
+import models.consumer.User;
+import models.consumer.UserVote;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import play.cache.Cache;
+import play.mvc.Http.Response;
+import play.test.Fixtures;
+import play.test.FunctionalTest;
+import controllers.modules.website.cas.Security;
+
+/**
+ * @author wangjia
+ * @date 2012-7-31 下午1:14:50 
+ */
+public class FindPasswordTest extends FunctionalTest{
+
+	
+	@Before
+	@SuppressWarnings("unchecked")
+	public void setup() {
+		Fixtures.delete(User.class);
+		Fixtures.loadModels("fixture/user.yml");
+
+		Long userId = (Long) Fixtures.idCache.get("models.consumer.User-user");
+		User user = User.findById(userId);
+
+		// 设置测试登录的用户名
+		Security.setLoginUserForTest(user.loginName);
+	}
+
+	@After
+	public void tearDown() {
+		// 清除登录Mock
+		Security.cleanLoginUserForTest();
+	}
+
+	@Test
+	public void testSendMessageCode() {
+		Response response = GET("/send-message_code?from=15618096151");
+		assertStatus(200, response);
+		assertNotNull(response); // this is OK
+		assertIsOk(response); // this is OK 200
+		assertContentType("application/json", response); // this is OK
+		assertCharset("utf-8", response); // this is OK 
+		assertEquals("1", response.out.toString()); // 浏览器相应
+	}
+	
+	@Test
+	public void testCheckMobileMobileNotExist() {
+		Response response = GET("/check-mobile");
+		assertStatus(200, response);
+		assertNotNull(response); // this is OK
+		assertIsOk(response); // this is OK 200
+		assertContentType("application/json", response); // this is OK
+		assertCharset("utf-8", response); // this is OK 
+		assertEquals("3", response.out.toString()); // 浏览器相应	
+	}
+	
+	
+	@Test
+	public void testCheckMobileValidCodeWrong() {
+		Response response = GET("/check-mobile?mobile=15618096151&validCode=123");
+		assertStatus(200, response);
+		assertNotNull(response); // this is OK
+		assertIsOk(response); // this is OK 200
+		assertContentType("application/json", response); // this is OK
+		assertCharset("utf-8", response); // this is OK 
+		assertEquals("1", response.out.toString()); // 浏览器相应	
+	}
+	
+	@Test  
+	public void testCheckMobilePhoneWrong() {
+		
+		//Object objMobile =Cache.set("mobile_","123","30mn");
+		
+		Cache.set("mobile_", "123","30mn");
+		//Object objMobile = Cache.get("mobile_");
+	
+		Response response = GET("/check-mobile?mobile=15618096151&validCode=123456");
+		assertStatus(200, response);
+		assertNotNull(response); // this is OK
+		assertIsOk(response); // this is OK 200
+		assertContentType("application/json", response); // this is OK
+		assertCharset("utf-8", response); // this is OK 
+		assertEquals("2", response.out.toString()); // 浏览器相应	
+		
+	}
+	
+	
+	@Test
+	public void testUpdatePassword() {
+		
+		Map<String, String> passwordParams = new HashMap<>();
+		passwordParams.put("password","123");
+		passwordParams.put("confirmPassword","123");
+		Response response = POST("/reset-password", passwordParams);
+		assertEquals("-1", response.out.toString()); // 浏览器相应	
+	}
+	
+	
+	@Test
+	public void testUpdatePasswordMobileBlank() {
+		
+		Map<String, String> passwordParams = new HashMap<>();
+		passwordParams.put("mobile","15618096151");
+		passwordParams.put("password","123");
+		passwordParams.put("confirmPassword","123");
+		Response response = POST("/reset-password", passwordParams);
+		assertEquals("1", response.out.toString()); // 浏览器相应	
+	}
+	
+	
+	
+
+}
