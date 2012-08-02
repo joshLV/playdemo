2e6e4126c8c8c830a0035317384cdc55ffc25827
likang <likang@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-08-02 10:11:42 +0000

    reeb-762
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@2403 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- module/emaillogs/src/play/modules/emaillogs/EmailLoggerAppender.java
+++ module/emaillogs/src/play/modules/emaillogs/EmailLoggerAppender.java
@@ -6,18 +6,52 @@ import play.Play;
 /**
  * @author likang
  *         Date: 12-7-30
+ *
+ * 自定义 Appender 配置，使其允许从 Play! 的配置文件中读取配置
  */
 public class EmailLoggerAppender extends SMTPAppender{
-    static String receiver = Play.configuration.getProperty("email_log.receiver", "bugs@uhuila.com");
-    static String applicationName = Play.configuration.getProperty("application.name", "");
+    static String applicationName = Play.configuration.getProperty("application.name", null);
+    static String receiver = Play.configuration.getProperty("email_log.receiver", null);
+    static String host = Play.configuration.getProperty("email_log.host", null);
+    static String username = Play.configuration.getProperty("email_log.username", null);
+    static String password = Play.configuration.getProperty("email_log.password", null);
+    static String from = Play.configuration.getProperty("email_log.from", null);
+    static String subject = Play.configuration.getProperty("email_log.subject", null);
+    static String bufferSize = Play.configuration.getProperty("email_log.buffer_size", null);
 
     @Override
-    public String getSubject(){
-        return "[" + applicationName + "] " + super.getSubject();
+    public void setSubject(String s){
+        String prefix = applicationName == null ? "" : "[" + applicationName + "] ";
+        super.setSubject(prefix + (subject == null ? s : subject));
     }
 
     @Override
-    public String getTo(){
-        return receiver;
+    public void setTo(String to){
+        super.setTo(receiver == null ? to : receiver);
+    }
+
+    @Override
+    public void setSMTPHost(String h) {
+        super.setSMTPHost(host == null ? h : host);
+    }
+
+    @Override
+    public void setSMTPUsername(String u) {
+        super.setSMTPUsername(username == null ? u : username);
+    }
+
+    @Override
+    public void setSMTPPassword(String p) {
+        super.setSMTPPassword(password == null ? p : password);
+    }
+
+    @Override
+    public void setFrom(String f) {
+        super.setFrom(from == null ? f : from);
+    }
+
+    @Override
+    public void setBufferSize(int bs) {
+        super.setBufferSize(bufferSize == null ? bs : Integer.parseInt(bufferSize));
     }
 }
