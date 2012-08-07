acfbff1a0c00331eb6d94211428d6405b2656827
likang <likang@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-08-07 03:07:30 +0000

    reeb-731
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@2510 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- module/emaillogs/src/play/modules/emaillogs/EmailAppenderPlugin.java
+++ module/emaillogs/src/play/modules/emaillogs/EmailAppenderPlugin.java
@@ -1,5 +1,7 @@
 package play.modules.emaillogs;
 
+import org.apache.log4j.Appender;
+import org.apache.log4j.net.SMTPAppender;
 import play.Logger;
 import play.Play;
 import play.PlayPlugin;
@@ -16,6 +18,60 @@ public class EmailAppenderPlugin extends PlayPlugin{
         if(Play.runingInTestMode() || !Play.mode.isProd()){
             Logger.info("remove log4j appender: EMAIL");
             org.apache.log4j.Logger.getRootLogger().removeAppender("EMAIL");
+        }
+    }
+
+    @Override
+    public void onApplicationStart(){
+        Appender appender = org.apache.log4j.Logger.getRootLogger().getAppender("EMAIL");
+        if(appender == null) {
+            return;
+        }
+
+        String applicationName = Play.configuration.getProperty("application.name", null);
+        String receiver = Play.configuration.getProperty("email_log.receiver", null);
+        String host = Play.configuration.getProperty("email_log.host", null);
+        String username = Play.configuration.getProperty("email_log.username", null);
+        String password = Play.configuration.getProperty("email_log.password", null);
+        String from = Play.configuration.getProperty("email_log.from", null);
+        String subject = Play.configuration.getProperty("email_log.subject", null);
+        String bufferSize = Play.configuration.getProperty("email_log.buffer_size", null);
+
+
+        SMTPAppender smtpAppender = (SMTPAppender) appender;
+
+        if (applicationName != null || subject != null) {
+            String subjectPrefix =  applicationName == null ? "" : ("[" + applicationName + "] ");
+            subject = subject == null ? smtpAppender.getSubject() : subject;
+            smtpAppender.setSubject(subjectPrefix + subject);
+        }
+
+        if (receiver != null) {
+            smtpAppender.setTo(receiver);
+        }
+
+        if (host != null) {
+            smtpAppender.setSMTPHost(host);
+        }
+
+        if (username != null) {
+            smtpAppender.setSMTPUsername(username);
+        }
+
+        if (password != null) {
+            smtpAppender.setSMTPPassword(password);
+        }
+
+        if (from != null) {
+            smtpAppender.setFrom(from);
+        }
+
+        if (bufferSize != null) {
+            try{
+                smtpAppender.setBufferSize(Integer.parseInt(bufferSize));
+            }catch (NumberFormatException e) {
+                //ignore
+            }
         }
     }
 }
--- module/emaillogs/src/play/modules/emaillogs/EmailLoggerAppender.java
+++ module/emaillogs/src/play/modules/emaillogs/EmailLoggerAppender.java
@@ -1,6 +1,7 @@
 package play.modules.emaillogs;
 
 import org.apache.log4j.net.SMTPAppender;
+import org.apache.log4j.spi.LoggingEvent;
 import play.Logger;
 import play.Play;
 
@@ -11,55 +12,9 @@ import play.Play;
  * 自定义 Appender 配置，使其允许从 Play! 的配置文件中读取配置
  */
 public class EmailLoggerAppender extends SMTPAppender{
-    static String applicationName = Play.configuration.getProperty("application.name", null);
-    static String receiver = Play.configuration.getProperty("email_log.receiver", null);
-    static String host = Play.configuration.getProperty("email_log.host", null);
-    static String username = Play.configuration.getProperty("email_log.username", null);
-    static String password = Play.configuration.getProperty("email_log.password", null);
-    static String from = Play.configuration.getProperty("email_log.from", null);
-    static String subject = Play.configuration.getProperty("email_log.subject", null);
-    static String bufferSize = Play.configuration.getProperty("email_log.buffer_size", null);
 
     @Override
-    public void setSubject(String s){
-        Logger.info("set email logger subject. applicationName: %s, subject[conf]: %s, subject[param]: %s", applicationName, subject, s);
-        String prefix = applicationName == null ? "" : "[" + applicationName + "] ";
-        super.setSubject(prefix + (subject == null ? s : subject));
-    }
-
-    @Override
-    public void setTo(String to){
-        Logger.info("set email logger receiver. receiver[conf]: %s, receiver[param]: %s", receiver, to);
-        super.setTo(receiver == null ? to : receiver);
-    }
-
-    @Override
-    public void setSMTPHost(String h) {
-        Logger.info("set email logger SMTPHost. host[conf]: %s, host[param]: %s", host, h);
-        super.setSMTPHost(host == null ? h : host);
-    }
-
-    @Override
-    public void setSMTPUsername(String u) {
-        Logger.info("set email logger SMTPUser. user[conf]: %s, user[param]: %s", username, u);
-        super.setSMTPUsername(username == null ? u : username);
-    }
-
-    @Override
-    public void setSMTPPassword(String p) {
-        Logger.info("set email logger SMTPPassword. password[conf]: %s, password[param]: %s", password, p);
-        super.setSMTPPassword(password == null ? p : password);
-    }
-
-    @Override
-    public void setFrom(String f) {
-        Logger.info("set email logger From. from[conf]: %s, from[param]: %s", from, f);
-        super.setFrom(from == null ? f : from);
-    }
-
-    @Override
-    public void setBufferSize(int bs) {
-        Logger.info("set email logger buffer. buffer[conf]: %s, buffer[param]: %s", bufferSize, bs);
-        super.setBufferSize(bufferSize == null ? bs : Integer.parseInt(bufferSize));
+    public void append(LoggingEvent event) {
+        super.append(event);
     }
 }
