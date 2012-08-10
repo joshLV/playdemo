8a70bd0653c2c3fc91e1d5266d8fef403e1ebf1b
wangjia <wangjia@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-08-10 10:03:56 +0000

    REEB-872
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@2594 96626eaa-6fa6-4472-8f5b-47c901fe3adc

--- operate/business/app/controllers/OperatePointGoodsOrders.java
+++ operate/business/app/controllers/OperatePointGoodsOrders.java
@@ -1,17 +1,16 @@
 package controllers;
 
-import models.accounts.AccountType;
-import models.order.Order;
-import models.order.OrderItems;
-import models.order.OrdersCondition;
+import models.sales.PointGoodsOrder;
+import models.sales.PointGoodsOrderCondition;
 import operate.rbac.annotations.ActiveNavigation;
 import org.apache.commons.lang.StringUtils;
 import play.modules.paginate.JPAExtPaginator;
 import play.mvc.Controller;
 import play.mvc.With;
 
-import java.math.BigDecimal;
-import java.util.List;
+//import models.order.Order;
+//import models.order.OrderItems;
+//import models.order.OrdersCondition;
 
 /**
  * Created with IntelliJ IDEA.
@@ -33,33 +32,63 @@ public class OperatePointGoodsOrders extends Controller {
      *
      * @param condition 页面条件信息
      */
-    public static void index(OrdersCondition condition) {
+    public static void index(PointGoodsOrderCondition condition) {
         if (condition == null) {
-            condition = new OrdersCondition();
+            condition = new PointGoodsOrderCondition();
         }
         String page = request.params.get("page");
         int pageNumber = StringUtils.isEmpty(page) ? 1 : Integer.parseInt(page);
-        JPAExtPaginator<Order> orderList =
-                models.order.Order.query(condition, null, pageNumber, PAGE_SIZE);
+        JPAExtPaginator<PointGoodsOrder> orderList =
+        models.sales.PointGoodsOrder.query(condition,pageNumber, PAGE_SIZE);
 
-        BigDecimal amountSummary = Order.summary(orderList);
+//        BigDecimal amountSummary = PointGoodsOrder.findUserTotalPoint  (orderList);
 //        List<Brand> brandList = Brand.findByOrder(null);
 //        renderArgs.put("brandList", brandList);
-        render(orderList, condition, amountSummary);
+//        render(orderList, condition, amountSummary);
+        render(orderList, condition);
 
     }
 
     /**
-     * 订单发货.
+     * 审核.
      */
-    public static void send(Long id, Order order) {
-        Order originalOrder = Order.findById(id);
+    //Long id,     PointGoodsOrder order
+    public static void send(Long id,PointGoodsOrder pointGoodsOrder) {
+   PointGoodsOrder originalOrder = PointGoodsOrder.findById(id);
         if (originalOrder == null) {
-            error(500, "can not find the order:" + id);
+            error(500, "can not deal with the order:" + id);
         }
-        Order.sendRealGoodsAndPayCommissions(id, order.deliveryCompany, order.deliveryNo);
+
+        if(originalOrder.note==null)
+        originalOrder.acceptOrder(id);
+        else
+        originalOrder.cancelOrder(id,pointGoodsOrder.note);
+
         index(null);
     }
+
+    /**
+     *
+     * @param id
+     * @param pointGoodsOrder
+     */
+//    订单发货
+
+    public static void sendGoods(Long id,PointGoodsOrder pointGoodsOrder) {
+        PointGoodsOrder originalOrder = PointGoodsOrder.findById(id);
+        if (originalOrder == null) {
+            error(500, "can not deal with the order:" + id);
+        }
+
+
+            originalOrder.sendGoods(id,pointGoodsOrder.note);
+
+        index(null);
+    }
+
+
+
+
 
     /**
      * 积分商品订单详细
@@ -69,32 +98,35 @@ public class OperatePointGoodsOrders extends Controller {
     public static void details(Long id) {
         //订单信息
         models.order.Order orders = models.order.Order.findById(id);
-        List<OrderItems> orderItems = orders.orderItems;
+//        List<OrderItems> orderItems = orders.orderItems;
         //收货信息
-        render(orders, orderItems);
+//        render(orders, orderItems);
     }
 
 
-    public static void orderExcelOut(OrdersCondition condition) {
+    public static void orderExcelOut(PointGoodsOrderCondition condition) {
 
         if (condition == null) {
-            condition = new OrdersCondition();
+            condition = new PointGoodsOrderCondition();
         }
         String page = request.params.get("page");
         request.format = "xls";
         String __EXCEL_FILE_NAME__ = "订单_" + System.currentTimeMillis() + "xls";
         renderArgs.put("__EXCEL_FILE_NAME__", __EXCEL_FILE_NAME__);
-        JPAExtPaginator<models.order.Order> orderList = models.order.Order.query(condition, null, 1, PAGE_SIZE);
-        for (Order order : orderList) {
-            if (order.userType == AccountType.CONSUMER) {
-                order.accountEmail = order.getUser().loginName;
-            } else if (order.userType == AccountType.RESALER) {
-                order.accountEmail = order.getResaler().loginName;
-            }
+//        JPAExtPaginator<models.order.Order> orderList = models.order.Order.query(condition, null, 1, PAGE_SIZE);
+//        for (PointGoodsOrder order : orderList) {
+//            if (order.userType == AccountType.CONSUMER) {
+//                order.accountEmail = order.getUser().loginName;
+//            } else if (order.userType == AccountType.RESALER) {
+//                order.accountEmail = order.getResaler().loginName;
+//            }
 //            order.save();
-        }
-        render(__EXCEL_FILE_NAME__, orderList);
+//        }
+//        render(__EXCEL_FILE_NAME__, orderList);
 
     }
+
+
+
 
 }
