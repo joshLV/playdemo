7c41e0a3f8c4b1eec6cc53e8791e17dacdfaaa84
tangliqun <tangliqun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-08-16 12:16:59 +0000

    REEB-904
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@2686 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ operate/business/test/function/OperateDiscountCodesTest.java
@@ -0,0 +1,52 @@
+package function;
+
+import java.math.BigDecimal;
+import models.admin.OperateUser;
+import models.order.DiscountCode;
+import operate.rbac.RbacLoader;
+import org.junit.Before;
+import org.junit.Test;
+import play.mvc.Http.Response;
+import play.test.FunctionalTest;
+import play.vfs.VirtualFile;
+import controllers.operate.cas.Security;
+import factory.FactoryBoy;
+import factory.SequenceCallBack;
+
+public class OperateDiscountCodesTest extends FunctionalTest {
+
+    @Before
+    public void setUp() {
+
+        FactoryBoy.lazyDelete();
+
+        // 重新加载配置文件
+        VirtualFile file = VirtualFile.open("conf/rbac.xml");
+        RbacLoader.init(file);
+                
+        OperateUser user = FactoryBoy.create(OperateUser.class);
+        // 设置测试登录的用户名
+        Security.setLoginUserForTest(user.loginName);
+    }
+    
+    @Test
+    public void 测试正常访问列表页() {
+        FactoryBoy.batchCreate(10, DiscountCode.class, new SequenceCallBack<DiscountCode>() {
+            @Override
+            public void sequence(DiscountCode target, int seq) {
+                target.discountAmount = BigDecimal.TEN;
+                target.discountSn = "TEST" + seq;
+            }
+        });
+        
+        Response response = GET("/discountcodes");
+
+        
+        assertIsOk(response);
+        assertContentType("text/html", response);
+        System.out.println(getContent(response));
+        
+        assertNull(renderArgs("discountSN"));
+        assertNotNull(renderArgs("discountCodePage"));
+    }
+}
