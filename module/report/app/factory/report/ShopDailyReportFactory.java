c0c98596f9414f23d5bb845925b6c18515d1661f
wangjia <wangjia@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-08-22 08:11:02 +0000

    REEB-919
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@2784 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ module/report/app/factory/report/ShopDailyReportFactory.java
@@ -0,0 +1,30 @@
+package factory.report;
+
+import factory.FactoryBoy;
+import factory.ModelFactory;
+import models.report.ShopDailyReport;
+import models.sales.Shop;
+
+import java.math.BigDecimal;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: wangjia
+ * Date: 12-8-20
+ * Time: 下午5:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class ShopDailyReportFactory extends ModelFactory<ShopDailyReport> {
+    @Override
+    public ShopDailyReport define() {
+        ShopDailyReport report = new ShopDailyReport();
+       Shop shop=FactoryBoy.create(Shop.class);
+        report.shop=shop;
+        report.buyCount = (long)20.4;
+        report.orderCount = (long)5;
+        report.originalAmount = BigDecimal.valueOf(100);
+        return report;
+
+    }
+
+}
