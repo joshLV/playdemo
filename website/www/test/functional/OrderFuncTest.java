e66d936f50401ce9b8f42145309ff7472d647d02
hejun <hejun@96626eaa-6fa6-4472-8f5b-47c901fe3adc>
2012-08-22 07:26:45 +0000

    REEB-912 为每个module添加 factory-boy 依赖
    git-svn-id: http://svn.uhuila.cn/reeb/trunk@2779 96626eaa-6fa6-4472-8f5b-47c901fe3adc

---
+++ website/www/test/functional/OrderFuncTest.java
@@ -0,0 +1,70 @@
+package functional;
+
+import controllers.modules.website.cas.Security;
+import models.consumer.User;
+import models.order.Order;
+import models.order.OrderItems;
+import models.sales.*;
+import models.supplier.Supplier;
+import org.junit.Before;
+import org.junit.Test;
+import play.mvc.Http;
+import play.test.Fixtures;
+import play.test.FunctionalTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hejun
+ * Date: 12-8-22
+ * Time: 下午2:17
+ * To change this template use File | Settings | File Templates.
+ */
+public class OrderFuncTest extends FunctionalTest {
+
+    @Before
+    public void setup() {
+        Fixtures.delete(Order.class);
+        Fixtures.delete(User.class);
+        Fixtures.delete(Goods.class);
+        Fixtures.delete(OrderItems.class);
+        Fixtures.delete(Order.class);
+        Fixtures.delete(Shop.class);
+        Fixtures.delete(Goods.class);
+        Fixtures.delete(Category.class);
+        Fixtures.delete(Brand.class);
+        Fixtures.delete(Area.class);
+        Fixtures.delete(Supplier.class);
+        Fixtures.loadModels("fixture/orders.yml");
+        Fixtures.loadModels("fixture/user.yml");
+        Fixtures.loadModels("fixture/supplier_unit.yml");
+        Fixtures.loadModels("fixture/areas_unit.yml");
+        Fixtures.loadModels("fixture/categories_unit.yml");
+        Fixtures.loadModels("fixture/brands_unit.yml");
+        Fixtures.loadModels("fixture/shops_unit.yml");
+        Fixtures.loadModels("fixture/goods_unit.yml");
+        Fixtures.loadModels("fixture/orders.yml");
+
+        //设置虚拟登陆
+        Long userId = (Long) Fixtures.idCache.get("models.consumer.User-user");
+        User user = User.findById(userId);
+
+        Security.setLoginUserForTest(user.loginName);
+    }
+
+    @Test
+    public void testCreate(){
+
+        Map<String,String> params =new HashMap<>();
+        String order ="101-1,";
+        params.put("items",order);
+        params.put("mobile","13800001111");
+        params.put("remark","hehe");
+
+        Http.Response response =POST("/orders/new",params);
+        assertStatus(302,response);
+
+    }
+}
