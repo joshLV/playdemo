05a363d04b9f5b6b1d0cce27cc53730e6cd83fdd
yanjingyun <yanjingyun@uhuila.com>
2012-09-06 16:43:09 +0800

    返利排名及返利功能完善

---
+++ website/www/app/controllers/WEBApplication.java
@@ -0,0 +1,51 @@
+package controllers;
+
+import controllers.modules.website.cas.SecureCAS;
+import controllers.modules.website.cas.annotations.SkipCAS;
+import models.cms.Topic;
+import play.mvc.Controller;
+import play.mvc.With;
+
+/**
+ * .
+ * <p/>
+ * User: sujie
+ * Date: 5/22/12
+ * Time: 6:56 PM
+ */
+@With({SecureCAS.class, WebsiteInjector.class})
+@SkipCAS
+public class WEBApplication extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+    public static void about() {
+        render();
+    }
+
+    public static void help() {
+        render();
+    }
+
+    public static void rebate() {
+        render();
+    }
+
+    public static void contact() {
+        render();
+    }
+
+    public static void service() {
+        render();
+    }
+
+    public static void topic(Long id) {
+        Topic topic = Topic.findById(id);
+        if (topic == null) {
+            error(404, "没有找到该商品！");
+        }
+        render(topic);
+    }
+}
