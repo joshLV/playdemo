23c261d12210fa49619b5e26c730cd2eaa61a8b5
sujie <sujie@uhuila.com>
2012-09-25 15:34:51 +0800

    解决jenkins编译的问题

--- mq_consumer/app/models/taobao/TaobaoCometConsumer.java
+++ mq_consumer/app/models/taobao/TaobaoCometConsumer.java
@@ -148,7 +148,7 @@ public class TaobaoCometConsumer extends RabbitMQConsumer<TaobaoCometMessage>{
         String mobile =  taobaoOrder.getTrade().getReceiverMobile();
 
         // 遍历所有的子订单(也就是我们所谓的orderItem),生成我们的订单
-        List<com.taobao.api.domain.Order> taobaoOrders = taobaoOrder.getTrade().getOrders();
+        List<models.order.Order> taobaoOrders = taobaoOrder.getTrade().getOrders();
         models.order.Order order = models.order.Order.createConsumeOrder(userId, AccountType.RESALER);
         for(Order tOrder: taobaoOrders){
             ResalerFav resalerFav = ResalerFav.find("byTaobaoItemId", tOrder.getNumIid()).first();
---
+++ operate/business/test/function/DDAPIUtilTest.java
@@ -0,0 +1,56 @@
+package function;
+
+import factory.FactoryBoy;
+import models.dangdang.DDAPIInvokeException;
+import models.dangdang.DDAPIUtil;
+import models.order.ECoupon;
+import models.sales.Goods;
+import org.junit.Test;
+import play.mvc.Before;
+import play.test.FunctionalTest;
+
+/**
+ * TODO.
+ * <p/>
+ * User: sujie
+ * Date: 9/25/12
+ * Time: 2:58 PM
+ */
+public class DDAPIUtilTest extends FunctionalTest {
+    @Before
+    public void setup() {
+        FactoryBoy.lazyDelete();
+    }
+
+    @Test
+    public void tesSyncSellCount() {
+        Goods goods = FactoryBoy.create(Goods.class);
+        try {
+            DDAPIUtil.syncSellCount(goods);
+        } catch (DDAPIInvokeException e) {
+            assert false;
+        }
+    }
+
+    @Test
+    public void testIsRefund() {
+        ECoupon ecoupon = FactoryBoy.create(ECoupon.class);
+        try {
+            boolean isRefund = DDAPIUtil.isRefund(ecoupon);
+            assertFalse(isRefund);
+        } catch (DDAPIInvokeException e) {
+            assert false;
+        }
+    }
+
+    @Test
+    public void testNotifyVerified() {
+        ECoupon ecoupon = FactoryBoy.create(ECoupon.class);
+        try {
+            DDAPIUtil.notifyVerified(ecoupon);
+        } catch (DDAPIInvokeException e) {
+            assert false;
+        }
+    }
+
+}
