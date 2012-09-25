7f7da945f1d501f0e94f61ca09c57ef5292c31f0
tangliqun <tangliqun@uhuila.com>
2012-09-25 12:13:57 +0800

    使用不同的ztsms productid

--- mq_consumer/app/models/sms/impl/ZtSMSProvider.java
+++ mq_consumer/app/models/sms/impl/ZtSMSProvider.java
@@ -48,7 +48,7 @@ public class ZtSMSProvider implements SMSProvider {
         qparams.add(new BasicNameValuePair("username", USERNAME));
         qparams.add(new BasicNameValuePair("password", PASSWORD));
         qparams.add(new BasicNameValuePair("xh", message.getCode()));
-        qparams.add(new BasicNameValuePair("productid", "156041"));
+        qparams.add(new BasicNameValuePair("productid", "887361"));
         /*
         try {
             qparams.add(new BasicNameValuePair("Content", URLEncoder.encode(message.getContent(), "GBK")));
