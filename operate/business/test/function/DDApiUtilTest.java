f3cf9bd8e4776725a7c124898fe59e17b6cf10b2
sujie <sujie@uhuila.com>
2012-09-25 15:09:46 +0800

    解决jenkins编译的问题

---
+++ operate/business/test/function/DDApiUtilTest.java
@@ -0,0 +1,15 @@
+package function;
+
+import play.test.FunctionalTest;
+
+/**
+ * TODO.
+ * <p/>
+ * User: sujie
+ * Date: 9/25/12
+ * Time: 2:58 PM
+ */
+public class DDApiUtilTest extends FunctionalTest{
+
+
+}
