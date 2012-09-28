f2bcde6cc8afb5c266eb2f93b537367d79e47318
wangjia <wangjia@uhuila.com>
2012-09-28 11:58:35 +0800

    tuan360 xml without category and logerror(sendemailtodev)

--- website/www/app/controllers/TuanFeeds.java
+++ website/www/app/controllers/TuanFeeds.java
@@ -9,28 +9,37 @@ import cache.CacheHelper;
 public class TuanFeeds extends Controller {
 
     public static void tuan800(final long categoryId) {
-        render(getTopGoods(categoryId));
+        //精选商品        
+        List<models.sales.Goods> goodsList = CacheHelper.getCache(CacheHelper.getCacheKey(models.sales.Goods.CACHEKEY, "WWW_TUAN_TOPS"), new CacheCallBack<List<models.sales.Goods>>() {
+            @Override
+            public List<models.sales.Goods> loadData() {
+                return getTopGoods(categoryId);
+            }
+        });
+
+        render(goodsList);
     }
 
     public static void tuan360(final long categoryId) {
-        render(getTopGoods(categoryId));
-    }
-
-
-
-    private static List<models.sales.Goods> getTopGoods(final long categoryId) {
-        return CacheHelper.getCache(CacheHelper.getCacheKey(models.sales.Goods.CACHEKEY, "WWW_TUAN_TOPS"), new CacheCallBack<List<models.sales.Goods>>() {
+        //精选商品
+        List<models.sales.Goods> goodsList = CacheHelper.getCache(CacheHelper.getCacheKey(models.sales.Goods.CACHEKEY, "WWW_TUAN_TOPS"), new CacheCallBack<List<models.sales.Goods>>() {
             @Override
             public List<models.sales.Goods> loadData() {
-                List<models.sales.Goods> goodsList = null;
-                if (categoryId == 0) {
-                    goodsList = models.sales.Goods.findTop(6);
-                } else {
-                    goodsList = models.sales.Goods.findTopByCategory(categoryId, 6);
-                }
-                return goodsList;
+                return getTopGoods(categoryId);
             }
         });
+
+        render(goodsList);
     }
-    
+
+    private static List<models.sales.Goods> getTopGoods(long categoryId) {
+        List<models.sales.Goods> goodsList;
+        if (categoryId == 0) {
+            goodsList = models.sales.Goods.findTop(6);
+        } else {
+            goodsList = models.sales.Goods.findTopByCategory(categoryId, 6);
+        }
+        return goodsList;
+    }
+
 }
