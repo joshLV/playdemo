ed269873ca7a9f771cd8d0d688796f358b3e1c55
sujie <sujie@uhuila.com>
2012-10-11 11:45:16 +0800

    修复单元测试用例的错误

--- supplier/sales/test/unit/ShopUnitTest.java
+++ supplier/sales/test/unit/ShopUnitTest.java
@@ -2,12 +2,10 @@ package unit;
 
 import com.uhuila.common.constants.DeletedStatus;
 import factory.FactoryBoy;
-import models.sales.Area;
 import models.sales.Shop;
 import org.junit.Before;
 import org.junit.Test;
 import play.modules.paginate.ModelPaginator;
-import play.test.Fixtures;
 import play.test.UnitTest;
 
 import java.util.List;
@@ -16,11 +14,6 @@ public class ShopUnitTest extends UnitTest {
     Shop shop;
     @Before
     public void setup() {
-        Fixtures.delete(Shop.class);
-        Fixtures.delete(Area.class);
-        Fixtures.loadModels("fixture/areas_unit.yml");
-        Fixtures.loadModels("fixture/shops_unit.yml");
-
         FactoryBoy.lazyDelete();
         shop = FactoryBoy.create(Shop.class);
 
@@ -42,17 +35,16 @@ public class ShopUnitTest extends UnitTest {
     @Test
     public void testQuery() {
         Shop condition = new Shop();
-        condition.name = "宛平南路";
-        condition.address = "宛平南路";
-        condition.supplierId = 1;
+        condition.name = "shop";
+        condition.address = "宛平南路2号";
+        condition.supplierId = shop.supplierId;
         ModelPaginator<Shop> shops = Shop.query(condition, 1, 10);
         assertEquals(1, shops.size());
     }
 
     @Test
     public void testDelete() {
-        Long shopId = (Long) Fixtures.idCache.get("models.sales" +
-                ".Shop-Shop_5");
+        Long shopId = shop.id;
         boolean result = Shop.delete(shopId);
         assertTrue(result);
 
@@ -86,7 +78,7 @@ public class ShopUnitTest extends UnitTest {
 
     @Test
     public void testHasMap_纬度为00() {
-        shop.longitude = "0.0";
+        shop.latitude = "0.0";
         assertFalse(shop.hasMap());
     }
 
@@ -98,7 +90,7 @@ public class ShopUnitTest extends UnitTest {
 
     @Test
     public void testHasMap_纬度为空() {
-        shop.longitude = null;
+        shop.latitude = null;
         assertFalse(shop.hasMap());
     }
 }
