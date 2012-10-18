3d2c96da87327f81f4b99a85177d30674c3cb0ce
wangjia <wangjia@uhuila.com>
2012-10-18 15:00:05 +0800

    add the creation of goodshistory in OrderItemsFactory

--- module/order/app/factory/order/OrderItemsFactory.java
+++ module/order/app/factory/order/OrderItemsFactory.java
@@ -5,12 +5,14 @@ import factory.ModelFactory;
 import models.order.Order;
 import models.order.OrderItems;
 import models.sales.Goods;
+import models.sales.GoodsHistory;
 
 public class OrderItemsFactory extends ModelFactory<OrderItems> {
 
     @Override
     public OrderItems define() {
         Goods goods = FactoryBoy.lastOrCreate(Goods.class);
+        GoodsHistory goodsHistory=FactoryBoy.lastOrCreate(GoodsHistory.class);
         Order order = FactoryBoy.lastOrCreate(Order.class);
         return new OrderItems(order, goods, 1l, "13800000000", goods.salePrice, goods.salePrice);
     }
