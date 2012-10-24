d35d5997e286877ad717b46ea814e04dfe60d8fd
wangjia <wangjia@uhuila.com>
2012-10-24 19:22:30 +0800

    test intermediate tables in goodsupdate related to shops

--- operate/business/test/unit/GoodsUpdateTest.java
+++ operate/business/test/unit/GoodsUpdateTest.java
@@ -6,9 +6,17 @@ import models.sales.SecKillGoods;
 import models.sales.Shop;
 import org.junit.Before;
 import org.junit.Test;
+import play.db.DB;
+import play.db.jpa.JPA;
+import play.test.Fixtures;
 import play.test.UnitTest;
 
+import javax.persistence.Query;
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashSet;
 
 /**
@@ -28,16 +36,32 @@ public class GoodsUpdateTest extends UnitTest {
     }
 
     @Test
-    public void testUpdate() {
+    public void testUpdate() throws Exception {
+        // 全部门店
+        goods.isAllShop = true;
+        goods.shops.clear();
+        goods.save();
+        assertEquals(true, goods.isAllShop);
+        assertEquals(0, getTableCount("goods_shops"));
+        Goods.count();
+        // 单个门店
         BigDecimal faceValue = new BigDecimal("9.00");
         goods.faceValue = faceValue;
-        goods.isAllShop = false;
-        goods.shops.clear();
         Shop testShop = FactoryBoy.create(Shop.class, "SupplierId");
         goods.shops.add(testShop);
-        goods.save();
         Goods.update(goods.id, goods, false);
+        assertEquals(1, goods.shops.size());
         assertEquals(testShop.name, goods.shops.iterator().next().name);
         assertEquals(faceValue, goods.faceValue);
+        assertEquals(1, getTableCount("goods_shops"));
+    }
+
+    public int getTableCount(String tableName) throws SQLException {
+
+
+        Query q = JPA.em().createNativeQuery("select count(*) from \"" + tableName + "\"");
+        BigInteger count = (BigInteger)q.getSingleResult();
+
+        return count.intValue();
     }
 }
