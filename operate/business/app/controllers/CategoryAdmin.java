f839707cd4c3bfd550ebf3300e7e34702d616974
wangjia <wangjia@uhuila.com>
2012-10-25 10:27:45 +0800

    add CategoryAmin controller and view

---
+++ operate/business/app/controllers/CategoryAdmin.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import operate.rbac.annotations.ActiveNavigation;
+import play.mvc.Controller;
+import play.mvc.With;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: wangjia
+ * Date: 12-10-25
+ * Time: 上午10:14
+ * To change this template use File | Settings | File Templates.
+ */
+@With(OperateRbac.class)
+@ActiveNavigation("category_admin_index")
+public class CategoryAdmin extends Controller {
+    public static void index() {
+        render();
+    }
+
+}
