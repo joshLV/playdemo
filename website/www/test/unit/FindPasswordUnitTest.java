b033e427db8b6c6b130d01a838c83134ce5f03e4
wangjia <wangjia@uhuila.com>
2012-10-29 16:38:43 +0800

    replace yml with factoryboy in FindPasswordTest

--- website/www/test/unit/FindPasswordUnitTest.java
+++ website/www/test/unit/FindPasswordUnitTest.java
@@ -5,58 +5,54 @@ import models.consumer.User;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Before;
 import org.junit.Test;
-
+import factory.FactoryBoy;
+import factory.callback.SequenceCallback;
 import play.test.Fixtures;
 import play.test.UnitTest;
+
 public class FindPasswordUnitTest extends UnitTest {
-	@Before
-	public void setup() {
-		Fixtures.delete(User.class);
-		Fixtures.loadModels("fixture/user.yml");
-	}
-	
-	@Test
-	public void getUser(){
-		Long userId = (Long) Fixtures.idCache.get("models.consumer.User-user_test1");
-		User user= User.findById(userId);
-		String loginName ="selenium1@uhuila.com";
-		boolean  isExisted= user.checkAndSendEmail(loginName);
-		assertTrue(isExisted);
-	}
-	
-	@Test
-	public void checkMobile(){
-		Long userId = (Long) Fixtures.idCache.get("models.consumer.User-user_test1");
-		User user= User.findById(userId);
-		String mobile ="15618096151";
-		boolean returnFlag = user.checkMobile(mobile);
-		assertTrue(returnFlag);
-	}
-	
-	@Test
-	public void testUpdateFindPwd(){
-		Long userId = (Long) Fixtures.idCache.get("models.consumer.User-user_test1");
-		User user= User.findById(userId);
+    User user;
 
-		String totken ="63dc778349e8f64e7c1c3b9370848ba2";
-		String password = "654321";
-		user.updateFindPwd(totken, "", "654321");
-		user= User.findById(userId);
-		assertEquals(DigestUtils.md5Hex(password + user.passwordSalt),user.password);
-		
-		String mobile = "15618096151";
-		user.updateFindPwd("", mobile, "654321");
-		user= User.findById(userId);
-		assertEquals(DigestUtils.md5Hex(password + user.passwordSalt),user.password);
-	}
+    @Before
+    public void setup() {
+        FactoryBoy.deleteAll();
+        user = FactoryBoy.create(User.class);
+    }
 
-	@Test
-	public void testIsExpired(){
-		Long userId = (Long) Fixtures.idCache.get("models.consumer.User-user_test2");
-		User user= User.findById(userId);
-		String totken ="63dc778349e8f64e7c1c3b9370848ba1";
-		boolean isExpired  = user.isExpired(totken);
-		assertTrue(isExpired);
-	}
+    @Test
+    public void getUser() {
+        String loginName = user.loginName;
+        boolean isExisted = user.checkAndSendEmail(loginName);
+        assertTrue(isExisted);
+    }
+
+    @Test
+    public void checkMobile() {
+        String mobile = user.mobile;
+        boolean returnFlag = user.checkMobile(mobile);
+        assertTrue(returnFlag);
+    }
+
+    @Test
+    public void testUpdateFindPwd() {
+        String totken = "63dc778349e8f64e7c1c3b9370848ba2";
+        String password = "654321";
+        user.updateFindPwd(totken, "", "654321");
+        user = User.findById(user.id);
+        assertEquals(DigestUtils.md5Hex(password + user.passwordSalt), user.password);
+
+        String mobile = "15618096151";
+        user.updateFindPwd("", mobile, "654321");
+        user = User.findById(user.id);
+        assertEquals(DigestUtils.md5Hex(password + user.passwordSalt), user.password);
+    }
+
+    @Test
+    public void testIsExpired() {
+        String totken = "63dc778349e8f64e7c1c3b9370848ba1";
+        boolean isExpired = user.isExpired(totken);
+        assertTrue(isExpired);
+    }
+
 
 }
