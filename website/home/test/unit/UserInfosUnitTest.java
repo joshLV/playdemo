abce3cac83335ae094d89ea5358eeb9cde2b287b
wangjia <wangjia@uhuila.com>
2012-11-12 09:57:26 +0800

    test userInfo in website/home/test/unit

--- website/home/test/unit/UserInfosUnitTest.java
+++ website/home/test/unit/UserInfosUnitTest.java
@@ -8,51 +8,36 @@ import org.junit.Test;
 
 import play.test.Fixtures;
 import play.test.UnitTest;
+import factory.FactoryBoy;
+
 public class UserInfosUnitTest extends UnitTest {
-	@Before
-	public void setup() {
-		Fixtures.delete(User.class);
-		Fixtures.delete(UserInfo.class);
-		Fixtures.loadModels("fixture/user.yml");
-		Fixtures.loadModels("fixture/userInfo.yml");
-		Long userId = (Long) Fixtures.idCache.get("models.consumer.User-selenium");
+    User user;
+    UserInfo userInfo;
 
-		User user= User.findById(userId);
+    @Before
+    public void setup() {
+        FactoryBoy.deleteAll();
+        user = FactoryBoy.create(User.class);
+        userInfo = FactoryBoy.create(UserInfo.class);
+        userInfo.user = user;
+        userInfo.save();
+    }
 
-		Long userInfoId = (Long) Fixtures.idCache.get("models.consumer.UserInfo-userInfo1");
-		UserInfo userInfo= UserInfo.findById(userInfoId);
-
-		userInfo.user = user;
-		userInfo.save();
-	}
-
-	@Test
-	public void testUpdateMobile(){
-		Long userInfoId = (Long) Fixtures.idCache.get("models.consumer.UserInfo-userInfo1");
-		UserInfo userInfo= UserInfo.findById(userInfoId);
-
-		UserInfo userInfo1= new UserInfo();
-		userInfo1.fullName="小小";
-		userInfo1.birthday="2001-01-01";
-		String interests = "1,2";
-
-		userInfo.update(userInfo1, interests);
-
-		assertEquals("小小",userInfo.fullName);
-		assertEquals("2001-01-01",userInfo.birthday);
-	}
+    @Test
+    public void testUpdateMobile() {
+        UserInfo userInfo1 = new UserInfo();
+        userInfo1.fullName = "小小";
+        userInfo1.birthday = "2001-01-01";
+        String interests = "1,2";
+        userInfo.update(userInfo1, interests);
+        assertEquals(userInfo1.fullName, userInfo.fullName);
+        assertEquals(userInfo1.birthday, userInfo.birthday);
+    }
 
 
-	@Test
-	public void testFindByUser(){
-		Long userId = (Long) Fixtures.idCache.get("models.consumer.User-selenium");
-		User user= User.findById(userId);
-
-		Long userInfoId = (Long) Fixtures.idCache.get("models.consumer.UserInfo-userInfo1");
-		UserInfo userInfo= UserInfo.findById(userInfoId);
-		userInfo.findByUser(user);
-
-		assertEquals("2001-01-01",userInfo.birthday);
-	}
+    @Test
+    public void testFindByUser() {
+        assertNotNull(userInfo.findByUser(user));
+    }
 
 }
