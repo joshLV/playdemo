51f139978378ff7c6592fee5a144b0d88397120f
tangliqun <tangliqun@uhuila.com>
2012-11-12 10:02:49 +0800

    格式化代码:website/home/test/functional/UserCartsFunctionalTest.java，去除无用代码

--- website/home/test/functional/UserCartsFunctionalTest.java
+++ website/home/test/functional/UserCartsFunctionalTest.java
@@ -1,26 +1,25 @@
 package functional;
 
-import controllers.UserCarts;
-import controllers.modules.website.cas.Security;
-import factory.FactoryBoy;
+import java.util.List;
+
 import models.consumer.User;
 import models.order.Cart;
 import models.sales.Category;
 import models.sales.Goods;
 import models.sales.Shop;
 import models.supplier.Supplier;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import play.mvc.Http;
-import play.test.Fixtures;
-import play.test.FunctionalTest;
 
-import java.util.List;
+import play.mvc.Http;
+import play.test.FunctionalTest;
+import controllers.modules.website.cas.Security;
+import factory.FactoryBoy;
 
 /**
- * User: hejun
- * Date: 12-7-27
+ * User: hejun Date: 12-7-27
  */
 public class UserCartsFunctionalTest extends FunctionalTest {
 
@@ -35,7 +34,7 @@ public class UserCartsFunctionalTest extends FunctionalTest {
         FactoryBoy.create(Goods.class);
         FactoryBoy.create(Cart.class);
 
-        //设置测试登录的用户名
+        // 设置测试登录的用户名
         Security.setLoginUserForTest(user.loginName);
     }
 
@@ -46,17 +45,16 @@ public class UserCartsFunctionalTest extends FunctionalTest {
     }
 
     @Test
-    public void testTops(){
-        Goods goods = FactoryBoy.last(Goods.class);
+    public void testTops() {
         Http.Response response = GET("/carts/tops");
         assertIsOk(response);
-        int count = (Integer)renderArgs("count");
+        int count = (Integer) renderArgs("count");
         assertEquals(1, count);
-        assertEquals(1, ((List<Cart>)renderArgs("carts")).size());
+        assertEquals(1, ((List<Cart>) renderArgs("carts")).size());
     }
 
     @Test
-    public void testDelete(){
+    public void testDelete() {
         User user = FactoryBoy.last(User.class);
         Goods goods = FactoryBoy.last(Goods.class);
         String cookieValue = "";
@@ -64,11 +62,10 @@ public class UserCartsFunctionalTest extends FunctionalTest {
         int oldSize = cartList.size();
 
         Http.Response response = DELETE("/carts/" + goods.id);
-        cartList = Cart.findAll(user,cookieValue);
+        cartList = Cart.findAll(user, cookieValue);
         int newSize = cartList.size();
         assertIsOk(response);
-        //删除成功， 购物车中商品数减一。
-        assertEquals(oldSize-1,newSize);
-
+        // 删除成功， 购物车中商品数减一。
+        assertEquals(oldSize - 1, newSize);
     }
 }
