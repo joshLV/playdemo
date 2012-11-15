0fcbae3db28b1e7727d632a05a38f831206a0d15
likang <likang@uhuila.com>
2012-11-15 10:51:25 +0800

    测试用例 lk 1115

--- module/cms/app/factory/cms/TopicFactory.java
+++ module/cms/app/factory/cms/TopicFactory.java
@@ -4,27 +4,25 @@ import com.uhuila.common.constants.DeletedStatus;
 import com.uhuila.common.constants.PlatformType;
 import factory.ModelFactory;
 import models.cms.Topic;
-import models.cms.TopicType;
-import util.DateHelper;
+
+import java.util.Date;
 
 /**
- * Created with IntelliJ IDEA.
- * User: wangjia
- * Date: 12-11-15
- * Time: 上午10:29
- * To change this template use File | Settings | File Templates.
+ * @author likang
+ *         Date: 12-11-15
  */
-public class TopicFactory extends ModelFactory<Topic> {
+public class TopicFactory extends ModelFactory<Topic>{
     @Override
     public Topic define() {
-        Topic t = new Topic();
-        t.title = "测试公告开始啦";
-        t.effectiveAt = DateHelper.t("2012-3-21 00:00:00");
-        t.expireAt = DateHelper.t("2013-3-21 00:00:00");
-        t.displayOrder = 100;
-        t.platformType = PlatformType.UHUILA;
-        t.type = TopicType.NOTICE;
-        t.deleted = DeletedStatus.UN_DELETED;
-        return t;
+        Topic topic = new Topic();
+        topic.deleted = DeletedStatus.UN_DELETED;
+        topic.type = TopicType.NOTICE;
+        topic.platformType = PlatformType.UHUILA;
+        topic.effectiveAt = new Date();
+        topic.expireAt = new Date(System.currentTimeMillis() + 60*1000*60*24*30);
+        topic.displayOrder = 100;
+        topic.setContent("测试内容如下");
+        topic.title = "测试公告开始啦";
+        return topic;
     }
 }
--- operate/cms/test/functional/OperateTopicsFunctionalTest.java
+++ operate/cms/test/functional/OperateTopicsFunctionalTest.java
@@ -2,6 +2,7 @@ package functional;
 
 import com.uhuila.common.constants.DeletedStatus;
 import controllers.operate.cas.Security;
+import factory.FactoryBoy;
 import models.admin.OperateRole;
 import models.admin.OperateUser;
 import models.cms.Topic;
@@ -18,31 +19,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created with IntelliJ IDEA.
  * User: Juno
  * Date: 12-7-27
  * Time: 上午11:25
- * To change this template use File | Settings | File Templates.
  */
 public class OperateTopicsFunctionalTest extends FunctionalTest {
 
     @Before
     public void setup() {
-        Fixtures.delete(Topic.class);
-        Fixtures.delete(OperateUser.class);
-        Fixtures.delete(OperateRole.class);
-        Fixtures.loadModels("fixture/topics.yml");
-
-        Fixtures.loadModels("fixture/roles.yml");
-        Fixtures.loadModels("fixture/users.yml");
-
-
+        FactoryBoy.deleteAll();
         // 重新加载配置文件
         VirtualFile file = VirtualFile.open("conf/rbac.xml");
         RbacLoader.init(file);
 
-        Long id = (Long) Fixtures.idCache.get("models.admin.OperateUser-user3");
-        OperateUser user = OperateUser.findById(id);
+
+        OperateUser user = FactoryBoy.create(OperateUser.class);
+        FactoryBoy.create(Topic.class);
+
+
         // 设置测试登录的用户名
         Security.setLoginUserForTest(user.loginName);
     }
@@ -98,46 +92,51 @@ public class OperateTopicsFunctionalTest extends FunctionalTest {
 
     @Test
     public void testEdit(){
-        long id = (Long) Fixtures.idCache.get("models.cms.Topic-Test");
-        assertNotNull(id);
+        Topic topic = FactoryBoy.last(Topic.class);
+        assertNotNull(topic);
 
-        Http.Response response = GET("/topics/" + id + "/edit");
+        Http.Response response = GET("/topics/" + topic.id + "/edit");
         assertIsOk(response);
         assertContentMatch("修改公告",response);
     }
 
     @Test
     public void testUpdate(){
-        long id = (Long) Fixtures.idCache.get("models.cms.Topic-Test");
+        Topic topic = FactoryBoy.last(Topic.class);
+        assertNotNull(topic);
+
         String params = "topic.title=TestTitle123456789" +
                 "&topic.displayOrder=1" +
                 "&topic.effectiveAt=2012-3-21" +
                 "&topic.expireAt=2013-3-21" +
                 "&topic.content=Test Content";
-        Http.Response response =  PUT("/topics/"+id,"application/x-www-form-urlencoded",params);
+        Http.Response response =  PUT("/topics/" + topic.id, "application/x-www-form-urlencoded", params);
         assertStatus(302,response);
-        Topic topic = Topic.findById(id);
+        topic.refresh();
         assertTrue((topic.title).equals("TestTitle123456789"));
     }
 
     @Test
     public void testUpdateWithError(){
-        long id = (Long) Fixtures.idCache.get("models.cms.Topic-Test");
+        Topic topic = FactoryBoy.last(Topic.class);
+        assertNotNull(topic);
+
         String params = "topic.title=TestTitle";
         // 传一个空的参数给UPDATE，更新不成功
-        Http.Response response =  PUT("/topics/"+id,"application/x-www-form-urlencoded",params);
+        Http.Response response =  PUT("/topics/"+topic.id,"application/x-www-form-urlencoded",params);
         assertStatus(200,response);
-        Topic topic = Topic.findById(id);
+        topic.refresh();
         assertFalse((topic.title).equals("TestTitle"));
     }
 
     @Test
     public void testDelete(){
-        long id = (Long) Fixtures.idCache.get("models.cms.Topic-Test");
-        Http.Response response = DELETE("/topics/"+id);
+        Topic topic = FactoryBoy.last(Topic.class);
+        assertNotNull(topic);
+
+        Http.Response response = DELETE("/topics/"+topic.id);
         assertStatus(302,response);
-        Topic topic = Topic.findById(id);
+        topic.refresh();
         assertEquals(DeletedStatus.DELETED,topic.deleted);
     }
-
 }
