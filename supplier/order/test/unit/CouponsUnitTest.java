4bc7bbd28f16fa44156b6a28d29b9bdbb25955dc
tangliqun <tangliqun@uhuila.com>
2012-11-15 17:16:47 +0800

    单元测试一天一个: CouponsUnitTest.

--- supplier/order/test/unit/CouponsUnitTest.java
+++ supplier/order/test/unit/CouponsUnitTest.java
@@ -1,45 +1,57 @@
 package unit;
 
-import factory.FactoryBoy;
-import models.accounts.Account;
-import models.accounts.AccountType;
-import models.consumer.User;
-import models.order.*;
-import models.sales.Area;
-import models.sales.Brand;
-import models.sales.Category;
-import models.sales.Goods;
-import org.junit.Before;
-import org.junit.Test;
-import play.modules.paginate.JPAExtPaginator;
-import play.test.Fixtures;
-import play.test.UnitTest;
-
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import models.accounts.AccountType;
+import models.consumer.User;
+import models.order.CouponHistory;
+import models.order.CouponsCondition;
+import models.order.ECoupon;
+import models.order.ECouponStatus;
+import models.order.Order;
+import models.order.OrderItems;
+import models.sales.Goods;
+import models.supplier.Supplier;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import play.modules.paginate.JPAExtPaginator;
+import play.test.UnitTest;
+import util.DateHelper;
+import factory.FactoryBoy;
+import factory.callback.BuildCallback;
+import factory.callback.SequenceCallback;
+
 public class CouponsUnitTest extends UnitTest {
+    Supplier supplier;
+    User user;
+    Goods goods;
+    Order order;
+    ECoupon ecoupon;
+    
     @Before
     public void setup() {
-        FactoryBoy.delete(CouponHistory.class);
-        Fixtures.delete(Category.class);
-        Fixtures.delete(Brand.class);
-        Fixtures.delete(Area.class);
-        Fixtures.delete(Order.class);
-        Fixtures.delete(OrderItems.class);
-        Fixtures.delete(Goods.class);
-        Fixtures.delete(User.class);
-        Fixtures.delete(ECoupon.class);
-        Fixtures.delete(Account.class);
-        Fixtures.loadModels(
-                "fixture/goods_base.yml",
-                "fixture/user.yml",
-                "fixture/goods.yml",
-                "fixture/accounts.yml",
-                "fixture/orders.yml",
-                "fixture/orderItems.yml");
+        FactoryBoy.deleteAll();
+        supplier = FactoryBoy.create(Supplier.class);
+        user = FactoryBoy.create(User.class);
+        goods = FactoryBoy.create(Goods.class);
+        order = FactoryBoy.create(Order.class, new BuildCallback<Order>() {
+            @Override
+            public void build(Order o) {
+                o.userId = user.id;
+                o.userType = AccountType.CONSUMER;
+                o.createdAt = DateHelper.beforeDays(3);
+            }
+        });
+        ecoupon = FactoryBoy.create(ECoupon.class);
+        FactoryBoy.batchCreate(3, ECoupon.class, new SequenceCallback<ECoupon>() {
+            @Override
+            public void sequence(ECoupon e, int seq) {
+            }
+        });
     }
 
 
@@ -48,10 +60,7 @@ public class CouponsUnitTest extends UnitTest {
      */
     @Test
     public void testQueryCoupons() {
-        Long supplierId = 1l;
-        int pageNumber = 1;
-        int pageSize = 15;
-        List<ECoupon> list = ECoupon.queryCoupons(supplierId, pageNumber, pageSize);
+        List<ECoupon> list = ECoupon.queryCoupons(supplier.id, 1, 15);
         assertEquals(4, list.size());
 
     }
@@ -62,18 +71,16 @@ public class CouponsUnitTest extends UnitTest {
     @Test
     public void testUserQueryCoupons() throws ParseException {
         CouponsCondition condition = new CouponsCondition();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        condition.createdAtBegin = sdf.parse("2012-03-01");
+        condition.createdAtBegin = DateHelper.beforeDays(30);
         condition.createdAtEnd = new Date();
         condition.status = ECouponStatus.UNCONSUMED;
         condition.goodsName = "";
-        Long userId = (Long) Fixtures.idCache.get("models.consumer.User-user");
-        condition.userId = userId;
+        condition.userId = user.id;
         condition.accountType = AccountType.CONSUMER;
         int pageNumber = 1;
         int pageSize = 15;
         JPAExtPaginator<ECoupon> list = ECoupon.query(condition, pageNumber, pageSize);
-        assertEquals(0, list.size());
+        assertEquals(4, list.size());
     }
 
     /**
@@ -81,13 +88,21 @@ public class CouponsUnitTest extends UnitTest {
      */
     @Test
     public void testECoupon() {
-        Long goodsId = (Long) Fixtures.idCache.get("models.sales.Goods-Goods_001");
-        Goods goods = Goods.findById(goodsId);
-        Long orderId = (Long) Fixtures.idCache.get("models.order.Order-order1");
-        Order order = Order.findById(orderId);
+        final Order order1 = FactoryBoy.create(Order.class);
+        List<OrderItems> list = FactoryBoy.batchCreate(3, OrderItems.class,
+                        new SequenceCallback<OrderItems>() {
+                            @Override
+                            public void sequence(OrderItems oi, int seq) {
+                                oi.goods = FactoryBoy.create(Goods.class);
+                                oi.order = order1;
+                            }
+                        });
+        order1.orderItems = list;
+        order1.save();
+        assertEquals(3, list.size());
         ECoupon coupon = null;
-        for (OrderItems orderItem : order.orderItems) {
-            coupon = new ECoupon(order, goods, orderItem);
+        for (OrderItems orderItem : order1.orderItems) {
+            coupon = new ECoupon(order1, orderItem.goods, orderItem);
             assertNotNull(coupon);
             assertNotNull(coupon.replyCode);
             assertEquals(4, coupon.replyCode.length());
@@ -97,10 +112,9 @@ public class CouponsUnitTest extends UnitTest {
 
     @Test
     public void testFreeze() {
-        Long id = (Long) Fixtures.idCache.get("models.order.ECoupon-coupon1");
-        ECoupon.freeze(id, null);
-        ECoupon eCoupon = ECoupon.findById(id);
-        assertEquals(1, eCoupon.isFreeze);
+        ECoupon.freeze(ecoupon.id, null);
+        ECoupon e = ECoupon.findById(ecoupon.id);
+        assertEquals(1, e.isFreeze);
         assertEquals(1, CouponHistory.count());
         List<CouponHistory> historyList = CouponHistory.findAll();
         assertEquals("冻结券号", historyList.get(0).remark);
@@ -109,10 +123,9 @@ public class CouponsUnitTest extends UnitTest {
 
     @Test
     public void testUnFreeze() {
-        Long id = (Long) Fixtures.idCache.get("models.order.ECoupon-coupon2");
-        ECoupon.unfreeze(id, null);
-        ECoupon eCoupon = ECoupon.findById(id);
-        assertEquals(0, eCoupon.isFreeze);
+        ECoupon.unfreeze(ecoupon.id, null);
+        ECoupon e = ECoupon.findById(ecoupon.id);
+        assertEquals(0, e.isFreeze);
         assertEquals(1, CouponHistory.count());
         List<CouponHistory> historyList = CouponHistory.findAll();
         assertEquals("解冻券号", historyList.get(0).remark);
@@ -120,18 +133,16 @@ public class CouponsUnitTest extends UnitTest {
 
     @Test
     public void testSendMessage() {
-        Long id = (Long) Fixtures.idCache.get("models.order.ECoupon-coupon2");
-
-        boolean sendFlag = ECoupon.sendMessage(id);
+        boolean sendFlag = ECoupon.sendMessage(ecoupon.id);
         assertTrue(sendFlag);
 
-        String phone = "A3905623568";
-        ECoupon.sendUserMessage(id, phone);
-        ECoupon eCoupon = ECoupon.findById(id);
+        String phone = user.mobile;
+        ECoupon.sendUserMessage(ecoupon.id, phone);
+        ECoupon eCoupon = ECoupon.findById(ecoupon.id);
         assertEquals(2, eCoupon.downloadTimes.intValue());
-        ECoupon.sendUserMessage(id, phone);
+        ECoupon.sendUserMessage(ecoupon.id, phone);
         assertEquals(1, eCoupon.downloadTimes.intValue());
-        ECoupon.sendUserMessage(id, phone);
+        ECoupon.sendUserMessage(ecoupon.id, phone);
         assertEquals(0, eCoupon.downloadTimes.intValue());
 
     }
