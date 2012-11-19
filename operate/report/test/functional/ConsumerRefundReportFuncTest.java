12027ab140ae8646e4c6cf8f4dc406071608c973
likang <likang@uhuila.com>
2012-11-19 10:36:35 +0800

    测试用例lk 1116

--- operate/report/test/functional/ConsumerRefundReportFuncTest.java
+++ operate/report/test/functional/ConsumerRefundReportFuncTest.java
@@ -16,7 +16,9 @@ import play.mvc.Http;
 import play.test.FunctionalTest;
 import play.vfs.VirtualFile;
 
+import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 /**
  * <p/>
@@ -33,52 +35,15 @@ public class ConsumerRefundReportFuncTest extends FunctionalTest {
             public void build(ECoupon target) {
                 target.refundAt = new Date();
                 target.status = ECouponStatus.REFUND;
+                target.refundPrice = target.salePrice;
             }
         });
-
-        OperateUser operateUser = FactoryBoy.create(OperateUser.class);
-
-        /*
-        Fixtures.delete(OperateUser.class);
-        Fixtures.delete(OperateRole.class);
-        Fixtures.delete(OrderItems.class);
-        Fixtures.delete(Order.class);
-        Fixtures.delete(Goods.class);
-        Fixtures.delete(Shop.class);
-        Fixtures.delete(Category.class);
-        Fixtures.delete(Brand.class);
-        Fixtures.delete(Supplier.class);
-        Fixtures.delete(ECoupon.class);
-        Fixtures.loadModels("fixture/suppliers_unit.yml");
-        Fixtures.loadModels("fixture/categories_unit.yml");
-        Fixtures.loadModels("fixture/brands_unit.yml");
-        Fixtures.loadModels("fixture/shops_unit.yml");
-        Fixtures.loadModels("fixture/goods_unit.yml");
-        Fixtures.loadModels("fixture/orders.yml");
-        Fixtures.loadModels("fixture/ecoupon.yml");
-        Fixtures.loadModels("fixture/user.yml");
-        Fixtures.loadModels("fixture/resaler.yml");
-        */
         VirtualFile file = VirtualFile.open("conf/rbac.xml");
         RbacLoader.init(file);
 
+        OperateUser operateUser = FactoryBoy.create(OperateUser.class);
         // 设置测试登录的用户名
         Security.setLoginUserForTest(operateUser.loginName);
-
-        /*
-        id = (Long) Fixtures.idCache.get("models.order.ECoupon-ecoupon_001");
-        ECoupon coupon = ECoupon.findById(id);
-        coupon.refundAt = new Date();
-        coupon.status = ECouponStatus.REFUND;
-        coupon.save();
-        coupon.refresh();
-        id = (Long) Fixtures.idCache.get("models.order.ECoupon-ecoupon_002");
-        coupon = ECoupon.findById(id);
-        coupon.refundAt = new Date();
-        coupon.status = ECouponStatus.REFUND;
-        coupon.save();
-        coupon.refresh();
-        */
     }
 
     @After
@@ -95,7 +60,14 @@ public class ConsumerRefundReportFuncTest extends FunctionalTest {
         ValuePaginator<RefundReport> reportPage = (ValuePaginator<RefundReport>) renderArgs("reportPage");
         assertEquals(1, reportPage.getRowCount());
         RefundReport summary = (RefundReport) renderArgs("summary");
-        assertEquals(160, summary.totalAmount.intValue());
+        BigDecimal refundPrice = BigDecimal.ZERO;
+        List<ECoupon> allCoupon = ECoupon.findAll();
+        for(ECoupon coupon : allCoupon){
+            refundPrice = refundPrice.add(coupon.refundPrice);
+        }
+
+
+        assertEquals(0, refundPrice.compareTo(summary.totalAmount));
         assertEquals(2, summary.buyNumber.intValue());
     }
 
