35506a843c51744703013e2c4ffd515dbb018f00
yanjingyun <yanjingyun@uhuila.com>
2012-11-20 11:47:18 +0800

    一天一测试

--- resale/home/test/unit/ResalerFavUnitTest.java
+++ resale/home/test/unit/ResalerFavUnitTest.java
@@ -1,113 +1,62 @@
 package unit;
 
+import factory.FactoryBoy;
 import models.resale.Resaler;
 import models.resale.ResalerFav;
-import models.sales.*;
+import models.sales.Goods;
 import org.junit.Before;
 import org.junit.Test;
-import play.test.Fixtures;
 import play.test.UnitTest;
+import util.DateHelper;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+
 public class ResalerFavUnitTest extends UnitTest {
+    Resaler resaler;
+    ResalerFav resalerFav;
+    Goods goods;
 
-	@Before
-	@SuppressWarnings("unchecked")
-	public void setup() {
-		Fixtures.delete(Shop.class);
-		Fixtures.delete(Goods.class);
-		Fixtures.delete(Category.class);
-		Fixtures.delete(Brand.class);
-		Fixtures.delete(Area.class);
-		Fixtures.delete(Resaler.class);
-		Fixtures.delete(ResalerFav.class);
-		Fixtures.loadModels("fixture/areas_unit.yml");
-		Fixtures.loadModels("fixture/categories_unit.yml");
-		Fixtures.loadModels("fixture/brands_unit.yml");
-		Fixtures.loadModels("fixture/shops_unit.yml");
-		Fixtures.loadModels("fixture/goods_unit.yml");
-		Fixtures.loadModels("fixture/level_price.yml");
-		Fixtures.loadModels("fixture/resaler.yml");
-		Fixtures.loadModels("fixture/resaler-fav.yml");
-	}
+    @Before
+    public void setup() {
+        FactoryBoy.deleteAll();
+        resalerFav = FactoryBoy.create(ResalerFav.class);
+        resaler = resalerFav.resaler;
+        goods = resalerFav.goods;
+    }
 
-	@Test
-	public void testResalerFav() {
-		List<ResalerFav> favList = ResalerFav.findAll();
-		int favListSize= favList.size();
-		Long resalerId = (Long) Fixtures.idCache.get("models.resale.Resaler-Resaler_1");
-		Resaler resaler =Resaler.findById(resalerId);
+    @Test
+    public void testFindAll() {
+        List<ResalerFav> favList = ResalerFav.findAll();
+        assertEquals(1, favList.size());
+    }
 
-		Long goodsId = (Long) Fixtures.idCache.get("models.sales.Goods-Goods_005");
-		Goods goods =Goods.findById(goodsId);
+    @Test
+    public void testfindFavs() {
+        Date createdAtBegin = DateHelper.beforeDays(3);
+        Date createdAtEnd = new Date();
+        String goodsName = "Product";
+        List<ResalerFav> favList = ResalerFav.findFavs(resaler, createdAtBegin, createdAtEnd, goodsName);
+        assertEquals(1, favList.size());
+    }
 
-		new ResalerFav(resaler,goods).save();
-		List<ResalerFav> newfavList = ResalerFav.findAll();
-		int newfavListSize= newfavList.size();
+    @Test
+    public void testDel() {
+        List<Long> goodsIds = new ArrayList();
+        goodsIds.add(goods.id);
+        int delfav = ResalerFav.delete(resaler, goodsIds);
+        assertEquals(1, delfav);
+    }
 
-		assertEquals(favListSize+1, newfavListSize);
-	}
+    @Test
+    public void testCheckGoods() {
+        Long[] goodsIds = new Long[1];
+        goodsIds[0] = goods.id;
+        Map<String, String> map = ResalerFav.checkGoods(resaler, goodsIds);
 
-	@Test
-	public void testFindAll() {
-
-
-		Long resalerId = (Long) Fixtures.idCache.get("models.resale.Resaler-Resaler_1");
-		Resaler resaler =Resaler.findById(resalerId);
-		List<ResalerFav> favList = ResalerFav.findAll(resaler);
-
-		assertEquals(4, favList.size());
-	}
-
-	@Test
-	public void testfindFavs() {
-		Long resalerId = (Long) Fixtures.idCache.get("models.resale.Resaler-Resaler_1");
-		Resaler resaler =Resaler.findById(resalerId);
-
-		Date createdAtBegin= null;
-		Date createdAtEnd=null;
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		try {
-			createdAtBegin = sdf.parse("2012-03-01");
-			createdAtEnd = new Date();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		String goodsName="券";
-
-		List<ResalerFav> favList = ResalerFav.findFavs(resaler,createdAtBegin,createdAtEnd,goodsName);
-
-		assertEquals(4, favList.size());
-	}
-
-	@Test
-	public void testDel() {
-		Long resalerId = (Long) Fixtures.idCache.get("models.resale.Resaler-Resaler_1");
-		Resaler resaler =Resaler.findById(resalerId);
-		Long goodsId = (Long) Fixtures.idCache.get("models.sales.Goods-Goods_001");
-		List<Long> goodsIds = new ArrayList();
-		goodsIds.add(goodsId);
-
-		int delfav = ResalerFav.delete(resaler,goodsIds);
-		assertEquals(1, delfav);
-	}
-
-
-	@Test
-	public void testCheckGoods() {
-		Long resalerId = (Long) Fixtures.idCache.get("models.resale.Resaler-Resaler_1");
-		Resaler resaler =Resaler.findById(resalerId);
-		Long goodsId = (Long) Fixtures.idCache.get("models.sales.Goods-Goods_004");
-		Long[] goodsIds = new Long[1];
-		goodsIds[0]=goodsId;
-		Map<String, String> map = ResalerFav.checkGoods(resaler,goodsIds);
-
-		assertEquals("1", map.get("isExist"));
-		assertEquals(goodsId.toString(), map.get("goodsId"));
-	}
+        assertEquals("1", map.get("isExist"));
+        assertEquals(goods.id.toString(), map.get("goodsId"));
+    }
 }
