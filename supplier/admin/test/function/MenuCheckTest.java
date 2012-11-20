fd9af26a09099eddb89045910c7d26f9164e0892
wangjia <wangjia@uhuila.com>
2012-11-20 11:52:47 +0800

    replace yml with factoryboy in supplier/admin/test/function/MenuCheckTest

--- supplier/admin/test/function/MenuCheckTest.java
+++ supplier/admin/test/function/MenuCheckTest.java
@@ -1,6 +1,8 @@
 package function;
 
 import java.util.List;
+
+import factory.callback.BuildCallback;
 import models.admin.SupplierRole;
 import models.admin.SupplierUser;
 import models.supplier.Supplier;
@@ -15,8 +17,11 @@ import play.test.Fixtures;
 import play.test.FunctionalTest;
 import play.vfs.VirtualFile;
 import controllers.supplier.cas.Security;
+import factory.FactoryBoy;
+import factory.callback.SequenceCallback;
 
 public class MenuCheckTest extends FunctionalTest {
+    SupplierUser supplierUserAdminSales;
 
     @BeforeClass
     public static void setUpRouter() {
@@ -27,42 +32,95 @@ public class MenuCheckTest extends FunctionalTest {
 
     @Before
     public void setUp() {
-		Fixtures.delete(SupplierUser.class);
-		Fixtures.delete(SupplierRole.class);
-		Fixtures.delete(Supplier.class);
-		Fixtures.loadModels("fixture/roles.yml");
-		Fixtures.loadModels("fixture/supplier_users.yml");
-		
+        FactoryBoy.deleteAll();
+
+        SupplierRole roleSales = FactoryBoy.create(SupplierRole.class, new BuildCallback<SupplierRole>() {
+            @Override
+            public void build(SupplierRole role) {
+                role.text = "销售人员";
+                role.key = "sales";
+            }
+        });
+
+
+        SupplierRole roleAdmin = FactoryBoy.create(SupplierRole.class, new BuildCallback<SupplierRole>() {
+            @Override
+            public void build(SupplierRole role) {
+                role.text = "系统管理员";
+                role.key = "admin";
+            }
+        });
+
+        SupplierRole roleTest = FactoryBoy.create(SupplierRole.class, new BuildCallback<SupplierRole>() {
+            @Override
+            public void build(SupplierRole role) {
+                role.text = "测试角色";
+                role.key = "test";
+            }
+        });
+
+        SupplierRole roleEditor = FactoryBoy.create(SupplierRole.class, new BuildCallback<SupplierRole>() {
+            @Override
+            public void build(SupplierRole role) {
+                role.text = "商品编辑";
+                role.key = "editor";
+            }
+        });
+
+        SupplierRole roleClerk = FactoryBoy.create(SupplierRole.class, new BuildCallback<SupplierRole>() {
+            @Override
+            public void build(SupplierRole role) {
+                role.text = "店员";
+                role.key = "clerk";
+            }
+        });
+
+        SupplierRole roleManager = FactoryBoy.create(SupplierRole.class, new BuildCallback<SupplierRole>() {
+            @Override
+            public void build(SupplierRole role) {
+                role.text = "经理";
+                role.key = "manager";
+            }
+        });
+
+        SupplierRole roleAccount = FactoryBoy.create(SupplierRole.class, new BuildCallback<SupplierRole>() {
+            @Override
+            public void build(SupplierRole role) {
+                role.text = "财务";
+                role.key = "account";
+            }
+        });
+
+        supplierUserAdminSales = FactoryBoy.create(SupplierUser.class, "SalesAdmin");
+
+
         // 加载test/rbac.xml配置文件
         VirtualFile file = VirtualFile.open("test/rbac.xml");
-        RbacLoader.init(file);		        
+        RbacLoader.init(file);
     }
-    
-	@After
-	public void tearDown() {
+
+    @After
+    public void tearDown() {
         // 重新加载配置文件
         VirtualFile file = VirtualFile.open("conf/rbac.xml");
         RbacLoader.init(file);
-        
+
         // 清除登录Mock
         Security.cleanLoginUserForTest();
-	}
-	
-	@Test
-	public void testMenuBaseUrl() {
-		Long id = (Long) Fixtures.idCache.get("models.admin.SupplierUser-user3");
-		SupplierUser user = SupplierUser.findById(id);
-		
+    }
+
+    @Test
+    public void testMenuBaseUrl() {
 //		System.out.println("++++++++++++++++   user.id:" + user.id + ", name:" + user.loginName + ", supplier:" + user.supplier);
-		
+
         // 设置测试登录的用户名
-        Security.setLoginUserForTest(user.loginName);
-		assertIsOk(GET("/singlefoo/user"));
-		
+        Security.setLoginUserForTest(supplierUserAdminSales.loginName);
+        assertIsOk(GET("/singlefoo/user"));
+
 
         List<ContextedMenu> list = (List<ContextedMenu>) renderArgs("topMenus");
 
         assertTrue(list.size() > 0);
         assertTrue(list.get(0).getBaseUrl().indexOf("localhost") > 0);
-	}
+    }
 }
