7b3b5efa5d6d11288ec85ce99b9a10e3a0641530
tangliqun <tangliqun@uhuila.com>
2012-11-22 14:47:29 +0800

    单元测试一天一个：supplier/order/test/unit/VerificationUnitTest.java

--- supplier/order/test/unit/VerificationUnitTest.java
+++ supplier/order/test/unit/VerificationUnitTest.java
@@ -1,58 +1,45 @@
 package unit;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import models.accounts.Account;
 import models.accounts.util.AccountUtil;
-import models.admin.SupplierRole;
 import models.admin.SupplierUser;
-import models.consumer.User;
-import models.order.*;
-import models.sales.Area;
-import models.sales.Brand;
-import models.sales.Category;
+import models.order.ECoupon;
+import models.order.ECouponStatus;
+import models.order.VerifyCouponType;
 import models.sales.Goods;
+import models.sales.Shop;
 import models.supplier.Supplier;
 import navigation.RbacLoader;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 import play.test.Fixtures;
 import play.test.UnitTest;
 import play.vfs.VirtualFile;
-
-import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import factory.FactoryBoy;
 
 public class VerificationUnitTest extends UnitTest {
+    
+    Supplier supplier;
+    Shop shop;
+    SupplierUser supplierUser;
+    Goods goods;
+    ECoupon ecoupon;
+    
     @Before
     public void setup() {
-        Fixtures.delete(Category.class);
-        Fixtures.delete(Brand.class);
-        Fixtures.delete(Area.class);
-        Fixtures.delete(Order.class);
-        Fixtures.delete(OrderItems.class);
-        Fixtures.delete(Goods.class);
-        Fixtures.delete(User.class);
-        Fixtures.delete(ECoupon.class);
-        Fixtures.delete(Account.class);
-        Fixtures.delete(SupplierRole.class);
-        Fixtures.delete(Supplier.class);
-        Fixtures.delete(SupplierUser.class);
-        Fixtures.loadModels("fixture/goods_base.yml", "fixture/roles.yml",
-                "fixture/supplierusers.yml",
-                "fixture/user.yml",
-                "fixture/goods.yml", "fixture/accounts.yml",
-                "fixture/orders.yml",
-                "fixture/orderItems.yml");
-
-        Long supplierId = (Long) Fixtures.idCache.get("models.supplier.Supplier-kfc");
-        List<Goods> goodsList = Goods.findAll();
-        for (Goods goods : goodsList) {
-            goods.supplierId = supplierId;
-            goods.save();
-        }
+        FactoryBoy.deleteAll();
+        
+        supplier = FactoryBoy.create(Supplier.class);
+        shop = FactoryBoy.create(Shop.class);
+        supplierUser = FactoryBoy.create(SupplierUser.class);
+        goods = FactoryBoy.create(Goods.class);
+        ecoupon = FactoryBoy.create(ECoupon.class);
 
         Account account = AccountUtil.getPlatformIncomingAccount();
         account.amount = new BigDecimal("99999");
@@ -69,17 +56,14 @@ public class VerificationUnitTest extends UnitTest {
 
     @Test
     public void testUpdate() {
-        String eCouponSn = "1234567002";
-        Long supplierId = (Long) Fixtures.idCache.get("models.supplier.Supplier-kfc");
-        SupplierUser user = SupplierUser.find("supplier=?", new Supplier(supplierId)).first();
-        ECoupon eCoupon = ECoupon.query(eCouponSn, supplierId);
+        String eCouponSn = ecoupon.eCouponSn;
+        ECoupon eCoupon = ECoupon.query(eCouponSn, supplier.id);
         assertNotNull(eCoupon);
 
-        Long shopId = (Long) Fixtures.idCache.get("models.sales.Shop-Shop_4");
-        eCoupon.consumeAndPayCommission(shopId, null, user, VerifyCouponType.SHOP);
+        eCoupon.consumeAndPayCommission(shop.id, null, supplierUser, VerifyCouponType.SHOP);
         List<ECoupon> couponList = ECoupon.find("byECouponSn", eCouponSn).fetch();
         assertEquals(ECouponStatus.CONSUMED, couponList.get(0).status);
-        assertEquals(shopId, couponList.get(0).shop.id);
+        assertEquals(shop.id, couponList.get(0).shop.id);
     }
 
 
@@ -88,21 +72,22 @@ public class VerificationUnitTest extends UnitTest {
      */
     @Test
     public void testQueryCoupons() {
-        Long supplierId = (Long) Fixtures.idCache.get("models.supplier.Supplier-kfc");
         int pageNumber = 1;
         int pageSize = 15;
-        List<ECoupon> list = ECoupon.queryCoupons(supplierId, pageNumber, pageSize);
-        assertEquals(4, list.size());
+        List<ECoupon> list = ECoupon.queryCoupons(supplier.id, pageNumber, pageSize);
+        assertEquals(1, list.size());
 
     }
 
     @Test
     public void testGetConsumedShop() {
-        String eCouponSn = "1234567004";
-        Long supplierId = (Long) Fixtures.idCache.get("models.supplier.Supplier-kfc");
-        ECoupon eCoupon = ECoupon.query(eCouponSn, supplierId);
+        assertEquals("", ecoupon.getConsumedShop());
+        ecoupon.shop = shop;
+        ecoupon.save();
+        String eCouponSn = ecoupon.eCouponSn;
+        ECoupon eCoupon = ECoupon.query(eCouponSn, supplier.id);
         String name = eCoupon.getConsumedShop();
-        assertEquals("优惠拉", name);
+        assertEquals(shop.name, name);
 
     }
 
