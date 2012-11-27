44e20a9cb2af247935b15ba3d78a757c0fbe4e1d
likang <likang@uhuila.com>
2012-11-27 18:41:35 +0800

    58券验证

--- module/order/app/models/order/ECoupon.java
+++ module/order/app/models/order/ECoupon.java
@@ -41,6 +41,7 @@ import models.sms.SMSUtil;
 import models.tsingtuan.TsingTuanOrder;
 import models.tsingtuan.TsingTuanSendOrder;
 
+import models.wuba.WubaUtil;
 import org.apache.commons.lang.StringUtils;
 
 import play.Logger;
@@ -473,6 +474,11 @@ public class ECoupon extends Model {
         if (this.partner == ECouponPartner.JD) {
             if (!JDGroupBuyUtil.verifyOnJingdong(this)) {
                 Logger.info("verify on jingdong failed");
+                return false;
+            }
+        }else if(this.partner == ECouponPartner.WUBA) {
+            if (!WubaUtil.verifyOnWuba(this)) {
+                Logger.info("verify on wuba failed");
                 return false;
             }
         }
