185ef686d8c829061ecd24ce8eb1da85db282b71
wangjia <wangjia@uhuila.com>
2012-12-03 09:48:28 +0800

    replace FindPassword.test.html with FindPasswordTest.java

--- website/www/app/controllers/FindPassword.java
+++ website/www/app/controllers/FindPassword.java
@@ -43,22 +43,22 @@ public class FindPassword extends Controller {
      */
     public static void sendMessageCode(String from) {
         boolean isExisted = false;
-        
+
         if (StringUtils.isNotEmpty(from) && from.indexOf("@") != -1) {
             isExisted = User.checkAndSendEmail(from);
         } else {
-        	
+
             isExisted = User.checkMobile(from);
             //手机存在
             if (isExisted) {
-            	
-                String validCode = RandomNumberUtil.generateSerialNumber(4);                
+
+                String validCode = RandomNumberUtil.generateSerialNumber(4);
                 String comment = "【一百券】您的验证码是" + validCode + ", 请将该号码输入后即可验证成功。如非本人操作，请及时修改密码";
                 SMSUtil.send(comment, from, "0000");
                 //保存手机和验证码
                 Cache.set("validCode_", validCode, "10mn");
                 Cache.set("mobile_", from, "30mn");
-                
+
             }
         }
         renderJSON(isExisted ? DataConstants.ONE.getValue() : DataConstants.ZERO.getValue());
@@ -74,30 +74,30 @@ public class FindPassword extends Controller {
         Object objCode = Cache.get("validCode_");
         Object objMobile = Cache.get("mobile_");
         String cacheValidCode = objCode == null ? "" : objCode.toString();
-        if (Play.runingInTestMode()){
-        	cacheValidCode="123456";
+        if (Play.runingInTestMode()) {
+            cacheValidCode = "123456";
         }
         String cacheMobile = objMobile == null ? "" : objMobile.toString();
         boolean isExisted = User.checkMobile(mobile);
         //手机不存在
-        if (!isExisted) {  
+        if (!isExisted) {
             renderJSON(DataConstants.THREE.getValue());
         }
         //判断验证码是否正确
-       
+
         if (!StringUtils.normalizeSpace(cacheValidCode).equals(validCode)) {
-        	 
+
             renderJSON(DataConstants.ONE.getValue());
         }
         //判断手机是否正确
-       
-      
+
+
         if (!StringUtils.normalizeSpace(cacheMobile).equals(mobile)) {
-        	
+
             renderJSON(DataConstants.TWO.getValue());
         }
         Cache.delete("validCode_");
-       
+
         renderJSON(DataConstants.ZERO.getValue());
     }
 
@@ -121,7 +121,7 @@ public class FindPassword extends Controller {
         if (StringUtils.isBlank(token) && StringUtils.isBlank(mobile)) {
             renderJSON("-1");
         }
-    	
+
         //根据手机有邮箱更改密码
         User.updateFindPwd(token, mobile, password);
 
