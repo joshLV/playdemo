6f4d71ffceb17cdeb2b0c09ae26783f490b0cb8b
wangjia <wangjia@uhuila.com>
2012-12-04 19:52:42 +0800

    search by realser in ResaleSalesReport

--- operate/report/app/models/ResaleSalesReport.java
+++ operate/report/app/models/ResaleSalesReport.java
@@ -4,6 +4,7 @@ import models.accounts.AccountType;
 import models.order.ECoupon;
 import models.order.ECouponStatus;
 import models.order.Order;
+import org.apache.commons.lang.StringUtils;
 import play.db.jpa.JPA;
 import play.db.jpa.Model;
 
@@ -34,11 +35,11 @@ public class ResaleSalesReport extends Model {
     /**
      * 退款券数
      */
-    public long refundNumber;
+    public long refundNumber = 0l;
     /**
      * 消费券数
      */
-    public long consumedNumber;
+    public long consumedNumber = 0l;
 
     /**
      * 售出金额
@@ -48,12 +49,12 @@ public class ResaleSalesReport extends Model {
     /**
      * 退款金额
      */
-    public BigDecimal refundPrice;
+    public BigDecimal refundPrice = BigDecimal.ZERO;
 
     /**
      * 消费金额
      */
-    public BigDecimal consumedPrice;
+    public BigDecimal consumedPrice = BigDecimal.ZERO;
 
     /**
      * 应收款金额
@@ -82,6 +83,38 @@ public class ResaleSalesReport extends Model {
         this.salePrice = salePrice;
         this.buyNumber = buyNumber;
     }
+
+
+    public ResaleSalesReport(BigDecimal consumedPrice, Order order, Long consumedNumber) {
+        this.order = order;
+        if (order != null) {
+            if (order.userType == AccountType.CONSUMER) {
+                this.loginName = "一百券";
+            } else {
+                this.loginName = order.getResaler().loginName;
+                this.userName = order.getResaler().userName;
+            }
+        }
+
+        this.consumedPrice = consumedPrice;
+        this.consumedNumber = consumedNumber;
+    }
+
+    public ResaleSalesReport(BigDecimal refundPrice, Long refundNumber, Order order) {
+        this.order = order;
+        if (order != null) {
+            if (order.userType == AccountType.CONSUMER) {
+                this.loginName = "一百券";
+            } else {
+                this.loginName = order.getResaler().loginName;
+                this.userName = order.getResaler().userName;
+            }
+        }
+
+        this.refundPrice = refundPrice;
+        this.refundNumber = refundNumber;
+    }
+
 
     public ResaleSalesReport(BigDecimal salePrice, Long buyNumber, BigDecimal refundPrice, Long refundCount, BigDecimal consumedPrice, Long consumedCount) {
         this.userName = "一百券";
@@ -126,7 +159,7 @@ public class ResaleSalesReport extends Model {
         List<ResaleSalesReport> paidResultList = query.getResultList();
 
         //consumedAt
-        sql = "select new models.ResaleSalesReport(e.order,sum(e.salePrice),count(e.orderItems.buyNumber)) from ECoupon e ";
+        sql = "select new models.ResaleSalesReport(sum(e.salePrice),e.order,count(e)) from ECoupon e ";
         query = JPA.em()
                 .createQuery(sql + condition.getFilterConsumedAt(AccountType.RESALER) + groupBy + " order by sum(e.salePrice) desc");
         for (String param : condition.getParamMap().keySet()) {
@@ -134,8 +167,7 @@ public class ResaleSalesReport extends Model {
         }
         List<ResaleSalesReport> consumedResultList = query.getResultList();
 
-        //refundAt
-        sql = "select new models.ResaleSalesReport(e.order,sum(e.salePrice),count(e.orderItems.buyNumber)) from ECoupon e ";
+        sql = "select new models.ResaleSalesReport(sum(e.refundPrice),count(e),e.order) from ECoupon e ";
         query = JPA.em()
                 .createQuery(sql + condition.getFilterRefundAt(AccountType.RESALER) + groupBy + " order by sum(e.refundPrice) desc");
         for (String param : condition.getParamMap().keySet()) {
@@ -143,65 +175,34 @@ public class ResaleSalesReport extends Model {
         }
         List<ResaleSalesReport> refundResultList = query.getResultList();
 
-        List<ResaleSalesReport> resultList = new ArrayList<>();
+        Map<Long, ResaleSalesReport> map = new HashMap<>();
 
-        Map<String, ResaleSalesReport> map = new HashMap<>();
+        for (ResaleSalesReport paidItem : paidResultList) {
+            map.put(getReportKey(paidItem), paidItem);
+        }
 
         for (ResaleSalesReport consumedItem : consumedResultList) {
-            map.put(getReportKey(consumedItem), consumedItem);
+            ResaleSalesReport item = map.get(getReportKey(consumedItem));
+            if (item == null) {
+                map.put(getReportKey(consumedItem), consumedItem);
+            }
+            item.consumedPrice = consumedItem.consumedPrice;
+            item.consumedNumber = consumedItem.consumedNumber;
         }
 
         for (ResaleSalesReport refundItem : refundResultList) {
-            map.put(getReportKey(refundItem), refundItem);
+            ResaleSalesReport item = map.get(getReportKey(refundItem));
+            if (item == null) {
+                map.put(getReportKey(refundItem), refundItem);
+            }
+            item.refundPrice = refundItem.refundPrice;
+            item.refundNumber = refundItem.refundNumber;
         }
 
-        for (ResaleSalesReport paidItem : paidResultList) {
-            ResaleSalesReport consumedItem = map.get(getReportKey(paidItem));
-            if (consumedItem != null) {
-                paidItem.consumedNumber = consumedItem.consumedNumber;
-                paidItem.consumedPrice = consumedItem.consumedPrice;
-            }
-            ResaleSalesReport refundItem = map.get(getReportKey(paidItem));
-            if (refundItem != null) {
-                paidItem.refundNumber = refundItem.refundNumber;
-                paidItem.refundPrice = refundItem.refundPrice;
-            }
-            map.remove(getReportKey(paidItem));
+        List resultList = new ArrayList();
+        for (Long key : map.keySet()) {
+            resultList.add(map.get(key));
         }
-
-        Iterator it = map.entrySet().iterator();
-//        while (it.hasNext()) {
-//            if(it.)
-//        }
-
-
-//        List<Order> newList = new ArrayList<Order>();
-//
-//        for (ResaleSalesReport item : resultList) {
-//            long consumedCount = 0l;
-//            long refundCount = 0l;
-//            BigDecimal consumedPrice = BigDecimal.ZERO;
-//            BigDecimal refundPrice = BigDecimal.ZERO;
-//            newList = Order.find("userId=? and userType=?", item.order.userId, AccountType.RESALER).fetch();
-//            for (Order order : newList) {
-//                List<ECoupon> eCoupons = ECoupon.find("order=?", order).fetch();
-//                for (ECoupon coupon : eCoupons) {
-//                    if (coupon.status == ECouponStatus.CONSUMED) {
-//                        consumedCount++;
-//                        consumedPrice = consumedPrice.add(coupon.salePrice == null ? BigDecimal.ZERO : coupon.salePrice);
-//                    }
-//                    if (coupon.status == ECouponStatus.REFUND) {
-//                        refundCount++;
-//                        refundPrice = refundPrice.add(coupon.refundPrice == null ? BigDecimal.ZERO : coupon.refundPrice);
-//                    }
-//
-//                }
-//            }
-//            item.consumedPrice = consumedPrice;
-//            item.consumedNumber = consumedCount;
-//            item.refundNumber = refundCount;
-//            item.refundPrice = refundPrice;
-//        }
 
         return resultList;
     }
@@ -212,6 +213,7 @@ public class ResaleSalesReport extends Model {
      * @param resultList
      * @return
      */
+
     public static ResaleSalesReport summary(List<ResaleSalesReport> resultList) {
         if (resultList == null || resultList.size() == 0) {
             return new ResaleSalesReport(0l, BigDecimal.ZERO, BigDecimal.ZERO, 0l, BigDecimal.ZERO, 0l, BigDecimal.ZERO, BigDecimal.ZERO);
@@ -286,8 +288,9 @@ public class ResaleSalesReport extends Model {
         return newList;
     }
 
-    private static String getReportKey(ResaleSalesReport refoundItem) {
-        return refoundItem.order.userId + "." + refoundItem.salePrice;
+    private static Long getReportKey(ResaleSalesReport refoundItem) {
+        return refoundItem.order.userId;
     }
+
 
 }
