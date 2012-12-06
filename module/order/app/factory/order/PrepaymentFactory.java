e95340bc2c87efba5b9e35e8909619736d21aa31
苏杰 <sujie@uhuila.com>
2012-12-06 13:59:33 +0800

    fix settle.html bug

--- module/order/app/factory/order/PrepaymentFactory.java
+++ module/order/app/factory/order/PrepaymentFactory.java
@@ -1,16 +1,15 @@
 package factory.order;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
+import com.uhuila.common.constants.DeletedStatus;
+import factory.FactoryBoy;
+import factory.ModelFactory;
+import models.accounts.SettlementStatus;
 import models.order.Prepayment;
 import models.supplier.Supplier;
 import util.DateHelper;
 
-import com.uhuila.common.constants.DeletedStatus;
-
-import factory.FactoryBoy;
-import factory.ModelFactory;
+import java.math.BigDecimal;
+import java.util.Date;
 
 public class PrepaymentFactory extends ModelFactory<Prepayment> {
 
@@ -23,6 +22,9 @@ public class PrepaymentFactory extends ModelFactory<Prepayment> {
         prepayment.effectiveAt = DateHelper.beforeDays(3);
         prepayment.expireAt = DateHelper.afterDays(1);
         prepayment.deleted = DeletedStatus.UN_DELETED;
+        prepayment.createdBy = "admin";
+        prepayment.warning = false;
+        prepayment.settlementStatus = SettlementStatus.UNCLEARED;
         prepayment.remark = "测试备注";
         return prepayment;
     }
--- supplier/sms/test/unit/PrepaymentNoticeUnitTest.java
+++ supplier/sms/test/unit/PrepaymentNoticeUnitTest.java
@@ -2,14 +2,19 @@ package unit;
 
 import com.uhuila.common.util.DateUtil;
 import factory.FactoryBoy;
+import models.job.PrepaymentNotice;
+import models.mail.MailMessage;
 import models.order.ECoupon;
+import models.order.ECouponStatus;
 import models.order.Prepayment;
 import org.junit.Before;
 import org.junit.Test;
 import play.test.UnitTest;
+import util.mq.MockMQ;
+import models.mail.MailUtil;
 
+import java.math.BigDecimal;
 import java.text.ParseException;
-import java.util.Date;
 
 /**
  * TODO.
@@ -20,17 +25,41 @@ import java.util.Date;
  */
 public class PrepaymentNoticeUnitTest extends UnitTest {
     Prepayment prepayment;
+    ECoupon eCoupon;
+
     @Before
     public void setup() {
         FactoryBoy.deleteAll();
 
-        ECoupon eCoupon = FactoryBoy.create(ECoupon.class);
+        eCoupon = FactoryBoy.create(ECoupon.class);
         prepayment = FactoryBoy.create(Prepayment.class);
+        prepayment.expireAt = DateUtil.getEndOfDay();
+        prepayment.save();
+        eCoupon.originalPrice = new BigDecimal(9.5);
+        eCoupon.consumedAt = DateUtil.getBeginOfDay();
+        eCoupon.status = ECouponStatus.CONSUMED;
+        eCoupon.save();
     }
 
     @Test
-    public void testJob() throws ParseException {
-        prepayment.expireAt = DateUtil.getEndOfDay(new Date());
-        prepayment.save();
+    public void testNoticeJob() throws ParseException {
+        new PrepaymentNotice().doJob();
+        prepayment.refresh();
+        assertTrue(prepayment.warning);
+
+        MailMessage msg = (MailMessage) MockMQ.getLastMessage(MailUtil.PREPAYMENT_NOTIFY);
+        assertEquals("可用预付款已少于10%", msg.getSubject());
+
+        assertEquals(prepayment.supplier.otherName, msg.getParam("supplier"));
     }
+
+    @Test
+    public void testNoNoticeJob() throws ParseException {
+        eCoupon.originalPrice = new BigDecimal(9);
+        eCoupon.save();
+        new PrepaymentNotice().doJob();
+        prepayment.refresh();
+        assertFalse(prepayment.warning);
+    }
+
 }
