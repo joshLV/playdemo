149ffe85ef54c20047528a63203886df834e371d
yanjingyun <yanjingyun@uhuila.com>
2012-12-10 13:36:05 +0800

    添加商品排期页面

--- operate/business/app/controllers/GoodsSchedule.java
+++ operate/business/app/controllers/GoodsSchedule.java
@@ -1,6 +1,8 @@
 package controllers;
 
+import navigation.annotations.ActiveNavigation;
 import play.mvc.Controller;
+import play.mvc.With;
 
 /**
  * <p/>
@@ -8,8 +10,22 @@ import play.mvc.Controller;
  * Date: 12-12-10
  * Time: 上午11:03
  */
+@With(OperateRbac.class)
+@ActiveNavigation("goods_schedule")
 public class GoodsSchedule extends Controller {
     public static void index() {
+        render();
+    }
+    @ActiveNavigation("goods_schedule_add")
+    public static void add() {
+        render();
+    }
+
+    public static void create() {
+        render();
+    }
+
+    public static void update() {
         render();
     }
 }
