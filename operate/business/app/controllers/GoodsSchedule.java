36bbf61a8c350ac84524e02496276bbafc98ea89
yanjingyun <yanjingyun@uhuila.com>
2012-12-10 11:07:14 +0800

    添加商品排期

---
+++ operate/business/app/controllers/GoodsSchedule.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * <p/>
+ * User: yanjy
+ * Date: 12-12-10
+ * Time: 上午11:03
+ */
+public class GoodsSchedule extends Controller {
+    public static void index() {
+        render();
+    }
+}
