4fff42c991f9c3400a7f2d0468c68d24e8859afb
tangliqun <tangliqun@uhuila.com>
2012-12-12 14:30:31 +0800

    rename mq_consumer/app/controllers/Application.java -> mq_consumer/app/controllers/MqApplication.java

--- mq_consumer/app/controllers/MqApplication.java
+++ mq_consumer/app/controllers/MqApplication.java
@@ -2,7 +2,7 @@ package controllers;
 
 import play.mvc.Controller;
 
-public class Application extends Controller {
+public class MqApplication extends Controller {
 
     public static void index() {
         render();
