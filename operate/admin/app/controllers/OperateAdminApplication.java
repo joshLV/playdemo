45e9c89de96adecd922165e48cb1baaba3cb6a72
tangliqun <tangliqun@uhuila.com>
2012-12-13 14:41:52 +0800

    重命名Application.java.

--- operate/admin/app/controllers/OperateAdminApplication.java
+++ operate/admin/app/controllers/OperateAdminApplication.java
@@ -5,7 +5,7 @@ import play.mvc.Controller;
 import play.mvc.With;
 
 @With(OperateRbac.class)
-public class Application extends Controller {
+public class OperateAdminApplication extends Controller {
 
     @ActiveNavigation("user_add")
     public static void index() {
