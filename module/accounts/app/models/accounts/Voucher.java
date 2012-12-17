5edf6ba65211d94fec71520763319cba8b306b1d
likang <likang@uhuila.com>
2012-12-17 13:59:08 +0800

    代金券

--- module/accounts/app/models/accounts/Voucher.java
+++ module/accounts/app/models/accounts/Voucher.java
@@ -1,9 +1,11 @@
 package models.accounts;
 
 
+import com.uhuila.common.constants.DeletedStatus;
 import models.accounts.Account;
 import models.order.Order;
 import play.db.jpa.Model;
+import play.modules.paginate.JPAExtPaginator;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -17,13 +19,44 @@ import java.util.Date;
 @Table(name = "vouchers")
 public class Voucher extends Model {
 
+    /**
+     * 序列号.
+     * 保存为英文大写和数字。
+     *
+     * 录入时，编辑可以指定一个前缀，然后生成时用前缀加数字序号即可.
+     * 可以这样做：
+     *      DecimalFormat myFormatter = new DecimalFormat("00000");
+     *      String serialNo = prefix + myFormatter.format(22);
+     */
+    @Column(name="serial_no", unique = true)
+    public String serialNo;
+
+    @Column(name = "prefix")
+    public String prefix;
+
+    /**
+     * 充值码.
+     * 伪随机生成的15位数字
+     */
+    @Column(name="charge_code", unique = true)
+    public String chargeCode;
+
+    /**
+     * 关联的账户
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "account_id", nullable = true)
     public Account account;
 
+    /**
+     * 价值
+     */
     @Column(name = "value")
     public BigDecimal value;
 
+    /**
+     * 关联的订单
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id", nullable = true)
     public Order order;
@@ -32,6 +65,9 @@ public class Voucher extends Model {
     @Column(name="lock_version")
     public int lockVersion;
 
+    /**
+     * 卡片名称
+     */
     @Column(name = "name")
     public String name;
 
@@ -64,4 +100,27 @@ public class Voucher extends Model {
      */
     @Column(name = "expired_at")
     public Date expiredAt;
+
+    /**
+     * 删除状态。
+     * 没有被充值的记录才可被删除，删除后不可再被使用.
+     */
+    @Enumerated
+    public DeletedStatus deleted;
+
+    public Voucher() {
+        createdAt = new Date();
+        lockVersion = 0;
+    }
+
+    public static JPAExtPaginator<Voucher> findByCondition(
+            VoucherCondition condition, int pageNumber, int pageSize) {
+        JPAExtPaginator<Voucher> page = new JPAExtPaginator<>(
+                null, null, Voucher.class, condition.getFilter(), condition.getParams());
+
+        page.orderBy("createdAt DESC");
+        page.setPageNumber(pageNumber);
+        page.setPageSize(pageSize);
+        return page;
+    }
 }
