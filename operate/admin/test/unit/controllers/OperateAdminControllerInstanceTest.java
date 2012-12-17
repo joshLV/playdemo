9a39e4478fd61f5e58d044dd32cb9a502d5f9d2d
tangliqun <tangliqun@uhuila.com>
2012-12-17 10:15:49 +0800

    解决operate/admin/test/unit/controllers/OperateAdminControllerInstanceTest.java编译错.

--- operate/admin/test/unit/controllers/OperateAdminControllerInstanceTest.java
+++ operate/admin/test/unit/controllers/OperateAdminControllerInstanceTest.java
@@ -1,6 +1,9 @@
 package unit.controllers;
 
-import controllers.*;
+import controllers.OperateAdminApplication;
+import controllers.OperateUsers;
+import controllers.OperateUsersPassword;
+import controllers.OperateUsersProfiles;
 import org.junit.Test;
 import play.mvc.Controller;
 import play.test.UnitTest;
@@ -11,7 +14,7 @@ import play.test.UnitTest;
 public class OperateAdminControllerInstanceTest extends UnitTest{
     @Test
     public void controllerInstanceTest(){
-        assertTrue(new Application() instanceof Controller);
+        assertTrue(new OperateAdminApplication() instanceof Controller);
         assertTrue(new OperateUsers() instanceof Controller);
         assertTrue(new OperateUsersPassword() instanceof Controller);
         assertTrue(new OperateUsersProfiles() instanceof Controller);
