a6d35e0641248a1eff40d2de493b8c0264aa984f
yanjingyun <yanjingyun@uhuila.com>
2012-12-25 13:46:34 +0800

    wuba对应商圈映射

---
+++ module/wuba/app/models/wuba/AreaMapping.java
@@ -0,0 +1,37 @@
+package models.wuba;
+
+import play.db.jpa.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * <p/>
+ * User: yanjy
+ * Date: 12-12-25
+ * Time: 上午11:15
+ */
+@Table(name = "wuba_area_mapping")
+@Entity
+public class AreaMapping extends Model {
+    @Column(name = "ybq_circle_name")
+    public String ybqCircleName;
+    @Column(name = "wb_circle_name")
+    public String wbCircleName;
+
+    @Column(name = "created_at")
+    public Date createdAt;
+
+    public AreaMapping(String ybqCircleName, String wbCircleName) {
+        this.wbCircleName = wbCircleName;
+        this.ybqCircleName = ybqCircleName;
+        this.createdAt = new Date();
+    }
+
+    public static boolean isExistedYbqCircleName(String ybqCircleName) {
+        return AreaMapping.find("ybqCircleName", ybqCircleName).fetch().size() > 0;
+    }
+
+}
