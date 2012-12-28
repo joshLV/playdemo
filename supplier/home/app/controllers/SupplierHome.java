4e6131f7470a215642f9db12d5ae08f84b5b6198
tangliqun <tangliqun@uhuila.com>
2012-12-28 11:40:07 +0800

    记住默认的新版首页.

--- supplier/home/app/controllers/SupplierHome.java
+++ supplier/home/app/controllers/SupplierHome.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.admin.SupplierUser;
 import navigation.annotations.ActiveNavigation;
 import play.mvc.Controller;
 import play.mvc.With;
@@ -13,6 +14,14 @@ public class SupplierHome extends Controller {
 
     @ActiveNavigation("home")
     public static void index() {
+
+        SupplierUser supplierUser = SupplierRbac.currentUser();
+
+        // 如果跳转过新验证界面，使用之
+        if ("v2".equals(supplierUser.defaultUiVersion)) {
+            redirect("/ui-version/to/v2");
+        }
+
         render();
     }
 
