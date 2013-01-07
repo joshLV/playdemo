e96bcd018074763f4c795295e8881063bd4b75a0
likang <likang@uhuila.com>
2013-01-07 18:10:42 +0800

    测试分销商购物车

--- resale/home/test/function/ResalerCartsTest.java
+++ resale/home/test/function/ResalerCartsTest.java
@@ -3,6 +3,7 @@ package function;
 import controllers.modules.resale.cas.Security;
 import factory.FactoryBoy;
 import factory.callback.BuildCallback;
+import models.order.Order;
 import models.resale.Resaler;
 import models.resale.ResalerCart;
 import models.resale.ResalerFav;
@@ -13,7 +14,9 @@ import play.mvc.Http;
 import play.mvc.Router;
 import play.test.FunctionalTest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author likang
@@ -79,4 +82,106 @@ public class ResalerCartsTest extends FunctionalTest{
             assertEquals(goodsId, cart.goods.id);
         }
     }
+
+    @Test
+    public void 测试确认购物车() {
+        List<ResalerCart> allCarts = ResalerCart.findAll(resaler);
+        assertTrue(allCarts.size() > 0);
+        assertEquals(0, Order.count());
+
+        StringBuilder favItems = new StringBuilder();
+        for(ResalerCart cart : allCarts) {
+            favItems.append(cart.goods.id) .append("-")
+                    .append(cart.number).append("-").append(cart.phone).append(",");
+        }
+
+        Map<String, String> params = new HashMap<>();
+        params.put("favItems", favItems.toString());
+
+        Http.Response response = POST(Router.reverse("controllers.ResalerCarts.confirmCarts"), params);
+        assertStatus(302, response);
+        assertEquals(0, ResalerCart.findAll(resaler).size());
+        assertEquals(1, Order.count());
+    }
+
+    @Test
+    public void 测试批量添加() {
+        long itemSize = ResalerCart.count("resaler = ? and goods = ?", resaler, goods);
+        ResalerCart singleCart = ResalerCart.find("byResalerAndGoodsAndPhone", resaler,goods, "13472581853").first();
+        long phoneSize = singleCart.number;
+
+        Map<String, String> params = new HashMap<>();
+        params.put("goodsId",String.valueOf(goods.id));
+        params.put("phones", "13472581853 13472581840");
+
+        Http.Response response = POST(Router.reverse("controllers.ResalerCarts.formAdd"), params);
+        assertStatus(302, response);
+
+        assertEquals(itemSize + 1, ResalerCart.count("resaler = ? and goods = ?", resaler, goods));
+        singleCart.refresh();
+        assertEquals(phoneSize + 1, singleCart.number);
+    }
+
+    @Test
+    public void 测试修改订单数量() {
+        ResalerCart singleCart = ResalerCart.find("byResalerAndGoodsAndPhone", resaler,goods, "13472581853").first();
+        long phoneSize = singleCart.number;
+
+        Map<String, String> params = new HashMap<>();
+        params.put("goodsId",String.valueOf(goods.id));
+        params.put("phone", "13472581853");
+        params.put("increment", "1");
+
+        Http.Response response = POST(Router.reverse("controllers.ResalerCarts.reorder"), params);
+        assertIsOk(response);
+
+        singleCart.refresh();
+        assertEquals(phoneSize + 1, singleCart.number);
+    }
+
+    @Test
+    public void 测试批量删除() {
+        assertTrue(ResalerCart.count("resaler = ? and goods = ?", resaler, goods) > 0);
+
+        Map<String, String> params = new HashMap<>();
+        params.put("goodsIds",String.valueOf(goods.id));
+
+        Http.Response response = POST(Router.reverse("controllers.ResalerCarts.formBatchDelete"), params);
+        assertStatus(302, response);
+
+        assertEquals(0, ResalerCart.count("resaler = ? and goods = ?", resaler, goods));
+
+    }
+
+    @Test
+    public void 测试删除单个商品的单个用户() {
+        Long count =  ResalerCart.count("resaler = ? and goods = ?", resaler, goods);
+        assertTrue(count >= 2);
+
+        Map<String, String> params = new HashMap<>();
+        params.put("goodsId",String.valueOf(goods.id));
+        params.put("phone","13472581853");
+
+        Http.Response response = POST(Router.reverse("controllers.ResalerCarts.formDelete"), params);
+        assertStatus(302, response);
+
+        assertEquals(count-1, ResalerCart.count("resaler = ? and goods = ?", resaler, goods));
+    }
+
+    @Test
+    public void 测试json删除单个商品的单个用户() {
+        Long count =  ResalerCart.count("resaler = ? and goods = ?", resaler, goods);
+        assertTrue(count >= 2);
+
+
+        Map<String, String> params = new HashMap<>();
+        params.put("goodsId",String.valueOf(goods.id));
+        params.put("phone","13472581853");
+
+        Http.Response response = POST(Router.reverse("controllers.ResalerCarts.formDelete"), params);
+        assertStatus(302, response);
+
+        assertEquals(count-1, ResalerCart.count("resaler = ? and goods = ?", resaler, goods));
+    }
+
 }
