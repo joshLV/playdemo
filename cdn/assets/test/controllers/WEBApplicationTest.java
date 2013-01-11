c1985c884821b5f4711ea77744c37d20163c93b9
tangliqun <tangliqun@uhuila.com>
2013-01-11 20:36:26 +0800

    删除website/www下goods和homes两个controller类.

---
+++ cdn/assets/test/controllers/WEBApplicationTest.java
@@ -0,0 +1,67 @@
+package controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import play.test.FunctionalTest;
+
+/**
+ * User: tanglq
+ * Date: 13-1-11
+ * Time: 下午8:35
+ */
+public class WEBApplicationTest extends FunctionalTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testIndex() throws Exception {
+
+    }
+
+    @Test
+    public void testAbout() throws Exception {
+
+    }
+
+    @Test
+    public void testHelp() throws Exception {
+
+    }
+
+    @Test
+    public void testRebate() throws Exception {
+
+    }
+
+    @Test
+    public void testContact() throws Exception {
+
+    }
+
+    @Test
+    public void testService() throws Exception {
+
+    }
+
+    @Test
+    public void testRule() throws Exception {
+
+    }
+
+    @Test
+    public void testLink() throws Exception {
+
+    }
+
+    @Test
+    public void testTopic() throws Exception {
+
+    }
+
+    @Test
+    public void testList() throws Exception {
+
+    }
+}
--- website/www/test/unit/controllers/ControllerInstancesTest.java
+++ website/www/test/unit/controllers/ControllerInstancesTest.java
@@ -1,15 +1,11 @@
 package unit.controllers;
 
-import org.junit.Test;
-
-import play.mvc.Controller;
-import play.test.UnitTest;
 import controllers.Addresses;
 import controllers.Areas;
 import controllers.Carts;
 import controllers.FindPassword;
-import controllers.Goods;
-import controllers.Home;
+import controllers.Goods2;
+import controllers.Home2;
 import controllers.OrderResult;
 import controllers.Orders;
 import controllers.PaymentInfo;
@@ -17,6 +13,9 @@ import controllers.PaymentNotify;
 import controllers.Register;
 import controllers.Votes;
 import controllers.WEBApplication;
+import org.junit.Test;
+import play.mvc.Controller;
+import play.test.UnitTest;
 
 /**
  * @author likang
@@ -30,8 +29,8 @@ public class ControllerInstancesTest extends UnitTest{
         assertTrue(new Areas() instanceof Controller);
         assertTrue(new Carts() instanceof Controller);
         assertTrue(new FindPassword() instanceof Controller);
-        assertTrue(new Goods() instanceof Controller);
-        assertTrue(new Home() instanceof Controller);
+        assertTrue(new Goods2() instanceof Controller);
+        assertTrue(new Home2() instanceof Controller);
         assertTrue(new OrderResult() instanceof Controller);
         assertTrue(new Orders() instanceof Controller);
         assertTrue(new PaymentInfo() instanceof Controller);
