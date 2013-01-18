90a22ead6ed264ffcef2bbe1a260706304e5098e
tangliqun <tangliqun@uhuila.com>
2013-01-18 10:31:02 +0800

    先禁用operate/business/test/unit/account/AccountSequenceUtilTest.java

--- operate/business/test/unit/account/AccountSequenceUtilTest.java
+++ operate/business/test/unit/account/AccountSequenceUtilTest.java
@@ -1,6 +1,8 @@
 package unit.account;
 
 import factory.FactoryBoy;
+import org.junit.Ignore;
+import org.junit.Test;
 import play.test.FunctionalTest;
 
 /**
@@ -10,11 +12,16 @@ import play.test.FunctionalTest;
  * Date: 1/11/13
  * Time: 10:21 AM
  */
+@Ignore
 public class AccountSequenceUtilTest extends FunctionalTest {
     @org.junit.Before
     public void setup() {
         FactoryBoy.deleteAll();
     }
 
+    @Test
+    public void testEmpty() throws Exception {
+        // TOOD
+    }
 }
     
