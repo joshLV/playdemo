911332048676732efc28d54046fa944af0ac1be0
yanjingyun <yanjingyun@uhuila.com>
2013-01-22 14:09:01 +0800

    fix人效大类小计的bug

--- operate/report/app/models/PeopleEffectCategoryReportCondition.java
+++ operate/report/app/models/PeopleEffectCategoryReportCondition.java
@@ -98,93 +98,6 @@ public class PeopleEffectCategoryReportCondition implements Serializable {
         return condBuilder.toString();
     }
 
-    public String getFilterConsumedAt() {
-        StringBuilder condBuilder = new StringBuilder(" where e.goods.supplierId =s.id and s.deleted=0 and s.salesId=ou.id and ou.deleted=0 and e.goods.isLottery=false and e.status = models.order.ECouponStatus.CONSUMED");
-
-        Boolean hasSeeReportProfitRight = ContextedPermission.hasPermission("SEE_OPERATION_REPORT_PROFIT");
-        if (!hasSeeReportProfitRight) {
-            condBuilder.append(" and ou.id =:salesId");
-            paramMap.put("salesId", salesId);
-        }
-        if (StringUtils.isNotBlank(userName)) {
-            condBuilder.append(" and ou.userName like :shortName");
-            paramMap.put("shortName", "%" + userName + "%");
-        }
-        if (StringUtils.isNotBlank(jobNumber)) {
-            condBuilder.append(" and ou.jobNumber= :jobNumber");
-            paramMap.put("jobNumber", jobNumber);
-        }
-        if (beginAt != null) {
-            condBuilder.append(" and e.consumedAt >= :createdAtBegin");
-            paramMap.put("createdAtBegin", beginAt);
-        }
-        if (endAt != null) {
-            condBuilder.append(" and e.consumedAt < :createdAtEnd");
-            paramMap.put("createdAtEnd", com.uhuila.common.util.DateUtil.getEndOfDay(endAt));
-        }
-
-        return condBuilder.toString();
-    }
-
-    public String getFilterRefundAt() {
-        StringBuilder condBuilder = new StringBuilder(" where e.orderItems=r and e.goods.supplierId =s.id and s.deleted=0 and s.salesId=ou.id and ou.deleted=0 and e.goods.isLottery=false and e.status = models.order.ECouponStatus.REFUND");
-
-        Boolean hasSeeReportProfitRight = ContextedPermission.hasPermission("SEE_OPERATION_REPORT_PROFIT");
-        if (!hasSeeReportProfitRight) {
-            condBuilder.append(" and ou.id =:salesId");
-            paramMap.put("salesId", salesId);
-        }
-        if (StringUtils.isNotBlank(userName)) {
-            condBuilder.append(" and ou.userName like :shortName");
-            paramMap.put("shortName", "%" + userName + "%");
-        }
-        if (StringUtils.isNotBlank(jobNumber)) {
-            condBuilder.append(" and ou.jobNumber= :jobNumber");
-            paramMap.put("jobNumber", jobNumber);
-        }
-        if (beginAt != null) {
-            condBuilder.append(" and e.refundAt >= :createdAtBegin");
-            paramMap.put("createdAtBegin", beginAt);
-        }
-        if (endAt != null) {
-            condBuilder.append(" and e.refundAt < :createdAtEnd");
-            paramMap.put("createdAtEnd", com.uhuila.common.util.DateUtil.getEndOfDay(endAt));
-        }
-
-        return condBuilder.toString();
-    }
-
-    public String getFilterOfPeopleEffect() {
-        StringBuilder condBuilder = new StringBuilder(" where e.orderItems=r and r.goods.supplierId =s.id and s.deleted=0 and s.salesId=o.id and o.deleted=0  and (r.order.status='PAID' or r.order.status='SENT') and r.goods.isLottery=false");
-        Boolean hasSeeReportProfitRight = ContextedPermission.hasPermission("SEE_OPERATION_REPORT_PROFIT");
-        if (!hasSeeReportProfitRight) {
-            condBuilder.append(" and o.id =:salesId");
-            paramMap.put("salesId", salesId);
-        }
-        if (StringUtils.isNotBlank(userName)) {
-            condBuilder.append(" and o.userName like :shortName");
-            paramMap.put("shortName", "%" + userName + "%");
-        }
-        if (StringUtils.isNotBlank(jobNumber)) {
-            condBuilder.append(" and o.jobNumber= :jobNumber");
-            paramMap.put("jobNumber", jobNumber);
-        }
-
-        if (beginAt != null) {
-            condBuilder.append(" and r.order.paidAt >= :createdAtBegin");
-            paramMap.put("createdAtBegin", beginAt);
-        }
-        if (endAt != null) {
-            condBuilder.append(" and r.order.paidAt < :createdAtEnd");
-            paramMap.put("createdAtEnd", DateUtil.getEndOfDay(endAt));
-        }
-
-
-        return condBuilder.toString();
-
-
-    }
-
     public String getResalerFilterOfPeopleEffect() {
         StringBuilder condBuilder = new StringBuilder(" where r.goods.supplierId =s.id and s.deleted=0 and s.salesId=ou.id and ou.deleted=0 and r.order.userType=models.accounts.AccountType.RESALER " +
                 " and (r.order.status='PAID' or r.order.status='SENT')" +
@@ -216,21 +129,21 @@ public class PeopleEffectCategoryReportCondition implements Serializable {
 
     private Map<String, Object> paramMap1 = new HashMap<>();
 
-    public String getRefundFilterOfPeopleEffect(ECouponStatus status) {
+    public String getECouponFilterOfPeopleEffect(ECouponStatus status) {
         paramMap1 = new HashMap<>();
-        StringBuilder condBuilder = new StringBuilder(" where e.orderItems=r and r.goods.supplierId =s.id and e.goods.supplierId=s.id and s.deleted=0 and s.salesId=o.id and o.deleted=0 and e.status=:status and e.goods.isLottery=false");
+        StringBuilder condBuilder = new StringBuilder(" where e.orderItems=r and r.goods.supplierId =s.id and e.goods.supplierId=s.id and s.deleted=0 and s.salesId=ou.id and ou.deleted=0 and e.status=:status and e.goods.isLottery=false");
         paramMap1.put("status", status);
         Boolean hasSeeReportProfitRight = ContextedPermission.hasPermission("SEE_OPERATION_REPORT_PROFIT");
         if (!hasSeeReportProfitRight) {
-            condBuilder.append(" and o.id =:salesId");
+            condBuilder.append(" and ou.id =:salesId");
             paramMap1.put("salesId", salesId);
         }
         if (StringUtils.isNotBlank(userName)) {
-            condBuilder.append(" and o.userName like :userName");
+            condBuilder.append(" and ou.userName like :userName");
             paramMap1.put("userName", "%" + userName + "%");
         }
         if (StringUtils.isNotBlank(jobNumber)) {
-            condBuilder.append(" and o.jobNumber=:jobNumber");
+            condBuilder.append(" and ou.jobNumber=:jobNumber");
             paramMap1.put("jobNumber", jobNumber);
         }
         if (status == ECouponStatus.REFUND) {
