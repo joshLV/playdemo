48155bf120f0fa38d54a5bfd4ba2058f0208e11c
yanjingyun <yanjingyun@uhuila.com>
2013-03-08 16:32:22 +0800

    完善渠道订单上传

--- operate/business/app/controllers/ImportPartnerOrders.java
+++ operate/business/app/controllers/ImportPartnerOrders.java
@@ -30,7 +30,6 @@ import java.util.Map;
 @With(OperateRbac.class)
 @ActiveNavigation("import_order_index")
 public class ImportPartnerOrders extends Controller {
-
     @ActiveNavigation("import_order_index")
     public static void index() {
         render();
@@ -43,10 +42,10 @@ public class ImportPartnerOrders extends Controller {
      * @param partner
      */
     public static void upload(File orderFile, String partner) {
-        String msgInfo = "";
+        String errorInfo = "";
         if (orderFile == null) {
-            msgInfo = "请先选择文件！";
-            render("ImportPartnerOrders/index.html", msgInfo);
+            errorInfo = "请先选择文件！";
+            render("ImportPartnerOrders/index.html", errorInfo, partner);
         }
         List<PartnerOrderView> partnerOrderViews = new ArrayList<>();
         try {
@@ -58,22 +57,19 @@ public class ImportPartnerOrders extends Controller {
             Map<String, Object> beans = new HashMap<>();
             beans.put("partnerOrderViews", partnerOrderViews);
 
-            //转换
             XLSReadStatus readStatus = mainReader.read(new FileInputStream(orderFile), beans);
             if (!readStatus.isStatusOK()) {
-                msgInfo = "转换出错，请检查文件格式！";
-                render("ImportPartnerOrders/index.html", msgInfo);
+                errorInfo = "转换出错，请检查文件格式！";
+                render("ImportPartnerOrders/index.html", errorInfo, partner);
             }
         } catch (Exception e) {
-            msgInfo = "转换出现异常，请检查文件格式！";
-            render("ImportPartnerOrders/index.html", msgInfo);
+            errorInfo = "转换出现异常，请检查文件格式！";
+            render("ImportPartnerOrders/index.html", errorInfo, partner);
         }
 
         int duplicateCount = 0;
         List<String> orderList = new ArrayList<>();
         List<String> goodsList = new ArrayList<>();
-        String existedOrders = "";
-        String notExistGoods = "";
         String preGoodsNo = "";
         String duplicateInfo = "";
         for (PartnerOrderView view : partnerOrderViews) {
@@ -83,22 +79,25 @@ public class ImportPartnerOrders extends Controller {
                 if (ybqOrder != null) {
                     outerOrder.ybqOrder = ybqOrder;
                     outerOrder.save();
-                    msgInfo = "外部订单导入完毕！";
+                    ybqOrder.paidAt = view.paidAt;
+                    ybqOrder.createdAt = view.paidAt;
+                    ybqOrder.save();
+                    renderArgs.put("successInfo", "外部订单导入完毕！");
                 } else {
                     if (!preGoodsNo.equals(view.outerGoodsNo)) {
                         goodsList.add(view.outerGoodsNo);
-                        notExistGoods = StringUtils.join(goodsList, ",");
                         preGoodsNo = view.outerGoodsNo;
                     }
-                    msgInfo = "请检查以下渠道商品ID是否已映射一百券商品ID！";
+                    renderArgs.put("notExistGoodsInfo", "请检查以下渠道商品ID是否已映射一百券商品ID！");
                 }
             } catch (Exception e) {
                 orderList.add(view.outerOrderNo);
-                duplicateInfo = "重复订单:" + duplicateCount++ + "个,";
-                existedOrders = StringUtils.join(orderList, ",");
+                duplicateInfo = "重复订单:" + (duplicateCount++) + "个,";
             }
         }
-        render("ImportPartnerOrders/index.html", partner, duplicateInfo, msgInfo, duplicateCount, existedOrders, notExistGoods);
-    }
 
+        renderArgs.put("notExistGoods", StringUtils.join(goodsList, ","));
+        renderArgs.put("existedOrders", StringUtils.join(orderList, ","));
+        render("ImportPartnerOrders/index.html", partner, duplicateInfo, errorInfo, duplicateCount);
+    }
 }
