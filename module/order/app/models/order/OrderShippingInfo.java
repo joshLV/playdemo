dddaaa821b68003bbe6c9682e5fd11745fe14f94
tangliqun <tangliqun@uhuila.com>
2013-03-14 10:09:08 +0800

    fix 编译错.

--- module/order/app/models/order/OrderShippingInfo.java
+++ module/order/app/models/order/OrderShippingInfo.java
@@ -44,7 +44,7 @@ public class OrderShippingInfo extends Model {
      * TODO: 删除这一属性，值放orderItems中了
      */
     @Column(name = "buy_number")
-    public Long buyNumber;
+    public Integer buyNumber;
 
     /**
      * 备注
