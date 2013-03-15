3fde5ce0527b7937b8dfc4d6329c6f1762d9e6a8
likang <likang@uhuila.com>
2013-03-15 18:40:43 +0800

    运营后台测试上传发货单

--- operate/business/test/functional/UploadOrderShippingInfosTest.java
+++ operate/business/test/functional/UploadOrderShippingInfosTest.java
@@ -2,18 +2,14 @@ package functional;
 
 import controllers.operate.cas.Security;
 import factory.FactoryBoy;
+import factory.callback.BuildCallback;
 import models.operator.OperateUser;
-import models.order.ExpressCompany;
-import models.order.OrderItems;
-import models.order.OrderStatus;
+import models.order.*;
 import models.sales.Goods;
 import models.sales.Sku;
 import models.supplier.Supplier;
 import operate.rbac.RbacLoader;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import play.Play;
 import play.mvc.Http;
 import play.mvc.Router;
@@ -21,7 +17,9 @@ import play.test.FunctionalTest;
 import play.vfs.VirtualFile;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p/>
@@ -35,6 +33,8 @@ public class UploadOrderShippingInfosTest extends FunctionalTest {
     Sku sku;
     ExpressCompany expressCompany;
     Goods goods;
+    OrderShippingInfo orderShippingInfo;
+    OrderShippingInfo orderShippingInfo1;
 
     /**
      * 测试数据准备
@@ -52,11 +52,23 @@ public class UploadOrderShippingInfosTest extends FunctionalTest {
         Security.setLoginUserForTest(user.loginName);
         FactoryBoy.create(ExpressCompany.class);
         sku = FactoryBoy.create(Sku.class);
-        goods = FactoryBoy.create(Goods.class);
-        goods.sku = sku;
-        goods.save();
+        goods = FactoryBoy.create(Goods.class, new BuildCallback<Goods>() {
+            @Override
+            public void build(Goods target) {
+                target.sku = sku;
+                target.code = "1";
+            }
+        });
         orderItems = FactoryBoy.create(OrderItems.class);
-        orderItems1 = FactoryBoy.create(OrderItems.class);
+        orderShippingInfo = FactoryBoy.create(OrderShippingInfo.class);
+        orderShippingInfo1 = FactoryBoy.create(OrderShippingInfo.class);
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+        Security.cleanLoginUserForTest();
+
     }
 
     @BeforeClass
@@ -80,34 +92,45 @@ public class UploadOrderShippingInfosTest extends FunctionalTest {
 
     @Test
     public void testUpload() {
-
         orderItems.goods = goods;
         orderItems.order.orderNumber = "15558146";
         orderItems.order.save();
         orderItems.status = OrderStatus.PREPARED;
+        orderItems.shippingInfo = orderShippingInfo;
         orderItems.save();
 
-//        orderItems1.goods = goods;
-//        orderItems1.order.orderNumber = "18600152";
-//        orderItems1.order.save();
-//        orderItems1.status = OrderStatus.PREPARED;
-//        orderItems1.save();
-//        assertEquals(OrderStatus.PREPARED, orderItems.status);
-//        assertNull(orderItems.shippingInfo);
-//           List<OrderItems> a=OrderItems.findAll();
-//            for (OrderItems item:a){
-//                System.out.println( item.goods.id+ ">>>>"+item.order.orderNumber+"==="+item.goods.sku);
-//            }
-//        VirtualFile vfImage = VirtualFile.fromRelativePath("test/data/order_shipping.xlsx");
-//        Map<String, File> fileParams = new HashMap<>();
-//        fileParams.put("orderShippingFile", vfImage.getRealFile());
-//        Map<String, String> params = new HashMap<>();
-//        Http.Response response = POST(Router.reverse("UploadOrderShippingInfos.upload").url, params, fileParams);
-//        assertIsOk(response);
-//        assertContentType("text/html", response);
-//        assertEquals(OrderStatus.SENT, orderItems.status);
-//        assertEquals("test", orderItems.shippingInfo.expressCompany.code);
-//        assertEquals("123456", orderItems.shippingInfo.expressNumber);
+        Order order = FactoryBoy.create(Order.class);
+        order.orderNumber = "18600152";
+        order.save();
+
+        orderItems1 = FactoryBoy.create(OrderItems.class);
+        orderItems1.goods = goods;
+        orderItems1.order = order;
+        orderItems1.shippingInfo = orderShippingInfo1;
+        orderItems1.status = OrderStatus.PREPARED;
+        orderItems1.save();
+        assertEquals(OrderStatus.PREPARED, orderItems.status);
+        assertNull(orderItems.shippingInfo.expressCompany);
+        assertEquals(OrderStatus.PREPARED, orderItems1.status);
+        assertNull(orderItems1.shippingInfo.expressCompany);
+
+        VirtualFile vfImage = VirtualFile.fromRelativePath("test/data/order_shipping.xlsx");
+        Map<String, File> fileParams = new HashMap<>();
+        fileParams.put("orderShippingFile", vfImage.getRealFile());
+        Map<String, String> params = new HashMap<>();
+        Http.Response response = POST(Router.reverse("UploadOrderShippingInfos.upload").url, params, fileParams);
+        assertIsOk(response);
+        assertContentType("text/html", response);
+        orderItems.refresh();
+        orderShippingInfo.refresh();
+        assertEquals(OrderStatus.SENT, orderItems.status);
+        assertEquals("test", orderShippingInfo.expressCompany.code);
+        assertEquals("1234568", orderShippingInfo.expressNumber);
+        orderItems1.refresh();
+        orderShippingInfo1.refresh();
+        assertEquals(OrderStatus.SENT, orderItems1.status);
+        assertEquals("test", orderShippingInfo1.expressCompany.code);
+        assertEquals("12345689", orderShippingInfo1.expressNumber);
     }
 
 }
