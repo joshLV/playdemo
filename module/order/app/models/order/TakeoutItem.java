f6b74f64951fe00109871649950d6fd9545e7bc8
苏杰 <sujie@uhuila.com>
2013-04-07 09:38:39 +0800

    解决测试用例问题

--- module/order/app/models/order/TakeoutItem.java
+++ module/order/app/models/order/TakeoutItem.java
@@ -15,7 +15,6 @@ public class TakeoutItem {
     public Long count;
 
     public TakeoutItem(Goods goods, Long count) {
-        System.out.println("goods.id:" + goods.id);
         this.sku = goods.sku;
         this.count = count * goods.skuCount;
     }
