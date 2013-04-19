b592b309cb5eda15395d396488b99403c1aea2b2
likang <likang@uhuila.com>
2013-04-19 17:59:37 +0800

    完善查询功能

--- website/www/app/controllers/KtvAPI.java
+++ website/www/app/controllers/KtvAPI.java
@@ -3,13 +3,6 @@ package controllers;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import models.ktv.KtvPriceSchedule;
-import models.ktv.KtvRoom;
-import models.ktv.KtvRoomOrderInfo;
-import models.order.OuterOrderPartner;
-import models.sales.Goods;
-import models.sales.GoodsStatus;
-import models.sales.ResalerProduct;
-import models.sales.Shop;
 import play.mvc.Controller;
 
 import java.util.*;
@@ -21,51 +14,13 @@ import java.util.*;
  */
 public class KtvAPI extends Controller {
 
-    public static void jsonRoom(String productId, Date day) {
-        Goods goods = ResalerProduct.getGoodsByPartnerProductId(productId, OuterOrderPartner.SINA);
-        if (goods == null || goods.status != GoodsStatus.ONSALE) {
-            error("no goods!");
+    public static void jsonRoom(Long shopId, Date day) {
+        Map<String, Object> jsonParams = KtvPriceSchedule.dailyScheduleOverview(shopId, day);
+        if (jsonParams == null) {
+            error("shop not found");
         }
-        Map<String, Object> jsonParams = new HashMap<>();
-
-        Collection<Shop> shops = goods.getShopList();
-        Shop shop = shops.iterator().next();
-        List<KtvRoom> roomList = KtvRoom.findByShop(shop);
-        List<Map<String, Object>> rooms = new ArrayList<>();
-        for (KtvRoom ktvRoom : roomList) {
-            Map<String, Object> roomsInfo = new HashMap<>();
-            roomsInfo.put("id", ktvRoom.id);
-            roomsInfo.put("name", ktvRoom.roomType.name);
-            roomsInfo.put("type", ktvRoom.roomType.id);
-            rooms.add(roomsInfo);
-        }
-
-        List<KtvPriceSchedule> schedules = KtvPriceSchedule.getSchedulesByShop(day, shop);
-        List<Map<String, Object>> prices = new ArrayList<>();
-        for (KtvPriceSchedule schedule : schedules) {
-            Map<String, Object> scheduleInfo = new HashMap<>();
-            scheduleInfo.put("weekday", schedule.useWeekDay);
-            scheduleInfo.put("startTime", schedule.startTime);
-            scheduleInfo.put("endTime", schedule.endTime);
-            scheduleInfo.put("price", schedule.price);
-            scheduleInfo.put("roomType", schedule.roomType.id);
-            prices.add(scheduleInfo);
-        }
-        List<KtvRoomOrderInfo> scheduledRoomList = KtvRoomOrderInfo.findScheduledInfos(day, shop);
-        List<Map<String, Object>> scheduleList = new ArrayList<>();
-        for (KtvRoomOrderInfo orderInfo : scheduledRoomList) {
-            Map<String, Object> scheduleRoomInfo = new HashMap<>();
-            scheduleRoomInfo.put("roomId", orderInfo.ktvRoom.id);
-            scheduleRoomInfo.put("roomTime", orderInfo.scheduledTime);
-            scheduleList.add(scheduleRoomInfo);
-        }
-
-        jsonParams.put("rooms", rooms);
-        jsonParams.put("prices", prices);
-        jsonParams.put("schedules", scheduleList);
 
         Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
-
         renderJSON(gson.toJson(jsonParams));
     }
 
