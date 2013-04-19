0573a33cf7811e198edeaddafbc0fafc211184bb
likang <likang@uhuila.com>
2013-04-19 16:17:00 +0800

    初始化后台查看每日预订情况

---
+++ supplier/ktv/app/controllers/KtvDailySchedule.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * @author likang
+ *         Date: 13-4-19
+ */
+public class KtvDailySchedule extends Controller {
+
+    public static void index(){
+        render();
+    }
+
+}
