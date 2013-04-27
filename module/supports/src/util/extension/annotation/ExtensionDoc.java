f66bb06ca06857baa6a8001437469b7118bdc64b
tangliqun <tangliqun@uhuila.com>
2013-04-27 15:13:55 +0800

    add ExtendsionDoc，用于描述扩展文档.

---
+++ module/supports/src/util/extension/annotation/ExtensionDoc.java
@@ -0,0 +1,17 @@
+package util.extension.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * User: tanglq
+ * Date: 13-4-22
+ * Time: 下午5:24
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface ExtensionDoc {
+    String value();
+}
