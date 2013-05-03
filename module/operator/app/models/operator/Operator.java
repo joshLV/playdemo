4665c2e1c69335d2fe2a9984d8b9e43f6b1d3e84
wangjia <clarawang89@hotmail.com>
2013-05-03 18:55:08 +0800

    add Operators

--- module/operator/app/models/operator/Operator.java
+++ module/operator/app/models/operator/Operator.java
@@ -1,12 +1,15 @@
 package models.operator;
 
 import com.uhuila.common.constants.DeletedStatus;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang.StringUtils;
 import play.data.validation.Match;
 import play.data.validation.MaxSize;
 import play.data.validation.MinSize;
 import play.data.validation.Phone;
 import play.data.validation.Required;
 import play.db.jpa.Model;
+import play.libs.Images;
 import play.modules.view_ext.annotation.Mobile;
 
 import javax.persistence.Column;
@@ -16,7 +19,9 @@ import javax.persistence.Enumerated;
 import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 运营商.
@@ -50,7 +55,6 @@ public class Operator extends Model {
     @Column(name = "updated_at")
     public Date updatedAt;
 
-    @Required
     @Match(value = "\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*", message = "邮箱格式不对！")
     public String email;
 
@@ -67,7 +71,38 @@ public class Operator extends Model {
     @Column(name = "updated_by")
     public String updatedBy;
 
-    public static String checkValue(Long id, String name) {
+    public static void update(long id, Operator operator, String updatedBy) {
+        Operator updatedOperator = Operator.findById(id);
+        updatedOperator.name = operator.name;
+        updatedOperator.code = operator.code;
+        updatedOperator.mobile = operator.mobile;
+        updatedOperator.email = operator.email;
+        updatedOperator.phone = operator.phone;
+        updatedOperator.updatedAt = new Date();
+        updatedOperator.remark = operator.remark;
+        updatedOperator.companyName = operator.companyName;
+        updatedOperator.save();
+    }
+
+    public static List<Operator> findByCondition(String name, String code) {
+        StringBuilder sql = new StringBuilder("deleted= :deleted ");
+        Map<String, Object> paramsMap = new HashMap<>();
+        paramsMap.put("deleted", DeletedStatus.UN_DELETED);
+        if (StringUtils.isNotBlank(name)) {
+            sql.append("and name like :name ");
+            paramsMap.put("name", name);
+        }
+
+        if (StringUtils.isNotBlank(code)) {
+            sql.append("and code like :code ");
+            paramsMap.put("code", code + "%");
+        }
+
+        sql.append(" order by createdAt DESC");
+        return find(sql.toString(), paramsMap).fetch();
+    }
+
+    public static String checkNameAndCode(Long id, String name, String code) {
         StringBuilder sq = new StringBuilder("name = ? ");
         List list = new ArrayList();
         list.add(name);
@@ -77,8 +112,20 @@ public class Operator extends Model {
         }
         List<Operator> operatorList = Operator.find(sq.toString(), list.toArray()).fetch();
         String returnFlag = "0";
-        //用户名存在的情况
-        if (operatorList.size() > 0) returnFlag = "1";
+        //姓名存在的情况
+        if (operatorList.size() > 0) returnFlag = "existedName";
+        else {
+            sq = new StringBuilder("code = ? ");
+            list = new ArrayList();
+            list.add(code);
+            if (id != null) {
+                sq.append("and id <> ?");
+                list.add(id);
+            }
+            //编码存在的情况
+            List<Operator> mList = Operator.find(sq.toString(), list.toArray()).fetch();
+            if (mList.size() > 0) returnFlag = "existedCode";
+        }
         return returnFlag;
     }
 
