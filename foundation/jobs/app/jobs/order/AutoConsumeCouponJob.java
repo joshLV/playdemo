03abed55d119fb280ac35ee934f866637f48f03d
tangliqun <tangliqun@uhuila.com>
2013-05-20 13:51:33 +0800

    解决自动验证失败的问题。

--- foundation/jobs/app/jobs/order/AutoConsumeCouponJob.java
+++ foundation/jobs/app/jobs/order/AutoConsumeCouponJob.java
@@ -5,7 +5,7 @@ import models.jobs.JobWithHistory;
 import models.jobs.annotation.JobDefine;
 import models.order.ECoupon;
 import models.order.ECouponCreateType;
-import models.order.ECouponStatus;
+import play.Logger;
 import play.jobs.Every;
 import util.extension.ExtensionResult;
 
@@ -17,15 +17,16 @@ import java.util.List;
  *         Date: 12-12-25
  */
 @JobDefine(title = "自动验证券", description = "导入券在售出后，需要自动验证")
-@Every("1mn")
+@Every("6mn")
 public class AutoConsumeCouponJob extends JobWithHistory {
     @Override
     public void doJobWithHistory() {
         //2天内，5分钟前
-        List<ECoupon> couponList = ECoupon.find("createdAt > ? and createdAt < ? and status = ? and createType = ? and autoConsumed = ?",
-                new Date(System.currentTimeMillis() - 2*24*60*60*1000), new Date(System.currentTimeMillis() - 5*60*1000),
-                ECouponStatus.UNCONSUMED, ECouponCreateType.IMPORT, DeletedStatus.UN_DELETED).fetch(20);
+        List<ECoupon> couponList = ECoupon.find("createdAt > ? and createdAt < ? and createType = ? and autoConsumed = ?",
+                new Date(System.currentTimeMillis() - 20*24*60*60*1000), new Date(System.currentTimeMillis() - 5*60*1000),
+                ECouponCreateType.IMPORT, DeletedStatus.UN_DELETED).fetch(20);
         for(ECoupon coupon : couponList) {
+            Logger.info("AutoConsumeCouponJob.verify eCouponSN=%s", coupon.eCouponSn);
             ExtensionResult result = coupon.verifyAndCheckOnPartnerResaler();
             if (result.isOk()) { //验证成功
                 coupon.autoConsumed = DeletedStatus.DELETED;
