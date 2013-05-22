53490ebb38a3b30417456101e68a54e3bb93dc8f
wangjia <clarawang89@hotmail.com>
2013-05-22 16:46:46 +0800

    add RESERVATION_VERIFY in consultType

--- module/sales/app/models/sales/ConsultType.java
+++ module/sales/app/models/sales/ConsultType.java
@@ -6,15 +6,16 @@ package models.sales;
  * Time: 上午10:05
  */
 public enum ConsultType {
-      ORDERCONSULT,     //订购咨询
-      REFUND,          //退款
-      VERIFYERROR,    //验证错误
-      WEBSITEBROKEN,  //网站报修
-      QUESTIION,        //商户疑问
-      FEEDBACK,        //意见反馈
-      COMPLAINT,        //投诉反馈
-      OUTDIAL,          //外拨
-      RESERVATION       //预约咨询
+    ORDERCONSULT,     //订购咨询
+    REFUND,          //退款
+    VERIFYERROR,    //验证错误
+    WEBSITEBROKEN,  //网站报修
+    QUESTIION,        //商户疑问
+    FEEDBACK,        //意见反馈
+    COMPLAINT,        //投诉反馈
+    OUTDIAL,          //外拨
+    RESERVATION,       //预约咨询
+    RESERVATION_VERIFY    //预约验证
 }
 
 
