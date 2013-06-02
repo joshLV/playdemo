4f74cba8cb55ef600e4249fdae8841ba5ea9bdaf
likang <likang@uhuila.com>
2013-06-02 17:34:55 +0800

    调整自动更新sku的job运行时间

--- foundation/jobs/app/jobs/ktv/KtvUpdateSkuJob.java
+++ foundation/jobs/app/jobs/ktv/KtvUpdateSkuJob.java
@@ -16,7 +16,7 @@ import java.util.List;
  * Time: 下午1:48
  */
 @JobDefine(title = "更新淘宝ktv sku", description = "每天18点执行，把当天之前的ktv sku 删除，同时更新最新的sku信息")
-@On("0 0 18 * * ?")
+@On("0 0/10 18,19 * * ?")
 public class KtvUpdateSkuJob extends JobWithHistory {
     @Override
     public void doJobWithHistory() {
