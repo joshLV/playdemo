d49283f3065ee62a0083c4ab259bf59221b0bee2
tangliqun <tangliqun@uhuila.com>
2013-06-19 15:16:07 +0800

    fix consumer redis bug.

--- module/supports/app/models/mq/RabbitMQConsumerWithTx.java
+++ module/supports/app/models/mq/RabbitMQConsumerWithTx.java
@@ -6,7 +6,6 @@ import play.modules.rabbitmq.consumer.RabbitMQConsumer;
 import play.modules.redis.Redis;
 import play.modules.redis.RedisConnectionManager;
 import util.mq.MQPublisher;
-import util.transaction.RedisLock;
 
 import java.util.Set;
 
@@ -19,25 +18,27 @@ public abstract class RabbitMQConsumerWithTx<T> extends RabbitMQConsumer<T> {
             queueIDMessage = (QueueIDMessage) t;
         }
 
-        // 检查是否可执行
-        if (queueIDMessage != null) {
-            if (!Redis.exists(MQPublisher.getMessageRedisId(queueIDMessage))) {
-                Logger.info("未找到需要运行的Redis Message Key，应该是已经运行好了，本次运行跳过。message:" + queueIDMessage);
-                return;
-            }
-            if (queueIDMessage.queueIDRunType() == QueueIDRunType.LAST_IN_FIRST_RUN) {
-                Set<String> uuidsNeedRun = Redis.zrevrange(MQPublisher.getMessageRedisId(queueIDMessage), 0, 0);
-                if (!uuidsNeedRun.contains(queueIDMessage.getUuid())) {
-                    Logger.info("当前消息不是需要执行的Message，跳过。message:" + queueIDMessage);
+        boolean rollback = false;
+
+        try {
+
+            // 检查是否可执行
+            if (queueIDMessage != null) {
+                if (!Redis.exists(MQPublisher.getMessageRedisId(queueIDMessage))) {
+                    Logger.info("未找到需要运行的Redis Message Key，应该是已经运行好了，本次运行跳过。message:" + queueIDMessage);
                     return;
                 }
+                if (queueIDMessage.queueIDRunType() == QueueIDRunType.LAST_IN_FIRST_RUN) {
+                    Set<String> uuidsNeedRun = Redis.zrevrange(MQPublisher.getMessageRedisId(queueIDMessage), 0, 0);
+                    if (!uuidsNeedRun.contains(queueIDMessage.getUuid())) {
+                        Logger.info("当前消息不是需要执行的Message，跳过。message:" + queueIDMessage);
+                        return;
+                    }
+                }
             }
-        }
 
-        JPAPlugin.startTx(false);
+            JPAPlugin.startTx(false);
 
-        boolean rollback = false;
-        try {
             consumeWithTx(t);
 
             // 执行成功，清除queue id
