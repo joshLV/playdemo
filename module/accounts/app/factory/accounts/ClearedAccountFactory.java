fa895092ab0342930029b0a38c29ac2074e1685c
wangjia <clarawang89@hotmail.com>
2013-07-10 09:42:15 +0800

    temp fix-prepayment-test

---
+++ module/accounts/app/factory/accounts/ClearedAccountFactory.java
@@ -0,0 +1,13 @@
+package factory.accounts;
+
+import factory.ModelFactory;
+import models.accounts.ClearedAccount;
+
+/**
+ * User: wangjia
+ * Date: 13-7-10
+ * Time: 上午9:40
+ */
+public class ClearedAccountFactory extends ModelFactory<ClearedAccount> {
+    
+}
