58f02878f6cefd628e3933c7eae6190aae044120
wangjia <clarawang89@hotmail.com>
2013-07-25 12:30:39 +0800

    add exporting wuba channel account cheaking

--- operate/business/app/controllers/ExportChannelAccountChecking.java
+++ operate/business/app/controllers/ExportChannelAccountChecking.java
@@ -4,10 +4,14 @@ import com.uhuila.common.util.DateUtil;
 import models.operator.Operator;
 import models.order.ChannelAccountCheckingDetail;
 import models.resale.Resaler;
+import models.wuba.WubaUtil;
 import operate.rbac.annotations.ActiveNavigation;
 import play.mvc.Controller;
 import play.mvc.With;
+import util.DateHelper;
 
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,10 +29,18 @@ public class ExportChannelAccountChecking extends Controller {
 
     }
 
-    public static void export58(ChannelAccountCheckingDetail bill) {
-        String startDay = DateUtil.dateToString(bill.startDay,"yyyy";
-        String endDay = bill.endDay.toString();
-        System.out.println("startDay = " + startDay);
-//        createWubaAccountCheckingDetail
+    public static void wubaExcelOut(ChannelAccountCheckingDetail bill) throws ParseException {
+        request.format = "xls";
+        renderArgs.put("__FILE_NAME__", "58对账清单_" + DateUtil.dateToString(bill.startDay,
+                "yyyy-MM-dd") + "-" + DateUtil.dateToString(bill.endDay, "yyyy-MM-dd") + ".xls");
+        int days = DateUtil.daysBetween(bill.startDay, bill.endDay) + 1;
+        List<ChannelAccountCheckingDetail> thirdBills = new ArrayList<>();
+        for (int i = 0; i < days; i++) {
+            String date = DateUtil.dateToString(DateHelper.afterDays(bill.startDay, 0),
+                    "yyyy-MM-dd");
+            List<ChannelAccountCheckingDetail> tempThirdBills = WubaUtil.createWubaAccountCheckingDetail(date);
+            thirdBills.addAll(tempThirdBills);
+        }
+        render(thirdBills);
     }
 }
