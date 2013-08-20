88ab6dbc7c8936d2cef7dcff91b638f9dffe1cf4
yanjingyun <yanjingyun@uhuila.com>
2013-08-20 16:48:46 +0800

    添加速8和a8自动消费的门店

--- supplier/home/app/controllers/SupplierVerifyResalerECoupons.java
+++ supplier/home/app/controllers/SupplierVerifyResalerECoupons.java
@@ -134,7 +134,7 @@ public class SupplierVerifyResalerECoupons extends Controller {
         jsonMap.put("partnerGoodsId", partnerGoodsId);
         jsonMap.put("partnerShopId", supplierResalerShop.resalerPartnerShopId);
         jsonMap.put("goodsId", supplierResalerProduct.goods.id.toString());
-//        body = "{\"status\":0,\"data\":[{\"result\":\"\u6d88\u8d39\u6210\u529f\",\"code\":\"201819625306\",\"errcode\":false}]}";
+        body = "{\"status\":0,\"data\":[{\"result\":\"\u6d88\u8d39\u6210\u529f\",\"code\":\"9083555383\",\"errcode\":false}]}";
         JsonParser jsonParser = new JsonParser();
         JsonObject result = jsonParser.parse(body).getAsJsonObject();
         String errcode = "";
